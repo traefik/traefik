4232cccd7548c3f0ecdbbfb27fe2b5189d4eff4f
emile <emile@vauge.com>
2015-09-12 01:30:28 +0200

    Dockerfile

---
+++ Dockerfile.run
@@ -0,0 +1,3 @@
+FROM scratch
+ADD traefik /
+CMD ["/traefik"]
--- docker.go
+++ docker.go
@@ -66,11 +66,11 @@ func (provider *DockerProvider) Provide(configurationChan chan <- *Configuration
 		log.Fatalf("Failed to create a client for docker, error: %s", err)
 	} else {
 		provider.dockerClient = client
-		info, err := provider.dockerClient.Info()
+		_, err := provider.dockerClient.Info()
 		if (err != nil){
 			log.Fatalf("Docker connection error %+v", err)
 		}
-		log.Debug("Docker info %=v", info)
+		log.Debug("Docker connection established")
 		dockerEvents := make(chan *docker.APIEvents)
 		if (provider.Watch) {
 			provider.dockerClient.AddEventListener(dockerEvents)
