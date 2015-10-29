b71b5dd0d4f471e02e8709af409fc5d08e741807
Vincent Demeester <vincent@sbr.pm>
2015-10-29 22:04:23 +0100

    Use go generate for dockerversion
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- generate.go
+++ generate.go
@@ -6,4 +6,7 @@ Copyright
 //go:generate rm -vf gen.go
 //go:generate go-bindata -o gen.go static/... templates/...
 
+//go:generate mkdir -p vendor/github.com/docker/docker/autogen/dockerversion
+//go:generate cp script/dockerversion vendor/github.com/docker/docker/autogen/dockerversion/dockerversion.go
+
 package main
