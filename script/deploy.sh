60e9282f0adac48cbf283306ceb08ad7a31ac94b
emile <emile@vauge.com>
2016-02-22 15:35:51 +0100

    fixes typo ghr

--- script/deploy.sh
+++ script/deploy.sh
@@ -10,7 +10,7 @@ fi
 
 curl -LO https://github.com/tcnksm/ghr/releases/download/pre-release/linux_amd64.zip
 unzip -q linux_amd64.zip
-sudo mv grh /usr/bin/ghr
+sudo mv ghr /usr/bin/ghr
 sudo chmod +x /usr/bin/ghr
 
 ghr -t $GITHUB_TOKEN -u emilevauge -r traefik --prerelease ${VERSION} dist/
