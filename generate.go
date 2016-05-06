c61d9776e75bdcd7ff84ecfd6906307fc38a3505
Ján Koščo <3k.stanley@gmail.com>
2016-05-06 16:37:46 +0200

    Create static folder on first generate

--- generate.go
+++ generate.go
@@ -3,6 +3,7 @@ Copyright
 */
 
 //go:generate rm -vf autogen/gen.go
+//go:generate mkdir -p static
 //go:generate go-bindata -pkg autogen -o autogen/gen.go ./static/... ./templates/...
 
 //go:generate mkdir -p vendor/github.com/docker/docker/autogen/dockerversion
