2290f78d123842ff9ab0d954b0a6f4dfec7f8b68
Diego Oliveira <diego@diegooliveira.com>
2016-10-22 18:47:19 -0200

    Add forward authentication option
        A forward authentication that extracts parameters from the request and
    then call a authenticate endpoint. If the authentication outcome is a 200
    it allows to extract information from the response body and add to the
    proxy call.

--- middlewares/authenticator.go
+++ middlewares/authenticator.go
@@ -2,12 +2,15 @@ package middlewares
 
 import (
 	"fmt"
-	"github.com/abbot/go-http-auth"
+	goauth "github.com/abbot/go-http-auth"
+
 	"github.com/codegangsta/negroni"
+	"github.com/containous/traefik/auth"
 	"github.com/containous/traefik/log"
 	"github.com/containous/traefik/types"
 	"net/http"
 	"strings"
+
 )
 
 // Authenticator is a middleware that provides HTTP basic and digest authentication
@@ -28,7 +31,7 @@ func NewAuthenticator(authConfig *types.Auth) (*Authenticator, error) {
 		if err != nil {
 			return nil, err
 		}
-		basicAuth := auth.NewBasicAuthenticator("traefik", authenticator.secretBasic)
+		basicAuth := goauth.NewBasicAuthenticator("traefik", authenticator.secretBasic)
 		authenticator.handler = negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 			if username := basicAuth.CheckAuth(r); username == "" {
 				log.Debugf("Auth failed...")
@@ -42,13 +45,17 @@ func NewAuthenticator(authConfig *types.Auth) (*Authenticator, error) {
 		if err != nil {
 			return nil, err
 		}
-		digestAuth := auth.NewDigestAuthenticator("traefik", authenticator.secretDigest)
+		digestAuth := goauth.NewDigestAuthenticator("traefik", authenticator.secretDigest)
 		authenticator.handler = negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 			if username, _ := digestAuth.CheckAuth(r); username == "" {
 				digestAuth.RequireAuth(w, r)
 			} else {
 				next.ServeHTTP(w, r)
 			}
+		})
+	} else if authConfig.Forward != nil {
+		authenticator.handler = negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+			auth.Forward(authConfig.Forward, w, r, next)
 		})
 	}
 	return &authenticator, nil
