9909e66715049757ad2752875ece555b1f8b4917
Andrzej Ressel <jereksel@gmail.com>
2017-04-08 18:51:00 +0200

    Add generic metric for successfull/failing requests

--- middlewares/metrics.go
+++ middlewares/metrics.go
@@ -1,16 +1,18 @@
 package middlewares
 
 import (
-	"github.com/go-kit/kit/metrics"
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/go-kit/kit/metrics"
 )
 
 // Metrics is an Interface that must be satisfied by any system that
 // wants to expose and monitor metrics
 type Metrics interface {
 	getReqsCounter() metrics.Counter
+	getReqsStatusCounter() metrics.Counter
 	getLatencyHistogram() metrics.Histogram
 	handler() http.Handler
 }
@@ -36,7 +38,17 @@ func (m *MetricsWrapper) ServeHTTP(rw http.ResponseWriter, r *http.Request, next
 	prw := &responseRecorder{rw, http.StatusOK}
 	next(prw, r)
 	labels := []string{"code", strconv.Itoa(prw.StatusCode()), "method", r.Method}
+
+	var state string
+	if prw.StatusCode() < 400 {
+		state = "Successful"
+	} else {
+		state = "Failing"
+	}
+
+	labelsStatus := []string{"state", state, "method", r.Method}
 	m.Impl.getReqsCounter().With(labels...).Add(1)
+	m.Impl.getReqsStatusCounter().With(labelsStatus...).Add(1)
 	m.Impl.getLatencyHistogram().Observe(float64(time.Since(start).Seconds()))
 }
 
--- middlewares/prometheus.go
+++ middlewares/prometheus.go
@@ -1,28 +1,35 @@
 package middlewares
 
 import (
+	"net/http"
+
 	"github.com/containous/traefik/types"
 	"github.com/go-kit/kit/metrics"
 	"github.com/go-kit/kit/metrics/prometheus"
 	stdprometheus "github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"net/http"
 )
 
 const (
-	reqsName    = "traefik_requests_total"
-	latencyName = "traefik_request_duration_seconds"
+	reqsName       = "traefik_requests_total"
+	reqsStatusName = "traefik_requests_status_total"
+	latencyName    = "traefik_request_duration_seconds"
 )
 
 // Prometheus is an Implementation for Metrics that exposes prometheus metrics for the latency
 // and the number of requests partitioned by status code and method.
 type Prometheus struct {
-	reqsCounter      metrics.Counter
-	latencyHistogram metrics.Histogram
+	reqsCounter       metrics.Counter
+	reqsStatusCounter metrics.Counter
+	latencyHistogram  metrics.Histogram
 }
 
 func (p *Prometheus) getReqsCounter() metrics.Counter {
 	return p.reqsCounter
+}
+
+func (p *Prometheus) getReqsStatusCounter() metrics.Counter {
+	return p.reqsStatusCounter
 }
 
 func (p *Prometheus) getLatencyHistogram() metrics.Histogram {
@@ -39,6 +46,15 @@ func NewPrometheus(name string, config *types.Prometheus) *Prometheus {
 			ConstLabels: stdprometheus.Labels{"service": name},
 		},
 		[]string{"code", "method"},
+	)
+
+	m.reqsStatusCounter = prometheus.NewCounterFrom(
+		stdprometheus.CounterOpts{
+			Name:        reqsStatusName,
+			Help:        "How many HTTP requests processed, partitioned by state (successful/failing) and method.",
+			ConstLabels: stdprometheus.Labels{"service": name},
+		},
+		[]string{"state", "method"},
 	)
 
 	var buckets []float64
