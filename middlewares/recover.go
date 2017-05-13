111251da05a9434a201729e42ac4aea4b80f50f7
Emile Vauge <emile@vauge.com>
2017-05-13 19:02:52 +0200

    Adds Panic Recover middleware
    Signed-off-by: Emile Vauge <emile@vauge.com>

---
+++ middlewares/recover.go
@@ -0,0 +1,32 @@
+package middlewares
+
+import (
+	"github.com/codegangsta/negroni"
+	"github.com/containous/traefik/log"
+	"net/http"
+)
+
+// RecoverHandler recovers from a panic in http handlers
+func RecoverHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer recoverFunc(w)
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
+
+// NegroniRecoverHandler recovers from a panic in negroni handlers
+func NegroniRecoverHandler() negroni.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		defer recoverFunc(w)
+		next.ServeHTTP(w, r)
+	}
+	return negroni.HandlerFunc(fn)
+}
+
+func recoverFunc(w http.ResponseWriter) {
+	if err := recover(); err != nil {
+		log.Errorf("Recovered from panic in http handler: %+v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+}
