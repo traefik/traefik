79bdc64327c12369d23bbdfecaf578efca32a3af
Fernandez Ludovic <ludovic@containo.us>
2017-07-09 13:05:41 +0200

    refactor(accesslog): better use of checker.

--- integration/access_log_test.go
+++ integration/access_log_test.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"regexp"
 	"strings"
 	"time"
 
@@ -47,7 +46,7 @@ func (s *AccessLogSuite) TestAccessLog(c *check.C) {
 	c.Assert(err, checker.IsNil)
 	if len(traefikLog) > 0 {
 		fmt.Printf("%s\n", string(traefikLog))
-		c.Assert(len(traefikLog), checker.Equals, 0)
+		c.Assert(traefikLog, checker.HasLen, 0)
 	}
 
 	// Start test servers
@@ -76,12 +75,12 @@ func (s *AccessLogSuite) TestAccessLog(c *check.C) {
 			count++
 			tokens, err := shellwords.Parse(line)
 			c.Assert(err, checker.IsNil)
-			c.Assert(len(tokens), checker.Equals, 14)
-			c.Assert(regexp.MustCompile(`^\d{3}$`).MatchString(tokens[6]), checker.True)
+			c.Assert(tokens, checker.HasLen, 14)
+			c.Assert(tokens[6], checker.Matches, `^\d{3}$`)
 			c.Assert(tokens[10], checker.Equals, fmt.Sprintf("%d", i+1))
-			c.Assert(strings.HasPrefix(tokens[11], "frontend"), checker.True)
-			c.Assert(strings.HasPrefix(tokens[12], "http://127.0.0.1:808"), checker.True)
-			c.Assert(regexp.MustCompile(`^\d+ms$`).MatchString(tokens[13]), checker.True)
+			c.Assert(tokens[11], checker.HasPrefix, "frontend")
+			c.Assert(tokens[12], checker.HasPrefix, "http://127.0.0.1:808")
+			c.Assert(tokens[13], checker.Matches, `^\d+ms$`)
 		}
 	}
 	c.Assert(count, checker.GreaterOrEqualThan, 3)
@@ -91,7 +90,7 @@ func (s *AccessLogSuite) TestAccessLog(c *check.C) {
 	c.Assert(err, checker.IsNil)
 	if len(traefikLog) > 0 {
 		fmt.Printf("%s\n", string(traefikLog))
-		c.Assert(len(traefikLog), checker.Equals, 0)
+		c.Assert(traefikLog, checker.HasLen, 0)
 	}
 }
 
