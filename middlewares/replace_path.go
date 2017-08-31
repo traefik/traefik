b2121da536381f90e295879b64c07a5af9c4cadc
Tiscs <tiscs@live.com>
2017-08-31 16:40:53 +0800

    Allow modifier "ReplactPath" to replace path with a text template.

--- middlewares/replace_path.go
+++ middlewares/replace_path.go
@@ -1,7 +1,12 @@
 package middlewares
 
 import (
+	"bytes"
 	"net/http"
+	"strings"
+	"text/template"
+
+	"github.com/containous/traefik/log"
 )
 
 // ReplacePath is a middleware used to replace the path of a URL request
@@ -15,7 +20,28 @@ const ReplacedPathHeader = "X-Replaced-Path"
 
 func (s *ReplacePath) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r.Header.Add(ReplacedPathHeader, r.URL.Path)
-	r.URL.Path = s.Path
+	r.URL.Path = s.ReplacePath(r.URL.Path)
 	r.RequestURI = r.URL.RequestURI()
 	s.Handler.ServeHTTP(w, r)
+}
+
+// ReplacePath returns a path replaced with path template.
+func (s *ReplacePath) ReplacePath(source string) string {
+	f := template.FuncMap{
+		"Replace": strings.Replace,
+	}
+	t, err := template.New("replace_path").Funcs(f).Parse(s.Path)
+	if err != nil {
+		log.Error("execution: ", err)
+	} else {
+		var buffer bytes.Buffer
+		if err = t.Execute(&buffer, source); err != nil {
+			log.Error("parsing: ", err)
+		} else {
+			r := buffer.String()
+			log.Debugf("ReplactPath: %s -> %s", source, r)
+			return r
+		}
+	}
+	panic(err)
 }
