b8653da695a5bc2093f1d39e5c2932959a50ee7a
Kendrick Erickson <wheresmysocks@gmail.com>
2017-09-11 21:38:16 -0500

    Fix forward auth to allow redirects

--- auth/forward.go
+++ auth/forward.go
@@ -10,7 +10,14 @@ import (
 
 // Forward the authentication to a external server
 func Forward(forward *types.Forward, w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	httpClient := http.Client{}
+
+	// Ensure our request client does not follow redirects
+
+	httpClient := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
 
 	if forward.TLS != nil {
 		tlsConfig, err := forward.TLS.CreateTLSConfig()
@@ -48,8 +55,30 @@ func Forward(forward *types.Forward, w http.ResponseWriter, r *http.Request, nex
 	}
 	defer forwardResponse.Body.Close()
 
+	// Pass the forward response's body and selected headers if it
+	// didn't return a response within the range of [200, 300).
+
 	if forwardResponse.StatusCode < http.StatusOK || forwardResponse.StatusCode >= http.StatusMultipleChoices {
 		log.Debugf("Remote error %s. StatusCode: %d", forward.Address, forwardResponse.StatusCode)
+
+		// Grab the location header, if any.
+
+		redirectURL, err := forwardResponse.Location()
+
+		if err != nil && err != http.ErrNoLocation {
+			log.Debugf("Error reading response location header %s. Cause: %s", forward.Address, err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// Set the location in our response if one was sent back.
+
+		if err != http.ErrNoLocation && redirectURL.String() != "" {
+			w.Header().Add("Location", redirectURL.String())
+		}
+
+		// Pass the forward request's response code and body.
+
 		w.WriteHeader(forwardResponse.StatusCode)
 		w.Write(body)
 		return
