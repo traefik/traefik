89a47786e03f0dc4fa80be753139ad72d2171c0a
Emile Vauge <emile@vauge.com>
2017-10-10 13:01:11 +0200

    Add Proxy Protocol integration tests
    Signed-off-by: Emile Vauge <emile@vauge.com>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -53,6 +53,7 @@ func init() {
 	check.Suite(&SimpleSuite{})
 	check.Suite(&TimeoutSuite{})
 	check.Suite(&WebsocketSuite{})
+	check.Suite(&ProxyProtocolSuite{})
 }
 
 var traefikBinary = "../dist/traefik"
