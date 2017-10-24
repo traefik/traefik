2c81728e3c438fce1b998147bdcce4ac74b20dfe
Fernandez Ludovic <ludovic@containo.us>
2017-10-24 11:12:36 +0200

    refactor: review tests.

--- middlewares/replace_path_regex_test.go
+++ middlewares/replace_path_regex_test.go
@@ -10,78 +10,71 @@ import (
 
 func TestReplacePathRegex(t *testing.T) {
 	testCases := []struct {
-		desc          string
-		path          string
-		replacement   string
-		regex         string
-		expectedPath  string
-		invalidRegexp bool
+		desc           string
+		path           string
+		replacement    string
+		regex          string
+		expectedPath   string
+		expectedHeader string
 	}{
 		{
-			desc:         "^/whoami/(.*) /who-am-i/$1",
-			path:         "/whoami/and/whoami",
-			replacement:  "/who-am-i/$1",
-			regex:        `^/whoami/(.*)`,
-			expectedPath: "/who-am-i/and/whoami",
+			desc:           "simple regex",
+			path:           "/whoami/and/whoami",
+			replacement:    "/who-am-i/$1",
+			regex:          `^/whoami/(.*)`,
+			expectedPath:   "/who-am-i/and/whoami",
+			expectedHeader: "/whoami/and/whoami",
 		},
 		{
-			desc:         "/whoami /who-am-i",
-			path:         "/whoami/and/whoami",
-			replacement:  "/who-am-i",
-			regex:        `/whoami`,
-			expectedPath: "/who-am-i/and/who-am-i",
+			desc:           "simple replace (no regex)",
+			path:           "/whoami/and/whoami",
+			replacement:    "/who-am-i",
+			regex:          `/whoami`,
+			expectedPath:   "/who-am-i/and/who-am-i",
+			expectedHeader: "/whoami/and/whoami",
 		},
 		{
-			desc:         "^/api/v2/(.*) /api/$1",
-			path:         "/api/v2/users/192",
-			replacement:  "/api/$1",
-			regex:        `^/api/v2/(.*)`,
-			expectedPath: "/api/users/192",
+			desc:           "multiple replacement",
+			path:           "/downloads/src/source.go",
+			replacement:    "/downloads/$1-$2",
+			regex:          `^(?i)/downloads/([^/]+)/([^/]+)$`,
+			expectedPath:   "/downloads/src-source.go",
+			expectedHeader: "/downloads/src/source.go",
 		},
 		{
-			desc:         "^(?i)/downloads/([^/]+)/([^/]+)$ /downloads/$1-$2",
-			path:         "/downloads/src/source.go",
-			replacement:  "/downloads/$1-$2",
-			regex:        `^(?i)/downloads/([^/]+)/([^/]+)$`,
-			expectedPath: "/downloads/src-source.go",
-		},
-		{
-			desc:          "invalid regular expression",
-			path:          "/invalid/regexp/test",
-			replacement:   "/valid/regexp/$1",
-			regex:         `^(?err)/invalid/regexp/([^/]+)$`,
-			expectedPath:  "/invalid/regexp/test",
-			invalidRegexp: true,
+			desc:         "invalid regular expression",
+			path:         "/invalid/regexp/test",
+			replacement:  "/valid/regexp/$1",
+			regex:        `^(?err)/invalid/regexp/([^/]+)$`,
+			expectedPath: "/invalid/regexp/test",
 		},
 	}
 
 	for _, test := range testCases {
+		test := test
 		t.Run(test.desc, func(t *testing.T) {
-			var expectedPath, expectedHeader, actualHeader, requestURI string
+			t.Parallel()
+
+			var actualPath, actualHeader, requestURI string
 			handler := NewReplacePathRegexHandler(
 				test.regex,
 				test.replacement,
 				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					expectedPath = r.URL.Path
+					actualPath = r.URL.Path
 					actualHeader = r.Header.Get(ReplacedPathHeader)
 					requestURI = r.RequestURI
 				}),
 			)
 
 			req := testhelpers.MustNewRequest(http.MethodGet, "http://localhost"+test.path, nil)
-			req.RequestURI = req.URL.Path
-
-			if test.invalidRegexp {
-				expectedHeader = ""
-			} else {
-				expectedHeader = req.URL.Path
-			}
 
 			handler.ServeHTTP(nil, req)
 
-			assert.Equal(t, expectedPath, test.expectedPath, "Unexpected path.")
-			assert.Equal(t, expectedHeader, actualHeader, "Unexpected '%s' header.", ReplacedPathHeader)
-			assert.Equal(t, expectedPath, requestURI, "Unexpected request URI.")
+			assert.Equal(t, test.expectedPath, actualPath, "Unexpected path.")
+			assert.Equal(t, test.expectedHeader, actualHeader, "Unexpected '%s' header.", ReplacedPathHeader)
+			if test.expectedHeader != "" {
+				assert.Equal(t, actualPath, requestURI, "Unexpected request URI.")
+			}
 		})
 	}
 }
