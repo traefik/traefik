5ff0bb0b4d8b08e0928acb8730636ffc6ae8dfd1
Alex Antonov <alex@antonov.ws>
2017-11-05 22:10:46 -0600

    Attempting to fix plugin test timeout...

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -5,18 +5,29 @@ import (
 	"runtime"
 	"testing"
 
+	"fmt"
 	"github.com/containous/traefik/plugin/proto"
 	"github.com/hashicorp/go-plugin"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
 )
 
 func TestBasicPlugin(t *testing.T) {
+	ex, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
+	fmt.Println(exPath)
+
 	client := plugin.NewClient(&plugin.ClientConfig{
 		HandshakeConfig:  RemoteHandshake,
 		Plugins:          RemotePluginMap,
-		Cmd:              exec.Command("sh", "-c", "./test/plugin-test-go-grpc_"+runtime.GOOS+"-"+runtime.GOARCH),
+		Cmd:              exec.Command("sh", "-c", exPath+"/plugin/test/plugin-test-go-grpc_"+runtime.GOOS+"-"+runtime.GOARCH),
 		AllowedProtocols: []plugin.Protocol{plugin.ProtocolNetRPC, plugin.ProtocolGRPC},
 	})
+	defer client.Kill()
 
 	assert.NotNil(t, client)
 
@@ -49,6 +60,4 @@ func TestBasicPlugin(t *testing.T) {
 
 	assert.Equal(t, int32(200), resp.Response.StatusCode)
 	assert.Equal(t, []byte("test"), resp.Response.Body)
-
-	client.Kill()
 }
