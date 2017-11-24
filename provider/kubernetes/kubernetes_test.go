6fa649161e14bf56956e7f0f955cd0ad440b15db
Kwok-kuen Cheung <me@cheungpat.com>
2017-11-24 14:14:50 +0800

    Fix kubernetes path prefix rule with rewrite-target
    When rewrite-target is specified, any existing PathPrefix rule is
    removed. Hence an ingress with rewrite-target is always matched
    for the same host regardless of path prefix.

--- provider/kubernetes/kubernetes_test.go
+++ provider/kubernetes/kubernetes_test.go
@@ -1728,7 +1728,7 @@ func TestIngressAnnotations(t *testing.T) {
 				PassHostHeader: true,
 				Routes: map[string]types.Route{
 					"/api": {
-						Rule: "ReplacePath:/",
+						Rule: "PathPrefix:/api;ReplacePath:/",
 					},
 					"rewrite": {
 						Rule: "Host:rewrite",
