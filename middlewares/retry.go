be8450ef18f6947474a3191df1248190cac21082
Julien Salleyron <julien.salleyron@gmail.com>
2018-01-02 15:07:24 +0100

    We need to flush the end of the body when retry is streamed

--- middlewares/retry.go
+++ middlewares/retry.go
@@ -56,6 +56,7 @@ func (retry *Retry) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		// It's a stream request and the body gets already sent to the client.
 		// Therefore we should not send the response a second time.
 		if recorder.streamingResponseStarted {
+			recorder.Flush()
 			break
 		}
 
