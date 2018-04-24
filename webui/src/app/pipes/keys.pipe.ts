f46891ca2c500e7726cc2a67e9a84406a378e5f3
Fernandez Ludovic <ludovic@containo.us>
2018-04-24 14:11:02 +0200

    review: minor fixes.

--- webui/src/app/pipes/keys.pipe.ts
+++ webui/src/app/pipes/keys.pipe.ts
@@ -3,7 +3,6 @@ import { PipeTransform, Pipe } from '@angular/core';
 @Pipe({ name: 'keys' })
 export class KeysPipe implements PipeTransform {
   transform(value, args: string[]): any {
-    const keys = Object.keys(value);
-    return keys;
+    return Object.keys(value);
   }
 }
--- webui/src/app/services/window.service.ts
+++ webui/src/app/services/window.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { EventManager } from '@angular/platform-browser';
-import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 
 @Injectable()
