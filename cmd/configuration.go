9b94d6e35b6a86022b73b6580e32d49b737066cf
Marco Jantke <marco.jantke@gmail.com>
2018-05-08 13:42:38 +0200

    use sane default value for Marathon dial timeout
    A dial timeout of 60 seconds is overly excessive for a DNS request and
    probably as good as no timeout at all. Therefore, I changed the default
    value to 5 seconds as this is more reasonable.

--- cmd/configuration.go
+++ cmd/configuration.go
@@ -88,7 +88,7 @@ func NewTraefikDefaultPointersConfiguration() *TraefikConfiguration {
 	defaultMarathon.Endpoint = "http://127.0.0.1:8080"
 	defaultMarathon.ExposedByDefault = true
 	defaultMarathon.Constraints = types.Constraints{}
-	defaultMarathon.DialerTimeout = flaeg.Duration(60 * time.Second)
+	defaultMarathon.DialerTimeout = flaeg.Duration(5 * time.Second)
 	defaultMarathon.KeepAlive = flaeg.Duration(10 * time.Second)
 
 	// default Consul
