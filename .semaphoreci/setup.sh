5b3bba8f6e0f4ce3a925f42c0817071d4fdd8edd
Ludovic Fernandez <ldez@users.noreply.github.com>
2018-05-23 16:12:05 +0200

    Add missing quotes.

--- .semaphoreci/setup.sh
+++ .semaphoreci/setup.sh
@@ -7,10 +7,10 @@ source .semaphoreci/vars
 
 if [ -z "${PULL_REQUEST_NUMBER}" ]; then SHOULD_TEST="-*-"; else TEMP_STORAGE=$(curl --silent https://patch-diff.githubusercontent.com/raw/containous/traefik/pull/${PULL_REQUEST_NUMBER}.diff | patch --dry-run -p1 -R); fi
 
-if [ -n "$TEMP_STORAGE" ]; then SHOULD_TEST=$(echo $TEMP_STORAGE | grep -Ev '(.md|.yaml|.yml)' || :); fi
+if [ -n "$TEMP_STORAGE" ]; then SHOULD_TEST=$(echo "$TEMP_STORAGE" | grep -Ev '(.md|.yaml|.yml)' || :); fi
 
 if [ -n "$SHOULD_TEST" ]; then sudo -E apt-get -yq update; fi
 
 if [ -n "$SHOULD_TEST" ]; then sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install docker-ce=${DOCKER_VERSION}*; fi
 
-if [ -n "$SHOULD_TEST" ]; then   docker version; fi
+if [ -n "$SHOULD_TEST" ]; then  docker version; fi
