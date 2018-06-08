e8df50f54d85cfd1bacf0107cfce8db77edbb8fb
Fernandez Ludovic <ludovic@containo.us>
2018-06-08 20:31:25 +0200

    refactor: rename lb to next.

--- middlewares/empty_backend_handler.go
+++ middlewares/empty_backend_handler.go
@@ -10,21 +10,21 @@ import (
 // has at least one active Server in respect to the healthchecks and if this
 // is not the case, it will stop the middleware chain and respond with 503.
 type EmptyBackendHandler struct {
-	lb healthcheck.BalancerHandler
+	next healthcheck.BalancerHandler
 }
 
 // NewEmptyBackendHandler creates a new EmptyBackendHandler instance.
 func NewEmptyBackendHandler(lb healthcheck.BalancerHandler) *EmptyBackendHandler {
-	return &EmptyBackendHandler{lb: lb}
+	return &EmptyBackendHandler{next: lb}
 }
 
 // ServeHTTP responds with 503 when there is no active Server and otherwise
 // invokes the next handler in the middleware chain.
 func (h *EmptyBackendHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if len(h.lb.Servers()) == 0 {
+	if len(h.next.Servers()) == 0 {
 		rw.WriteHeader(http.StatusServiceUnavailable)
 		rw.Write([]byte(http.StatusText(http.StatusServiceUnavailable)))
 	} else {
-		h.lb.ServeHTTP(rw, r)
+		h.next.ServeHTTP(rw, r)
 	}
 }
