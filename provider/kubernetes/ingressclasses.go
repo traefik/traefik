a7e5a3b7b88a45252dd50aad9c715dbd40f39648
Rene Treffer <rene.treffer@soundcloud.com>
2018-10-10 15:12:03 +0200

    Improve formatting of kubernetes

--- provider/kubernetes/ingressclasses.go
+++ provider/kubernetes/ingressclasses.go
@@ -1,7 +1,7 @@
 package kubernetes
 
 import (
-       "fmt"
+	"fmt"
 )
 
 // IngressClasses holds kubernetes ingressClass annotation values
@@ -10,8 +10,8 @@ type IngressClasses []string
 //Set adds strings elem into the the parser
 //it splits str on , and ;
 func (ic *IngressClasses) Set(str string) error {
-       *ic = append(*ic, str)
-       return nil
+	*ic = append(*ic, str)
+	return nil
 }
 
 //Get []string
@@ -22,5 +22,5 @@ func (ic *IngressClasses) String() string { return fmt.Sprintf("%v", *ic) }
 
 //SetValue sets []string into the parser
 func (ic *IngressClasses) SetValue(val interface{}) {
-       *ic = val.(IngressClasses)
+	*ic = val.(IngressClasses)
 }
