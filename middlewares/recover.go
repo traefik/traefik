09f412f3b473140860bfda395d7f8636b5c12f96
Fernandez Ludovic <ludovic@containo.us>
2018-10-12 14:12:28 +0200

    refactor: change messages.

--- middlewares/recover.go
+++ middlewares/recover.go
@@ -29,11 +29,11 @@ func NegroniRecoverHandler() negroni.Handler {
 func recoverFunc(w http.ResponseWriter, r *http.Request) {
 	if err := recover(); err != nil {
 		if !shouldLogPanic(err) {
-			log.Debugf("HTTP handler has been aborted [%s %s]: %v", r.RemoteAddr, r.URL, err)
+			log.Debugf("Request has been aborted [%s - %s]: %v", r.RemoteAddr, r.URL, err)
 			return
 		}
 
-		log.Errorf("Recovered from panic in http handler [%s %s]: %+v", r.RemoteAddr, r.URL, err)
+		log.Errorf("Recovered from panic in HTTP handler [%s - %s]: %+v", r.RemoteAddr, r.URL, err)
 
 		const size = 64 << 10
 		buf := make([]byte, size)
@@ -44,6 +44,7 @@ func recoverFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// https://github.com/golang/go/blob/a0d6420d8be2ae7164797051ec74fa2a2df466a1/src/net/http/server.go#L1761-L1775
 // https://github.com/golang/go/blob/c33153f7b416c03983324b3e8f869ce1116d84bc/src/net/http/httputil/reverseproxy.go#L284
 func shouldLogPanic(panicValue interface{}) bool {
 	return panicValue != nil && panicValue != http.ErrAbortHandler
