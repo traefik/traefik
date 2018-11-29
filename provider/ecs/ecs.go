1bee942346a5b425bf56a257773de0e34b9387c1
hwhelan <hwhelan@carbonblack.com>
2018-11-29 12:33:40 -0500

    filter ECS tasks by LastStatus before adding to list of service tasks

--- provider/ecs/ecs.go
+++ provider/ecs/ecs.go
@@ -240,7 +240,9 @@ func (p *Provider) listInstances(ctx context.Context, client *awsClient) ([]ecsI
 					log.Errorf("Unable to describe tasks for %v", page.TaskArns)
 				} else {
 					for _, t := range resp.Tasks {
-						tasks[aws.StringValue(t.TaskArn)] = t
+						lastStatus = aws.StringValue(t.LastStatus):
+                        if t.LastStatus == ecs.DesiredStatusRunning:
+						    tasks[aws.StringValue(t.TaskArn)] = t
 					}
 				}
 			}
