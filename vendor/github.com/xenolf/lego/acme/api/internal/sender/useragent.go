e3fc9323116313b91731cc2f8da2f57d66446652
Fernandez Ludovic <ludovic@containo.us>
2018-12-10 16:33:59 -0800

    chore: update lego.

--- vendor/github.com/xenolf/lego/acme/api/internal/sender/useragent.go
+++ vendor/github.com/xenolf/lego/acme/api/internal/sender/useragent.go
@@ -1,8 +1,11 @@
 package sender
 
+// CODE GENERATED AUTOMATICALLY
+// THIS FILE MUST NOT BE EDITED BY HAND
+
 const (
 	// ourUserAgent is the User-Agent of this underlying library package.
-	ourUserAgent = "xenolf-acme"
+	ourUserAgent = "xenolf-acme/1.2.1"
 
 	// ourUserAgentComment is part of the UA comment linked to the version status of this underlying library package.
 	// values: detach|release
