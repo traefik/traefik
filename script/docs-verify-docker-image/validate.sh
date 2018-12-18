cecd61654eed7c79b25968ffc73277c1ca992ddc
Damien DUPORTAL <damien.duportal@gmail.com>
2018-12-18 11:58:00 +0100

    Docs Verify: ignore google group anchors with special chars because of https://github.com/gjtorikian/html-proofer/issues/482
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- script/docs-verify-docker-image/validate.sh
+++ script/docs-verify-docker-image/validate.sh
@@ -20,7 +20,7 @@ find "${PATH_TO_SITE}" -type f -not -path "/app/site/theme/*" \
   --check-html \
   --check_external_hash \
   --alt_ignore="/traefik.logo.png/" \
-  --url-ignore "/localhost:/,/127.0.0.1:/,/fonts.gstatic.com/,/.minikube/,/github.com\/containous\/traefik\/*edit*/,/github.com\/containous\/traefik\/$/" \
+  --url-ignore "/https://groups.google.com/a/traefik.io/forum/#!forum/security/,/localhost:/,/127.0.0.1:/,/fonts.gstatic.com/,/.minikube/,/github.com\/containous\/traefik\/*edit*/,/github.com\/containous\/traefik\/$/" \
   '{}'
 ## HTML-proofer options at https://github.com/gjtorikian/html-proofer#configuration
 
