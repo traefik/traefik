ab80c62e368d836dd17df43b9a623d0ca42414f8
Fernandez Ludovic <ludovic@containo.us>
2019-02-07 15:29:57 +0100

    review: fix import

--- provider/kubernetes/kubernetes.go
+++ provider/kubernetes/kubernetes.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/cenk/backoff"
+	"github.com/cenkalti/backoff"
 	"github.com/containous/traefik/config"
 	"github.com/containous/traefik/job"
 	"github.com/containous/traefik/log"
