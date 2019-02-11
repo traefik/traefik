d0afd1560c0d6fcee00c7063c0a84c8abf571d47
Damien DUPORTAL <damien.duportal@gmail.com>
2019-02-11 18:09:45 +0100

    Docs: switch mkdocs to alpine 3.9 and python 3
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>
    
    Docs: Netlify debug
    
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>
    
    Docs: Netlify debug
    
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>
    
    Docs: Netlify debug
    
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- docs/docs.Dockerfile
+++ docs/docs.Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.7
+FROM alpine:3.9
 
 ENV PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/.local/bin
 
@@ -6,8 +6,5 @@ COPY requirements.txt /mkdocs/
 WORKDIR /mkdocs
 VOLUME /mkdocs
 
-RUN apk --update upgrade \
-&& apk --no-cache --no-progress add py-pip \
-&& rm -rf /var/cache/apk/* \
-&& pip install --upgrade pip \
-&& pip install --user -r requirements.txt
+RUN apk --no-cache --no-progress add py3-pip \
+  && pip3 install --user -r requirements.txt
--- docs/scripts/netlify-run.sh
+++ docs/scripts/netlify-run.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 #
 # This script is run in netlify environment to build and validate
 # the website for documentation
 
-set -eu
+# set -eux -o pipefail
 
-CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
+# CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
+# pushd "${CURRENT_DIR}"
 
-#### Prepare environment
-pip install -r "${CURRENT_DIR}/requirements.txt"
+# #### Prepare environment
+# pip install --upgrade pip
+# pip install -r ./requirements.txt
 
-#### Build website
-# Provide the URL for this deployment to Mkdocs
-echo "${DEPLOY_PRIME_URL}" > "${CURRENT_DIR}/../CNAME"
-sed -i "s#site_url:.*#site_url: ${DEPLOY_PRIME_URL}#" "${CURRENT_DIR}/../mkdocs.yml"
+# #### Build website
+# # Provide the URL for this deployment to Mkdocs
+# echo "${DEPLOY_PRIME_URL}" > "${CURRENT_DIR}/../CNAME"
+# sed -i "s#site_url:.*#site_url: ${DEPLOY_PRIME_URL}#" "${CURRENT_DIR}/../mkdocs.yml"
 
 # Build
 mkdocs build
