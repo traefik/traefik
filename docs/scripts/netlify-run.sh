55dbcb9685531bc5840bea661ec5a49b1d741572
Damien DUPORTAL <damien.duportal@gmail.com>
2019-02-11 20:07:06 +0100

    Docs: fix Travis + debug netlify
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>
    
    Docs: debug Netlify
    
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- docs/scripts/netlify-run.sh
+++ docs/scripts/netlify-run.sh
@@ -3,19 +3,12 @@
 # This script is run in netlify environment to build and validate
 # the website for documentation
 
-# set -eux -o pipefail
+CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
 
-# CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
-# pushd "${CURRENT_DIR}"
-
-# #### Prepare environment
-# pip install --upgrade pip
-# pip install -r ./requirements.txt
-
-# #### Build website
-# # Provide the URL for this deployment to Mkdocs
-# echo "${DEPLOY_PRIME_URL}" > "${CURRENT_DIR}/../CNAME"
-# sed -i "s#site_url:.*#site_url: ${DEPLOY_PRIME_URL}#" "${CURRENT_DIR}/../mkdocs.yml"
+#### Build website
+# Provide the URL for this deployment to Mkdocs
+echo "${DEPLOY_PRIME_URL}" > "${CURRENT_DIR}/../CNAME"
+sed -i "s#site_url:.*#site_url: ${DEPLOY_PRIME_URL}#" "${CURRENT_DIR}/../mkdocs.yml"
 
 # Build
 mkdocs build
