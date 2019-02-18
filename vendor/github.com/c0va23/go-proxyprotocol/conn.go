70a8343e978254d7fbabf9da9354a3eb5253d7d9
Dmitrij Fedorenko <job@fedorenko-d.ru>
2019-02-18 23:35:19 +0300

    Add support proxyprotocol v2

---
+++ vendor/github.com/c0va23/go-proxyprotocol/conn.go
@@ -0,0 +1,119 @@
+package proxyprotocol
+
+import (
+	"bufio"
+	"net"
+	"sync"
+	"time"
+)
+
+/*
+Conn is wrapper on net.Conn with overrided RemoteAddr().
+
+On first call Read() or RemoteAddr() parse proxyprotocol header and store
+local and remote addresses.
+*/
+type Conn struct {
+	conn         net.Conn
+	logger       Logger
+	readBuf      *bufio.Reader
+	header       *Header
+	headerErr    error
+	headerParser HeaderParser
+	trustedAddr  bool
+	once         sync.Once
+}
+
+// NewConn create wrapper on net.Conn.
+func NewConn(
+	conn net.Conn,
+	logger Logger,
+	headerParser HeaderParser,
+	trustedAddr bool,
+) net.Conn {
+	readBuf := bufio.NewReaderSize(conn, bufferSize)
+
+	return &Conn{
+		conn:         conn,
+		readBuf:      readBuf,
+		logger:       logger,
+		headerParser: headerParser,
+		trustedAddr:  trustedAddr,
+	}
+}
+
+func (conn *Conn) parseHeader() {
+	conn.header, conn.headerErr = conn.headerParser.Parse(conn.readBuf)
+	if conn.headerErr != nil {
+		conn.logger.Printf("Header parse error: %s", conn.headerErr)
+		return
+	}
+	conn.logger.Printf("Header parsed %v", conn.header)
+}
+
+/*
+Read on first call parse proxyprotocol header.
+
+If header parser return error, then error stored and returned. Otherwise call
+Read on source connection.
+
+Following calls of Read function check parse header error.
+If error not nil, then error returned. Otherwise called source "conn.Read".
+*/
+func (conn *Conn) Read(buf []byte) (int, error) {
+	conn.once.Do(conn.parseHeader)
+
+	if nil != conn.headerErr {
+		return 0, conn.headerErr
+	}
+
+	return conn.readBuf.Read(buf)
+}
+
+// Write proxy to conn.Write
+func (conn *Conn) Write(buf []byte) (int, error) {
+	return conn.conn.Write(buf)
+}
+
+// Close proxy to conn.Close
+func (conn *Conn) Close() error {
+	return conn.conn.Close()
+}
+
+// LocalAddr proxy to conn.LocalAddr
+func (conn *Conn) LocalAddr() net.Addr {
+	conn.once.Do(conn.parseHeader)
+	if conn.trustedAddr && nil != conn.header {
+		return conn.header.DstAddr
+	}
+	return conn.conn.LocalAddr()
+}
+
+/*
+RemoteAddr on first call parse proxyprotocol header.
+
+If header parser return header, then return source address from header.
+Otherwise return original source address.
+*/
+func (conn *Conn) RemoteAddr() net.Addr {
+	conn.once.Do(conn.parseHeader)
+	if conn.trustedAddr && nil != conn.header {
+		return conn.header.SrcAddr
+	}
+	return conn.conn.RemoteAddr()
+}
+
+// SetDeadline proxy to conn.SetDeadline
+func (conn *Conn) SetDeadline(t time.Time) error {
+	return conn.conn.SetDeadline(t)
+}
+
+// SetReadDeadline proxy to conn.SetReadDeadline
+func (conn *Conn) SetReadDeadline(t time.Time) error {
+	return conn.conn.SetReadDeadline(t)
+}
+
+// SetWriteDeadline  proxy to conn.SetWriteDeadline
+func (conn *Conn) SetWriteDeadline(t time.Time) error {
+	return conn.conn.SetWriteDeadline(t)
+}
---
+++ vendor/github.com/c0va23/go-proxyprotocol/proxyprotocol.go
@@ -0,0 +1,38 @@
+/*
+Package proxyprotocol implement receiver for HA ProxyProtocol V1 and V2.
+
+Prorxyprotocol spec http://www.haproxy.org/download/2.0/doc/proxy-protocol.txt
+
+This package provides a wrapper for the interface net.Listener, which extracts
+remote and local address of the connection from the headers in the format
+HA proxyprotocol.
+*/
+package proxyprotocol
+
+import (
+	"bufio"
+	"errors"
+	"net"
+)
+
+// Header struct represent header parsing result
+type Header struct {
+	SrcAddr net.Addr
+	DstAddr net.Addr
+}
+
+// HeaderParserBuilder build HeaderParser's
+type HeaderParserBuilder interface {
+	Build(Logger) HeaderParser
+}
+
+// HeaderParser describe interface for header parsers
+type HeaderParser interface {
+	Parse(readBuf *bufio.Reader) (*Header, error)
+}
+
+// Shared HeaderParser errors
+var (
+	ErrInvalidSignature = errors.New("invalid signature")
+	ErrUnknownProtocol  = errors.New("unknown protocol")
+)
