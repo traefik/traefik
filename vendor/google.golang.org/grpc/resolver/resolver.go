ace265e42ff29bec37425354cfff1681db541a3a
Alex Antonov <alex@antonov.ws>
2019-03-05 11:47:40 -0500

    Upgraded dependencies for gRPC and Protobuf

--- vendor/google.golang.org/grpc/resolver/resolver.go
+++ vendor/google.golang.org/grpc/resolver/resolver.go
@@ -49,8 +49,12 @@ func Get(scheme string) Builder {
 	return nil
 }
 
-// SetDefaultScheme sets the default scheme that will be used.
-// The default default scheme is "passthrough".
+// SetDefaultScheme sets the default scheme that will be used. The default
+// default scheme is "passthrough".
+//
+// NOTE: this function must only be called during initialization time (i.e. in
+// an init() function), and is not thread-safe. The scheme set last overrides
+// previously set values.
 func SetDefaultScheme(scheme string) {
 	defaultScheme = scheme
 }
