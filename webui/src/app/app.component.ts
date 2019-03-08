2d54065082043ca7feb69a8d4084d7265eaa7f89
Antoine CARON <Slashgear@users.noreply.github.com>
2019-03-08 14:08:03 +0100

    feat(webui): migrate to a work in progress webui

--- webui/src/app/app.component.ts
+++ webui/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-header></app-header>
-    <router-outlet></router-outlet>
+    <main class="wip">
+      <img src="./assets/images/traefik.logo.svg" alt="logo" />
+      <header>
+        <h1 class="title">Work in progress...</h1>
+      </header>
+    </main>
   `
 })
 export class AppComponent {}
--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -3,46 +3,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
-import { LineChartComponent } from './charts/line-chart/line-chart.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HealthComponent } from './components/health/health.component';
-import { ProvidersComponent } from './components/providers/providers.component';
-import { LetDirective } from './directives/let.directive';
-import { BackendFilterPipe } from './pipes/backend.filter.pipe';
-import { FrontendFilterPipe } from './pipes/frontend.filter.pipe';
-import { HumanReadableFilterPipe } from './pipes/humanreadable.filter.pipe';
-import { KeysPipe } from './pipes/keys.pipe';
-import { ApiService } from './services/api.service';
-import { WindowService } from './services/window.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ProvidersComponent,
-    HealthComponent,
-    LineChartComponent,
-    BarChartComponent,
-    KeysPipe,
-    FrontendFilterPipe,
-    BackendFilterPipe,
-    HumanReadableFilterPipe,
-    LetDirective
-  ],
-  imports: [
-    BrowserModule,
-    CommonModule,
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProvidersComponent, pathMatch: 'full' },
-      { path: 'status', component: HealthComponent }
-    ])
-  ],
-  providers: [ApiService, WindowService],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CommonModule, HttpClientModule, FormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
