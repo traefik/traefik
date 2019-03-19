c8395d73277c87b0ec330fc54f9d72a3cffd2d8a
Manuel Zapf <me@manuelzapf.io>
2019-03-19 16:52:32 +0100

    linting..

--- webui/src/app/app.component.ts
+++ webui/src/app/app.component.ts
@@ -7,19 +7,22 @@ import { DOCUMENT } from '@angular/common';
     <main class="wip">
       <img src="./assets/images/traefik.logo.svg" alt="logo" />
       <header>
-       
         <h1 class="title">
           <i class="fa fa-exclamation-triangle"></i>
           Work in progress...
         </h1>
         <p>
-          In the meantime, you can review your current configuration by using the
+          In the meantime, you can review your current configuration by using
+          the
           <a href="{{ href }}/api/rawdata">{{ href }}/api/rawdata</a> endpoint
-          <br><br>
-          Also, please keep your <i class="fa fa-eye"></i> on our <a href="https://docs.traefik.io/v2.0/operations/dashboard/">documentation</a> to stay informed 
+          <br /><br />
+          Also, please keep your <i class="fa fa-eye"></i> on our
+          <a href="https://docs.traefik.io/v2.0/operations/dashboard/"
+            >documentation</a
+          >
+          to stay informed
         </p>
-        <p>
-        </p>
+        <p></p>
       </header>
     </main>
   `
