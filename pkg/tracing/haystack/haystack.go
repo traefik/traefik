e3826f5ed5aec65073357c1bfb0688fc82887e16
Alex Antonov <alex@antonov.ws>
2019-05-03 13:20:14 -0500

    Extracted Haystack Logger to a separate file

--- pkg/tracing/haystack/haystack.go
+++ pkg/tracing/haystack/haystack.go
@@ -2,11 +2,10 @@ package haystack
 
 import (
 	"io"
-	"strconv"
 	"strings"
 	"time"
 
-	haystack "github.com/ExpediaDotCom/haystack-client-go"
+	"github.com/ExpediaDotCom/haystack-client-go"
 	"github.com/containous/traefik/pkg/log"
 	"github.com/opentracing/opentracing-go"
 )
@@ -14,14 +13,15 @@ import (
 // Name sets the name of this tracer
 const Name = "haystack"
 
-// Config provides configuration settings for a datadog tracer
+// Config provides configuration settings for a haystack tracer
 type Config struct {
-	LocalAgentHostPort     string `description:"Set datadog-agent's host:port that the reporter will used. Defaults to localhost:35000" export:"false"`
-	GlobalTag              string `description:"Key:Value tag to be set on all the spans." export:"true"`
-	TraceIDHeaderName      string `description:"Specifies the header name that will be used to store the trace ID.." export:"true"`
-	ParentIDHeaderName     string `description:"Specifies the header name that will be used to store the parent ID." export:"true"`
-	SpanIDHeaderName       string `description:"Specifies the header name that will be used to store the span ID." export:"true"`
-	BagagePrefixHeaderName string `description:"specifies the header name prefix that will be used to store baggage items in a map." export:"true"`
+	LocalAgentHost          string `description:"Set haystack-agent's host that the reporter will used. Defaults to localhost" export:"false"`
+	LocalAgentPort          int    `description:"Set haystack-agent's port that the reporter will used. Defaults to 35000" export:"false"`
+	GlobalTag               string `description:"Key:Value tag to be set on all the spans." export:"true"`
+	TraceIDHeaderName       string `description:"Specifies the header name that will be used to store the trace ID.." export:"true"`
+	ParentIDHeaderName      string `description:"Specifies the header name that will be used to store the parent ID." export:"true"`
+	SpanIDHeaderName        string `description:"Specifies the header name that will be used to store the span ID." export:"true"`
+	BaggagePrefixHeaderName string `description:"specifies the header name prefix that will be used to store baggage items in a map." export:"true"`
 }
 
 // Setup sets up the tracer
@@ -33,14 +33,13 @@ func (c *Config) Setup(serviceName string) (opentracing.Tracer, io.Closer, error
 		value = tag[1]
 	}
 
-	hostAndPort := strings.SplitN(c.LocalAgentHostPort, ":", 2)
 	host := "localhost"
 	port := 35000
-	if len(hostAndPort) > 0 {
-		host = hostAndPort[0]
-		if len(hostAndPort) == 2 {
-			port, _ = strconv.Atoi(hostAndPort[1])
-		}
+	if len(c.LocalAgentHost) > 0 {
+		host = c.LocalAgentHost
+	}
+	if c.LocalAgentPort > 0 {
+		port = c.LocalAgentPort
 	}
 
 	tracer, closer := haystack.NewTracer(serviceName, haystack.NewAgentDispatcher(host, port, 3*time.Second, 1000),
@@ -50,9 +49,9 @@ func (c *Config) Setup(serviceName string) (opentracing.Tracer, io.Closer, error
 				TraceIDKEYName:       c.TraceIDHeaderName,
 				ParentSpanIDKEYName:  c.ParentIDHeaderName,
 				SpanIDKEYName:        c.SpanIDHeaderName,
-				BaggagePrefixKEYName: c.BagagePrefixHeaderName,
+				BaggagePrefixKEYName: c.BaggagePrefixHeaderName,
 			}, haystack.DefaultCodex{})),
-		haystack.TracerOptionsFactory.Logger(&loggerWrapper{logger: log.WithoutContext()}),
+		haystack.TracerOptionsFactory.Logger(&haystackLogger{logger: log.WithoutContext()}),
 	)
 
 	// Without this, child spans are getting the NOOP tracer
@@ -61,24 +60,4 @@ func (c *Config) Setup(serviceName string) (opentracing.Tracer, io.Closer, error
 	log.WithoutContext().Debug("DataDog tracer configured")
 
 	return tracer, closer, nil
-}
-
-/*NullLogger does nothing*/
-type loggerWrapper struct {
-	logger log.Logger
-}
-
-/*Error prints the error message*/
-func (l loggerWrapper) Error(format string, v ...interface{}) {
-	l.logger.Errorf(format, v)
-}
-
-/*Info prints the info message*/
-func (l loggerWrapper) Info(format string, v ...interface{}) {
-	l.logger.Infof(format, v)
-}
-
-/*Debug prints the info message*/
-func (l loggerWrapper) Debug(format string, v ...interface{}) {
-	l.logger.Debug(format, v)
 }
