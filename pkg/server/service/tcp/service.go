36393601126273a0cc59583dca30103cb525bd1b
Fernandez Ludovic <ludovic@containo.us>
2019-05-09 10:11:21 +0200

    fix: context.

--- pkg/server/service/tcp/service.go
+++ pkg/server/service/tcp/service.go
@@ -25,18 +25,17 @@ func NewManager(configs map[string]*config.TCPService) *Manager {
 
 // BuildTCP Creates a tcp.Handler for a service configuration.
 func (m *Manager) BuildTCP(rootCtx context.Context, serviceName string) (tcp.Handler, error) {
-	ctx := log.With(rootCtx, log.Str(log.ServiceName, serviceName))
+	serviceQualifiedName := internal.GetQualifiedName(rootCtx, serviceName)
+	ctx := internal.AddProviderInContext(rootCtx, serviceQualifiedName)
+	ctx = log.With(ctx, log.Str(log.ServiceName, serviceName))
 
-	serviceName = internal.GetQualifiedName(ctx, serviceName)
-	ctx = internal.AddProviderInContext(ctx, serviceName)
-
-	conf, ok := m.configs[serviceName]
+	conf, ok := m.configs[serviceQualifiedName]
 	if !ok {
-		return nil, fmt.Errorf("the service %q does not exits", serviceName)
+		return nil, fmt.Errorf("the service %q does not exits", serviceQualifiedName)
 	}
 
 	if conf.LoadBalancer == nil {
-		return nil, fmt.Errorf("the service %q doesn't have any TCP load balancer", serviceName)
+		return nil, fmt.Errorf("the service %q doesn't have any TCP load balancer", serviceQualifiedName)
 	}
 
 	logger := log.FromContext(ctx)
@@ -46,13 +45,13 @@ func (m *Manager) BuildTCP(rootCtx context.Context, serviceName string) (tcp.Han
 
 	for _, server := range conf.LoadBalancer.Servers {
 		if _, err := parseIP(server.Address); err != nil {
-			logger.Errorf("Invalid IP address for a %q server %q: %v", serviceName, server.Address, err)
+			logger.Errorf("Invalid IP address for a %q server %q: %v", serviceQualifiedName, server.Address, err)
 			continue
 		}
 
 		handler, err := tcp.NewProxy(server.Address)
 		if err != nil {
-			logger.Errorf("In service %q server %q: %v", serviceName, server.Address, err)
+			logger.Errorf("In service %q server %q: %v", serviceQualifiedName, server.Address, err)
 			continue
 		}
 
