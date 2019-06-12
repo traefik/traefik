eb7389a587a0e31c1a8f008d8c2ebc2baae49df0
mpl <mathieu.lonjaret@gmail.com>
2019-06-12 18:43:13 +0200

    API: new contract

---
+++ docs/content/operations/api.md
@@ -0,0 +1,153 @@
+# API
+
+Traefik exposes a number of information through an API handler, such as the configuration of all routers, services, middlewares, etc.
+
+As with all features of Traefik, this handler can be enabled with the [static configuration](../getting-started/configuration-overview.md#the-static-configuration).
+
+## Security
+
+Enabling the API in production is not recommended, because it will expose all configuration elements,
+including sensitive data.
+
+In production, it should be at least secured by authentication and authorizations.
+
+A good sane default (non exhaustive) set of recommendations
+would be to apply the following protection mechanisms:
+
+* At the application level:  
+  securing with middlewares such as [basic authentication](../middlewares/basicauth.md) or [white listing](../middlewares/ipwhitelist.md).
+
+* At the transport level:  
+  NOT publicly exposing the API's port,
+  keeping it restricted to internal networks
+  (as in the [principle of least privilege](https://en.wikipedia.org/wiki/Principle_of_least_privilege), applied to networks).
+
+## Configuration
+
+To enable the API handler:
+
+```toml tab="File"
+[api]
+```
+
+```bash tab="CLI"
+--api
+```
+
+### `dashboard`
+
+_Optional, Default=false_
+
+Enable the dashboard. More about the dashboard features [here](./dashboard.md).
+
+```toml tab="File"
+[api]
+  dashboard = true
+```
+
+```bash tab="CLI"
+--api.dashboard
+```
+
+### `entrypoint`
+
+_Optional, Default="traefik"_
+
+The entry point that the API handler will be bound to.
+The default ("traefik") is an internal entry point (which is always defined).
+
+```toml tab="File"
+[api]
+  entrypoint = "web"
+```
+
+```bash tab="CLI"
+--api.entrypoint="web"
+```
+
+### `middlewares`
+
+_Optional, Default=empty_
+
+The list of [middlewares](../middlewares/overview.md) applied to the API handler.
+
+```toml tab="File"
+[api]
+  middlewares = ["api-auth", "api-prefix"]
+```
+
+```bash tab="CLI"
+--api.middlewares="api-auth,api-prefix"
+```
+
+## Endpoints
+
+All the following endpoints must be accessed with a `GET` HTTP request.
+
+| Path                           | Description                                                                               |
+|--------------------------------|-------------------------------------------------------------------------------------------|
+| `/api/http/routers`            | Lists all the HTTP routers information.                                                   |
+| `/api/http/routers/{name}`     | Returns the information of the HTTP router specified by `name`.                           |
+| `/api/http/services`           | Lists all the HTTP services information.                                                  |
+| `/api/http/services/{name}`    | Returns the information of the HTTP service specified by `name`.                          |
+| `/api/http/middlewares`        | Lists all the HTTP middlewares information.                                               |
+| `/api/http/middlewares/{name}` | Returns the information of the HTTP middleware specified by `name`.                       |
+| `/api/tcp/routers`             | Lists all the TCP routers information.                                                    |
+| `/api/tcp/routers/{name}`      | Returns the information of the TCP router specified by `name`.                            |
+| `/api/tcp/services`            | Lists all the TCP services information.                                                   |
+| `/api/tcp/services/{name}`     | Returns the information of the TCP service specified by `name`.                           |
+| `/api/version`                 | Returns information about Traefik version.                                                |
+| `/debug/vars`                  | See the [expvar](https://golang.org/pkg/expvar/) Go documentation.                        |
+| `/debug/pprof/`                | See the [pprof Index](https://golang.org/pkg/net/http/pprof/#Index) Go documentation.     |
+| `/debug/pprof/cmdline`         | See the [pprof Cmdline](https://golang.org/pkg/net/http/pprof/#Cmdline) Go documentation. |
+| `/debug/pprof/profile`         | See the [pprof Profile](https://golang.org/pkg/net/http/pprof/#Profile) Go documentation. |
+| `/debug/pprof/symbol`          | See the [pprof Symbol](https://golang.org/pkg/net/http/pprof/#Symbol) Go documentation.   |
+| `/debug/pprof/trace`           | See the [pprof Trace](https://golang.org/pkg/net/http/pprof/#Trace) Go documentation.     |
+
+## Common Configuration Use Cases
+
+### Address / Port
+
+You can define a custom address/port like this:
+
+```toml
+[entryPoints]
+  [entryPoints.web]
+  address = ":80"
+
+  [entryPoints.foo]
+  address = ":8082"
+
+  [entryPoints.bar]
+  address = ":8083"
+
+[ping]
+entryPoint = "foo"
+
+[api]
+entryPoint = "bar"
+```
+
+In the above example, you would access a service at /foo, an api endpoint, or the health-check as follows:
+
+* Service: `http://hostname:80/foo`
+* API: `http://hostname:8083/api/http/routers`
+* Ping URL: `http://hostname:8082/ping`
+
+### Authentication
+
+To restrict access to the API handler, one can add authentication with the [basic auth middleware](../middlewares/basicauth.md).
+
+```toml
+[api]
+  middlewares=["api-auth"]
+```
+
+```toml
+[http.middlewares]
+  [http.middlewares.api-auth.basicauth]
+  users = [
+    "test:$apr1$H6uskkkW$IgXLP6ewTrSuBkTrqE8wj/",
+    "test2:$apr1$d9hr9HBB$4HxwgUir3HP4EsggP/QNo0",
+  ]
+```
--- pkg/api/handler.go
+++ pkg/api/handler.go
@@ -1,8 +1,12 @@
 package api
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/containous/mux"
 	"github.com/containous/traefik/pkg/config"
@@ -11,14 +15,14 @@ import (
 	"github.com/containous/traefik/pkg/types"
 	"github.com/containous/traefik/pkg/version"
 	assetfs "github.com/elazarl/go-bindata-assetfs"
-	"github.com/unrolled/render"
 )
 
-var templateRenderer jsonRenderer = render.New(render.Options{Directory: "nowhere"})
+const (
+	defaultPerPage = 100
+	defaultPage    = 1
+)
 
-type jsonRenderer interface {
-	JSON(w io.Writer, status int, v interface{}) error
-}
+const nextPageHeader = "X-Next-Page"
 
 type serviceInfoRepresentation struct {
 	*config.ServiceInfo
@@ -32,6 +36,43 @@ type RunTimeRepresentation struct {
 	Services    map[string]*serviceInfoRepresentation `json:"services,omitempty"`
 	TCPRouters  map[string]*config.TCPRouterInfo      `json:"tcpRouters,omitempty"`
 	TCPServices map[string]*config.TCPServiceInfo     `json:"tcpServices,omitempty"`
+}
+
+type routerRepresentation struct {
+	*config.RouterInfo
+	Name     string `json:"name,omitempty"`
+	Provider string `json:"provider,omitempty"`
+}
+
+type serviceRepresentation struct {
+	*config.ServiceInfo
+	ServerStatus map[string]string `json:"serverStatus,omitempty"`
+	Name         string            `json:"name,omitempty"`
+	Provider     string            `json:"provider,omitempty"`
+}
+
+type middlewareRepresentation struct {
+	*config.MiddlewareInfo
+	Name     string `json:"name,omitempty"`
+	Provider string `json:"provider,omitempty"`
+}
+
+type tcpRouterRepresentation struct {
+	*config.TCPRouterInfo
+	Name     string `json:"name,omitempty"`
+	Provider string `json:"provider,omitempty"`
+}
+
+type tcpServiceRepresentation struct {
+	*config.TCPServiceInfo
+	Name     string `json:"name,omitempty"`
+	Provider string `json:"provider,omitempty"`
+}
+
+type pageInfo struct {
+	startIndex int
+	endIndex   int
+	nextPage   int
 }
 
 // Handler serves the configuration and status of Traefik on API endpoints.
@@ -71,14 +112,288 @@ func (h Handler) Append(router *mux.Router) {
 
 	router.Methods(http.MethodGet).Path("/api/rawdata").HandlerFunc(h.getRuntimeConfiguration)
 
+	router.Methods(http.MethodGet).Path("/api/http/routers").HandlerFunc(h.getRouters)
+	router.Methods(http.MethodGet).Path("/api/http/routers/{routerID}").HandlerFunc(h.getRouter)
+	router.Methods(http.MethodGet).Path("/api/http/services").HandlerFunc(h.getServices)
+	router.Methods(http.MethodGet).Path("/api/http/services/{serviceID}").HandlerFunc(h.getService)
+	router.Methods(http.MethodGet).Path("/api/http/middlewares").HandlerFunc(h.getMiddlewares)
+	router.Methods(http.MethodGet).Path("/api/http/middlewares/{middlewareID}").HandlerFunc(h.getMiddleware)
+
+	router.Methods(http.MethodGet).Path("/api/tcp/routers").HandlerFunc(h.getTCPRouters)
+	router.Methods(http.MethodGet).Path("/api/tcp/routers/{routerID}").HandlerFunc(h.getTCPRouter)
+	router.Methods(http.MethodGet).Path("/api/tcp/services").HandlerFunc(h.getTCPServices)
+	router.Methods(http.MethodGet).Path("/api/tcp/services/{serviceID}").HandlerFunc(h.getTCPService)
+
 	// FIXME stats
 	// health route
-	//router.Methods(http.MethodGet).Path("/health").HandlerFunc(p.getHealthHandler)
+	// router.Methods(http.MethodGet).Path("/health").HandlerFunc(p.getHealthHandler)
 
 	version.Handler{}.Append(router)
 
 	if h.dashboard {
 		DashboardHandler{Assets: h.dashboardAssets}.Append(router)
+	}
+}
+
+func (h Handler) getRouters(rw http.ResponseWriter, request *http.Request) {
+	results := make([]routerRepresentation, 0, len(h.runtimeConfiguration.Routers))
+
+	for name, rt := range h.runtimeConfiguration.Routers {
+		results = append(results, routerRepresentation{
+			RouterInfo: rt,
+			Name:       name,
+			Provider:   getProviderName(name),
+		})
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Name < results[j].Name
+	})
+
+	pageInfo, err := pagination(request, len(results))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rw.Header().Set(nextPageHeader, strconv.Itoa(pageInfo.nextPage))
+
+	err = json.NewEncoder(rw).Encode(results[pageInfo.startIndex:pageInfo.endIndex])
+	if err != nil {
+		log.FromContext(request.Context()).Error(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h Handler) getRouter(rw http.ResponseWriter, request *http.Request) {
+	routerID := mux.Vars(request)["routerID"]
+
+	router, ok := h.runtimeConfiguration.Routers[routerID]
+	if !ok {
+		http.NotFound(rw, request)
+		return
+	}
+
+	result := routerRepresentation{
+		RouterInfo: router,
+		Name:       routerID,
+		Provider:   getProviderName(routerID),
+	}
+
+	err := json.NewEncoder(rw).Encode(result)
+	if err != nil {
+		log.FromContext(request.Context()).Error(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h Handler) getServices(rw http.ResponseWriter, request *http.Request) {
+	results := make([]serviceRepresentation, 0, len(h.runtimeConfiguration.Services))
+
+	for name, si := range h.runtimeConfiguration.Services {
+		results = append(results, serviceRepresentation{
+			ServiceInfo:  si,
+			Name:         name,
+			Provider:     getProviderName(name),
+			ServerStatus: si.GetAllStatus(),
+		})
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Name < results[j].Name
+	})
+
+	pageInfo, err := pagination(request, len(results))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rw.Header().Set(nextPageHeader, strconv.Itoa(pageInfo.nextPage))
+
+	err = json.NewEncoder(rw).Encode(results[pageInfo.startIndex:pageInfo.endIndex])
+	if err != nil {
+		log.FromContext(request.Context()).Error(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h Handler) getService(rw http.ResponseWriter, request *http.Request) {
+	serviceID := mux.Vars(request)["serviceID"]
+
+	service, ok := h.runtimeConfiguration.Services[serviceID]
+	if !ok {
+		http.NotFound(rw, request)
+		return
+	}
+
+	result := serviceRepresentation{
+		ServiceInfo:  service,
+		Name:         serviceID,
+		Provider:     getProviderName(serviceID),
+		ServerStatus: service.GetAllStatus(),
+	}
+
+	err := json.NewEncoder(rw).Encode(result)
+	if err != nil {
+		log.FromContext(request.Context()).Error(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h Handler) getMiddlewares(rw http.ResponseWriter, request *http.Request) {
+	results := make([]middlewareRepresentation, 0, len(h.runtimeConfiguration.Middlewares))
+
+	for name, mi := range h.runtimeConfiguration.Middlewares {
+		results = append(results, middlewareRepresentation{
+			MiddlewareInfo: mi,
+			Name:           name,
+			Provider:       getProviderName(name),
+		})
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Name < results[j].Name
+	})
+
+	pageInfo, err := pagination(request, len(results))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rw.Header().Set(nextPageHeader, strconv.Itoa(pageInfo.nextPage))
+
+	err = json.NewEncoder(rw).Encode(results[pageInfo.startIndex:pageInfo.endIndex])
+	if err != nil {
+		log.FromContext(request.Context()).Error(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h Handler) getMiddleware(rw http.ResponseWriter, request *http.Request) {
+	middlewareID := mux.Vars(request)["middlewareID"]
+
+	middleware, ok := h.runtimeConfiguration.Middlewares[middlewareID]
+	if !ok {
+		http.NotFound(rw, request)
+		return
+	}
+
+	result := middlewareRepresentation{
+		MiddlewareInfo: middleware,
+		Name:           middlewareID,
+		Provider:       getProviderName(middlewareID),
+	}
+
+	err := json.NewEncoder(rw).Encode(result)
+	if err != nil {
+		log.FromContext(request.Context()).Error(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h Handler) getTCPRouters(rw http.ResponseWriter, request *http.Request) {
+	results := make([]tcpRouterRepresentation, 0, len(h.runtimeConfiguration.TCPRouters))
+
+	for name, rt := range h.runtimeConfiguration.TCPRouters {
+		results = append(results, tcpRouterRepresentation{
+			TCPRouterInfo: rt,
+			Name:          name,
+			Provider:      getProviderName(name),
+		})
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Name < results[j].Name
+	})
+
+	pageInfo, err := pagination(request, len(results))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rw.Header().Set(nextPageHeader, strconv.Itoa(pageInfo.nextPage))
+
+	err = json.NewEncoder(rw).Encode(results[pageInfo.startIndex:pageInfo.endIndex])
+	if err != nil {
+		log.FromContext(request.Context()).Error(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h Handler) getTCPRouter(rw http.ResponseWriter, request *http.Request) {
+	routerID := mux.Vars(request)["routerID"]
+
+	router, ok := h.runtimeConfiguration.TCPRouters[routerID]
+	if !ok {
+		http.NotFound(rw, request)
+		return
+	}
+
+	result := tcpRouterRepresentation{
+		TCPRouterInfo: router,
+		Name:          routerID,
+		Provider:      getProviderName(routerID),
+	}
+
+	err := json.NewEncoder(rw).Encode(result)
+	if err != nil {
+		log.FromContext(request.Context()).Error(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h Handler) getTCPServices(rw http.ResponseWriter, request *http.Request) {
+	results := make([]tcpServiceRepresentation, 0, len(h.runtimeConfiguration.TCPServices))
+
+	for name, si := range h.runtimeConfiguration.TCPServices {
+		results = append(results, tcpServiceRepresentation{
+			TCPServiceInfo: si,
+			Name:           name,
+			Provider:       getProviderName(name),
+		})
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Name < results[j].Name
+	})
+
+	pageInfo, err := pagination(request, len(results))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rw.Header().Set(nextPageHeader, strconv.Itoa(pageInfo.nextPage))
+
+	err = json.NewEncoder(rw).Encode(results[pageInfo.startIndex:pageInfo.endIndex])
+	if err != nil {
+		log.FromContext(request.Context()).Error(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h Handler) getTCPService(rw http.ResponseWriter, request *http.Request) {
+	serviceID := mux.Vars(request)["serviceID"]
+
+	service, ok := h.runtimeConfiguration.TCPServices[serviceID]
+	if !ok {
+		http.NotFound(rw, request)
+		return
+	}
+
+	result := tcpServiceRepresentation{
+		TCPServiceInfo: service,
+		Name:           serviceID,
+		Provider:       getProviderName(serviceID),
+	}
+
+	err := json.NewEncoder(rw).Encode(result)
+	if err != nil {
+		log.FromContext(request.Context()).Error(err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
 }
 
@@ -91,7 +406,7 @@ func (h Handler) getRuntimeConfiguration(rw http.ResponseWriter, request *http.R
 		}
 	}
 
-	rtRepr := RunTimeRepresentation{
+	result := RunTimeRepresentation{
 		Routers:     h.runtimeConfiguration.Routers,
 		Middlewares: h.runtimeConfiguration.Middlewares,
 		Services:    siRepr,
@@ -99,9 +414,55 @@ func (h Handler) getRuntimeConfiguration(rw http.ResponseWriter, request *http.R
 		TCPServices: h.runtimeConfiguration.TCPServices,
 	}
 
-	err := templateRenderer.JSON(rw, http.StatusOK, rtRepr)
+	err := json.NewEncoder(rw).Encode(result)
 	if err != nil {
 		log.FromContext(request.Context()).Error(err)
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func pagination(request *http.Request, max int) (pageInfo, error) {
+	perPage, err := getIntParam(request, "per_page", defaultPerPage)
+	if err != nil {
+		return pageInfo{}, err
+	}
+
+	page, err := getIntParam(request, "page", defaultPage)
+	if err != nil {
+		return pageInfo{}, err
+	}
+
+	startIndex := (page - 1) * perPage
+	if startIndex != 0 && startIndex >= max {
+		return pageInfo{}, fmt.Errorf("invalid request: page: %d, per_page: %d", page, perPage)
+	}
+
+	endIndex := startIndex + perPage
+	if endIndex >= max {
+		endIndex = max
+	}
+
+	nextPage := 1
+	if page*perPage < max {
+		nextPage = page + 1
+	}
+
+	return pageInfo{startIndex: startIndex, endIndex: endIndex, nextPage: nextPage}, nil
+}
+
+func getIntParam(request *http.Request, key string, defaultValue int) (int, error) {
+	raw := request.URL.Query().Get(key)
+	if raw == "" {
+		return defaultValue, nil
+	}
+
+	value, err := strconv.Atoi(raw)
+	if err != nil || value <= 0 {
+		return 0, fmt.Errorf("invalid request: %s: %d", key, value)
+	}
+	return value, nil
+}
+
+func getProviderName(id string) string {
+	return strings.SplitN(id, ".", 2)[0]
 }
