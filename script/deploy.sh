aa2681e3a1c3c2106e250dfddf126dc3662ccf5d
Fernandez Ludovic <ludovic@containo.us>
2019-06-17 21:55:47 +0200

    fix: deploy script.

--- script/deploy.sh
+++ script/deploy.sh
@@ -15,8 +15,6 @@ git config --global user.name "Traefiker"
 
 # load ssh key
 echo "Loading key..."
-${encrypted_f9e835a425bc_key:?}
-${encrypted_f9e835a425bc_iv:?}
 openssl aes-256-cbc -K "${encrypted_f9e835a425bc_key}" -iv "${encrypted_f9e835a425bc_iv}" -in .travis/traefiker_rsa.enc -out ~/.ssh/traefiker_rsa -d
 eval "$(ssh-agent -s)"
 chmod 600 ~/.ssh/traefiker_rsa
