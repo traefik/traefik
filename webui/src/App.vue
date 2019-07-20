242b74f51226055cf22b2d0ea28cea5df99eebcb
Antoine CARON <antoine.caron@zenika.com>
2019-07-20 16:54:11 +0200

    feat(webui): add doc and version in navbar

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -10,11 +10,71 @@
             height="28"
           />
         </a>
+
+        <a
+          :class="{ 'is-active': isActive }"
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          @click="toggle"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div
+        :class="{ 'is-active': isActive }"
+        class="navbar-menu"
+        v-if="version.Version"
+      >
+        <div class="navbar-start">
+          <a class="navbar-item" :href="documentationUrl">
+            Documentation
+          </a>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">Version: {{ version.Version }}</div>
+        </div>
       </div>
     </nav>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    version: {},
+    isActive: false
+  }),
+  computed: {
+    documentationUrl() {
+      return `https://docs.traefik.io/${
+        this.version.Version === "dev" ? "master" : this.version.Version
+      }`;
+    }
+  },
+  created() {
+    this.fetchVersion();
+  },
+  methods: {
+    fetchVersion() {
+      return fetch("/api/version")
+        .then(response => response.json())
+        .then(response => (this.version = response));
+    },
+    toggle() {
+      this.isActive = !this.isActive;
+    }
+  }
+};
+</script>
 
 <style lang="sass">
 
