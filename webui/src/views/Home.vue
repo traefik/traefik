4e0c3fbf18c53b7fe2c6ee315e0f8e4ccad3e1d3
Antoine CARON <antoine.caron@zenika.com>
2019-07-20 16:23:03 +0200

    refactor(webui): use components to split Home concerns

--- webui/src/views/Home.vue
+++ webui/src/views/Home.vue
@@ -1,27 +1,31 @@
 <template>
   <main class="home section">
-    <section class="container panel">
-      <p class="panel-heading ">🚧 Work in progress...</p>
-      <div class="panel-block">
-        <div>
-          <p>
-            In the meantime, you can review your current configuration by using
-            the <a href="/api/rawdata">/api/rawdata</a> endpoint.
-          </p>
-          <p>
-            Also, please keep your <i class="fa fa-eye" /> on our
-            <a href="https://docs.traefik.io/v2.0/operations/dashboard/"
-              >documentation</a
-            >
-            to stay informed
-          </p>
+    <section class="container hero is-info">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            🚧 Work in progress...
+          </h1>
+          <h2 class="subtitle">
+            <p>
+              In the meantime, you can review your current configuration by
+              using the <a href="/api/rawdata">/api/rawdata</a> endpoint.
+            </p>
+            <p>
+              Also, please keep your <i class="fa fa-eye" /> on our
+              <a href="https://docs.traefik.io/v2.0/operations/dashboard/"
+                >documentation</a
+              >
+              to stay informed
+            </p>
+          </h2>
         </div>
       </div>
     </section>
 
-    <section class="container panel" v-if="entrypoints.length">
-      <p class="panel-heading ">Entrypoints</p>
-      <div class="panel-block">
+    <section class="container" v-if="entrypoints.length">
+      <p class="title is-4">Entrypoints</p>
+      <div class="tile is-child box columns">
         <nav class="level" :style="{ flex: '1 1' }">
           <div
             class="level-item has-text-centered"
@@ -39,47 +43,53 @@
 
     <section class="container" v-if="overview.http">
       <p class="title is-4">HTTP</p>
-      <div class="tile is-child box columns is-height-limited">
+      <div class="tile is-child box columns">
         <div class="column is-4">
-          <canvas id="http-routers" />
+          <EntityStateDoughnut
+            :entity="overview.http.routers"
+            title="Routers"
+          />
         </div>
         <div class="column is-4">
-          <canvas id="http-middlewares" />
+          <EntityStateDoughnut
+            :entity="overview.http.middlewares"
+            title="Middlewares"
+          />
         </div>
         <div class="column is-4">
-          <canvas id="http-services" />
+          <EntityStateDoughnut
+            :entity="overview.http.services"
+            title="Services"
+          />
         </div>
       </div>
     </section>
 
     <section class="container" v-if="overview.tcp">
       <p class="title is-4">TCP</p>
-      <div class="tile is-child box columns is-height-limited">
+      <div class="tile is-child box columns">
         <div class="column is-4">
-          <canvas id="tcp-routers" />
+          <EntityStateDoughnut :entity="overview.tcp.routers" title="Routers" />
         </div>
         <div class="column is-4">
-          <canvas id="tcp-services" />
+          <EntityStateDoughnut
+            :entity="overview.tcp.services"
+            title="Services"
+          />
         </div>
       </div>
     </section>
 
-    <section class="container panel">
-      <p class="panel-heading">Features</p>
-      <div class="panel-block">
+    <section class="container">
+      <p class="title is-4">Features</p>
+      <div class="tile is-child box columns">
         <div class="tile is-ancestor">
-          <div
-            class="tile is-parent"
+          <FeatureTile
             v-for="(feature, key) of overview.features"
             :key="key"
-          >
-            <div
-              class="tile is-child notification"
-              :class="{ 'is-success': feature, 'is-danger': !feature }"
-            >
-              <p class="title">{{ key }}</p>
-            </div>
-          </div>
+            :title="key"
+            :modifier="{ 'is-success': feature, 'is-danger': !feature }"
+          />
         </div>
       </div>
     </section>
@@ -87,28 +97,15 @@
 </template>
 
 <script>
-import Chart from "chart.js";
-
-Chart.plugins.register({
-  afterDraw: function(chart) {
-    if (chart.data.datasets[0].data.reduce((acc, it) => acc + it, 0) === 0) {
-      var ctx = chart.chart.ctx;
-      var width = chart.chart.width;
-      var height = chart.chart.height
-      chart.clear();
-
-      ctx.save();
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      ctx.font = "16px normal 'Helvetica Nueue'";
-      ctx.fillText(`No ${chart.options.title.text}`, width / 2, height / 2);
-      ctx.restore();
-    }
-  }
-});
+import FeatureTile from "../components/Tile";
+import EntityStateDoughnut from "../components/EntityStateDoughnut";
 
 export default {
   name: "home",
+  components: {
+    FeatureTile,
+    EntityStateDoughnut
+  },
   data: () => ({
     entrypoints: [],
     overview: {
@@ -128,78 +125,10 @@ export default {
     interval: null
   }),
   methods: {
-    buildDoughnutChart(
-      selector,
-      entity = { errors: 2, warnings: 2, total: 6 },
-      name
-    ) {
-      return new Chart(this.$el.querySelector(selector), {
-        type: "doughnut",
-        data: {
-          datasets: [
-            {
-              data: [
-                entity.errors,
-                entity.warnings,
-                entity.total - (entity.errors + entity.warnings)
-              ],
-              backgroundColor: [
-                "hsl(348, 100%, 61%)",
-                "hsl(48, 100%, 67%)",
-                "hsl(141, 71%, 48%)"
-              ]
-            }
-          ],
-          labels: ["errors", "warnings", "success"]
-        },
-        options: {
-          title: {
-            display: true,
-            text: name
-          },
-          legend: {
-            display: false
-          }
-        }
-      });
-    },
     fetchOverview() {
       return fetch("/api/overview")
         .then(response => response.json())
-        .then(response => (this.overview = response))
-        .then(() => {
-          this.charts = {
-            http: {
-              routers: this.buildDoughnutChart(
-                "#http-routers",
-                this.overview.http.routers,
-                "Routers"
-              ),
-              middlewares: this.buildDoughnutChart(
-                "#http-middlewares",
-                this.overview.http.middlewares,
-                "Middlewares"
-              ),
-              services: this.buildDoughnutChart(
-                "#http-services",
-                this.overview.http.services,
-                "Services"
-              )
-            },
-            tcp: {
-              routers: this.buildDoughnutChart(
-                "#tcp-routers",
-                this.overview.tcp.routers,
-                "Routers"
-              ),
-              services: this.buildDoughnutChart(
-                "#tcp-services",
-                this.overview.tcp.services,
-                "Services"
-              )
-            }
-          };
-        });
+        .then(response => (this.overview = response));
     },
     fetchEntrypoints() {
       return fetch("/api/entrypoints")
