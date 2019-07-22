7c852fbf3348adc1686a1dafb9e3681a00e9c80e
Antoine Caron <Slashgear@users.noreply.github.com>
2019-07-22 11:06:04 +0200

    refactor(webui): use components to split Home concerns

--- webui/src/main.js
+++ webui/src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import store from "./store";
 
 import "bulma/css/bulma.min.css";
+import "@fortawesome/fontawesome-free";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.config.productionTip = false;
 
