09800027cfb75abbae8a3759c0415f3f278bb941
Michael <michael.matur@gmail.com>
2019-09-20 11:31:58 +0200

    review first implementation

---
+++ pkg/provider/consulcatalog/config.go
@@ -0,0 +1,201 @@
+package consulcatalog
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+
+	"github.com/containous/traefik/v2/pkg/config/label"
+
+	"github.com/containous/traefik/v2/pkg/provider/constraints"
+
+	"github.com/containous/traefik/v2/pkg/log"
+
+	"github.com/containous/traefik/v2/pkg/config/dynamic"
+	"github.com/containous/traefik/v2/pkg/provider"
+)
+
+func (p *Provider) buildConfiguration(ctx context.Context, datas []itemData) *dynamic.Configuration {
+	configurations := make(map[string]*dynamic.Configuration)
+
+	for _, item := range datas {
+		svcName := item.Name + "-" + item.ID
+		ctxSvc := log.With(ctx, log.Str("serviceName", svcName))
+
+		if !p.keepContainer(ctxSvc, item) {
+			continue
+		}
+
+		logger := log.FromContext(ctxSvc)
+		confFromLabel, err := label.DecodeConfiguration(item.Labels)
+		if err != nil {
+			logger.Error(err)
+			continue
+		}
+
+		if len(confFromLabel.TCP.Routers) > 0 || len(confFromLabel.TCP.Services) > 0 {
+			err := p.buildTCPServiceConfiguration(ctxSvc, item, confFromLabel.TCP)
+			if err != nil {
+				logger.Error(err)
+				continue
+			}
+			provider.BuildTCPRouterConfiguration(ctxSvc, confFromLabel.TCP)
+			if len(confFromLabel.HTTP.Routers) == 0 &&
+				len(confFromLabel.HTTP.Middlewares) == 0 &&
+				len(confFromLabel.HTTP.Services) == 0 {
+				configurations[svcName] = confFromLabel
+				continue
+			}
+		}
+
+		err = p.buildServiceConfiguration(ctxSvc, item, confFromLabel.HTTP)
+		if err != nil {
+			logger.Error(err)
+			continue
+		}
+
+		model := struct {
+			Name   string
+			Labels map[string]string
+		}{
+			Name:   item.Name,
+			Labels: item.Labels,
+		}
+
+		provider.BuildRouterConfiguration(ctx, confFromLabel.HTTP, item.Name, p.defaultRuleTpl, model)
+
+		configurations[svcName] = confFromLabel
+	}
+
+	return provider.Merge(ctx, configurations)
+}
+
+func (p *Provider) keepContainer(ctx context.Context, item itemData) bool {
+	logger := log.FromContext(ctx)
+
+	if !item.Enable {
+		logger.Debug("Filtering disabled item")
+		return false
+	}
+
+	matches, err := constraints.Match(item.Labels, p.Constraints)
+	if err != nil {
+		logger.Errorf("Error matching constraints expression: %v", err)
+		return false
+	}
+	if !matches {
+		logger.Debugf("Container pruned by constraint expression: %q", p.Constraints)
+		return false
+	}
+
+	if item.Status != "" && item.Status != "passing" {
+		logger.Debug("Filtering unhealthy or starting item")
+		return false
+	}
+
+	return true
+}
+
+func (p *Provider) buildTCPServiceConfiguration(ctx context.Context, item itemData, configuration *dynamic.TCPConfiguration) error {
+	if len(configuration.Services) == 0 {
+		configuration.Services = make(map[string]*dynamic.TCPService)
+		lb := &dynamic.TCPServersLoadBalancer{}
+		lb.SetDefaults()
+		configuration.Services[item.Name] = &dynamic.TCPService{
+			LoadBalancer: lb,
+		}
+	}
+
+	for name, service := range configuration.Services {
+		ctxSvc := log.With(ctx, log.Str(log.ServiceName, name))
+		err := p.addServerTCP(ctxSvc, item, service.LoadBalancer)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *Provider) buildServiceConfiguration(ctx context.Context, item itemData, configuration *dynamic.HTTPConfiguration) error {
+	if len(configuration.Services) == 0 {
+		configuration.Services = make(map[string]*dynamic.Service)
+		lb := &dynamic.ServersLoadBalancer{}
+		lb.SetDefaults()
+		configuration.Services[item.Name] = &dynamic.Service{
+			LoadBalancer: lb,
+		}
+	}
+
+	for name, service := range configuration.Services {
+		ctxSvc := log.With(ctx, log.Str(log.ServiceName, name))
+		err := p.addServer(ctxSvc, item, service.LoadBalancer)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *Provider) addServerTCP(ctx context.Context, item itemData, loadBalancer *dynamic.TCPServersLoadBalancer) error {
+	if loadBalancer == nil {
+		return errors.New("load-balancer is not defined")
+	}
+
+	var port string
+	if len(loadBalancer.Servers) > 0 {
+		port = loadBalancer.Servers[0].Port
+	}
+
+	if len(loadBalancer.Servers) == 0 {
+		server := dynamic.TCPServer{}
+
+		loadBalancer.Servers = []dynamic.TCPServer{server}
+	}
+
+	if item.Port != "" {
+		port = item.Port
+		loadBalancer.Servers[0].Port = ""
+	}
+
+	if port == "" {
+		return errors.New("port is missing")
+	}
+
+	loadBalancer.Servers[0].Address = net.JoinHostPort(item.Address, port)
+	return nil
+}
+
+func (p *Provider) addServer(ctx context.Context, item itemData, loadBalancer *dynamic.ServersLoadBalancer) error {
+	if loadBalancer == nil {
+		return errors.New("load-balancer is not defined")
+	}
+
+	var port string
+	if len(loadBalancer.Servers) > 0 {
+		port = loadBalancer.Servers[0].Port
+	}
+
+	if len(loadBalancer.Servers) == 0 {
+		server := dynamic.Server{}
+		server.SetDefaults()
+
+		loadBalancer.Servers = []dynamic.Server{server}
+	}
+
+	if item.Port != "" {
+		port = item.Port
+		loadBalancer.Servers[0].Port = ""
+	}
+
+	if port == "" {
+		return errors.New("port is missing")
+	}
+
+	loadBalancer.Servers[0].URL = fmt.Sprintf("%s://%s", loadBalancer.Servers[0].Scheme, net.JoinHostPort(item.Address, port))
+	loadBalancer.Servers[0].Scheme = ""
+
+	return nil
+}
--- pkg/provider/consulcatalog/consul_catalog.go
+++ pkg/provider/consulcatalog/consul_catalog.go
@@ -3,86 +3,108 @@ package consulcatalog
 import (
 	"context"
 	"fmt"
+	"text/template"
+	"time"
+
 	"github.com/cenkalti/backoff/v3"
 	"github.com/containous/traefik/v2/pkg/config/dynamic"
 	"github.com/containous/traefik/v2/pkg/job"
 	"github.com/containous/traefik/v2/pkg/log"
+	"github.com/containous/traefik/v2/pkg/provider"
 	"github.com/containous/traefik/v2/pkg/safe"
 	"github.com/containous/traefik/v2/pkg/types"
 	"github.com/hashicorp/consul/api"
-	"time"
 )
 
-type getConsulClientFunc func(*EndpointConfig) (consulCatalog, error)
+// DefaultTemplateRule The default template for the default rule.
+const DefaultTemplateRule = "Host(`{{ normalize .Name }}`)"
 
-type consulCatalog interface {
-	Service(string, string, *api.QueryOptions) ([]*api.CatalogService, *api.QueryMeta, error)
-	Services(*api.QueryOptions) (map[string][]string, *api.QueryMeta, error)
+var _ provider.Provider = (*Provider)(nil)
+
+// Provider holds configurations of the provider.
+type Provider struct {
+	Constraints      string          `description:"Constraints is an expression that Traefik matches against the container's labels to determine whether to create any route for that container." json:"constraints,omitempty" toml:"constraints,omitempty" yaml:"constraints,omitempty" export:"true"`
+	Endpoint         *EndpointConfig `description:"Consul endpoint settings" json:"endpoint,omitempty" toml:"endpoint,omitempty" yaml:"endpoint,omitempty" export:"true"`
+	Prefix           string          `description:"Prefix for consul service tags. Default 'traefik'" json:"prefix,omitempty" toml:"prefix,omitempty" yaml:"prefix,omitempty" export:"true"`
+	RefreshInterval  types.Duration  `description:"Interval for check Consul API. Default 100ms" json:"refreshInterval,omitempty" toml:"refreshInterval,omitempty" yaml:"refreshInterval,omitempty" export:"true"`
+	ExposedByDefault bool            `description:"Expose containers by default." json:"exposedByDefault,omitempty" toml:"exposedByDefault,omitempty" yaml:"exposedByDefault,omitempty" export:"true"`
+	DefaultRule      string          `description:"Default rule." json:"defaultRule,omitempty" toml:"defaultRule,omitempty" yaml:"defaultRule,omitempty"`
+
+	client         *api.Client
+	defaultRuleTpl *template.Template
 }
 
-type EndpointHttpAuthConfig struct {
-	Username string `description:"Basic Auth username" json:"username,omitempty" toml:"username,omitempty" yaml:"username,omitempty" export:"true"`
-	Password string `description:"Basic Auth password" json:"password,omitempty" toml:"password,omitempty" yaml:"password,omitempty" export:"true"`
-}
-
-type EndpointTLSConfig struct {
-	Address            string `description:"Address of the Consul server" json:"address,omitempty" toml:"address,omitempty" yaml:"address,omitempty" export:"true"`
-	CAFile             string `description:"Path to the CA certificate used for Consul communication, defaults to the system bundle if not specified" json:"cafile,omitempty" toml:"cafile,omitempty" yaml:"cafile,omitempty" export:"true"`
-	CAPath             string `description:"Path to a directory of CA certificates to use for Consul communication, defaults to the system bundle if not specified" json:"capath,omitempty" toml:"capath,omitempty" yaml:"capath,omitempty" export:"true"`
-	CertFile           string `description:"Path to the certificate for Consul communication. If this is set then you need to also set KeyFile" json:"certfile,omitempty" toml:"certfile,omitempty" yaml:"certfile,omitempty" export:"true"`
-	KeyFile            string `description:"Path to the private key for Consul communication. If this is set then you need to also set CertFile" json:"keyfile,omitempty" toml:"keyfile,omitempty" yaml:"keyfile,omitempty" export:"true"`
-	InsecureSkipVerify bool   `description:"InsecureSkipVerify if set to true will disable TLS host verification" json:"insecureSkipVerify,omitempty" toml:"insecureSkipVerify,omitempty" yaml:"insecureSkipVerify,omitempty" export:"true"`
-}
-
+// EndpointConfig holds configurations of the endpoint.
 type EndpointConfig struct {
 	Address          string                 `description:"The address of the Consul server" json:"address,omitempty" toml:"address,omitempty" yaml:"address,omitempty" export:"true"`
 	Scheme           string                 `description:"The URI scheme for the Consul server" json:"scheme,omitempty" toml:"scheme,omitempty" yaml:"scheme,omitempty" export:"true"`
 	Datacenter       string                 `description:"Datacenter to use. If not provided, the default agent datacenter is used" json:"datacenter,omitempty" toml:"datacenter,omitempty" yaml:"datacenter,omitempty" export:"true"`
 	Token            string                 `description:"Token is used to provide a per-request ACL token which overrides the agent's default token" json:"token,omitempty" toml:"token,omitempty" yaml:"token,omitempty" export:"true"`
-	TLS              EndpointTLSConfig      `description:"TLSConfig is used to generate a TLSClientConfig that's useful for talking to Consul using TLS" json:"tls,omitempty" toml:"tls,omitempty" yaml:"tls,omitempty" export:"true"`
+	TLS              *types.ClientTLS       `description:"Enable TLS support." json:"tls,omitempty" toml:"tls,omitempty" yaml:"tls,omitempty" export:"true"`
 	HttpAuth         EndpointHttpAuthConfig `description:"Auth info to use for http access" json:"httpAuth,omitempty" toml:"httpAuth,omitempty" yaml:"httpAuth,omitempty" export:"true"`
 	EndpointWaitTime types.Duration         `description:"WaitTime limits how long a Watch will block. If not provided, the agent default values will be used" json:"endpointWaitTime,omitempty" toml:"endpointWaitTime,omitempty" yaml:"endpointWaitTime,omitempty" export:"true"`
 }
 
-type Provider struct {
-	Endpoint         *EndpointConfig `description:"Consul endpoint settings" json:"endpoint,omitempty" toml:"endpoint,omitempty" yaml:"endpoint,omitempty" export:"true"`
-	Prefix           string          `description:"Prefix for consul service tags. Default 'traefik'" json:"prefix,omitempty" toml:"prefix,omitempty" yaml:"prefix,omitempty" export:"true"`
-	Entrypoints      []string        `description:"Default entrypoints" json:"entrypoints,omitempty" toml:"entrypoints,omitempty" yaml:"entrypoints,omitempty" export:"true"`
-	Middlewares      []string        `description:"Default middlewares" json:"middlewares,omitempty" toml:"middlewares,omitempty" yaml:"middlewares,omitempty" export:"true"`
-	RouterRule       string          `description:"Default router rule" json:"routerRule,omitempty" toml:"routerRule,omitempty" yaml:"routerRule,omitempty" export:"true"`
-	Protocol         string          `description:"Default protocol: http or tcp" json:"protocol,omitempty" toml:"protocol,omitempty" yaml:"protocol,omitempty" export:"true"`
-	RefreshInterval  types.Duration  `description:"Interval for check Consul API. Default 100ms" json:"refreshInterval,omitempty" toml:"refreshInterval,omitempty" yaml:"refreshInterval,omitempty" export:"true"`
-	PassHostHeader   bool            `description:"Default value PassHostHeader" json:"passHostHeader,omitempty" toml:"passHostHeader,omitempty" yaml:"passHostHeader,omitempty" export:"true"`
-	ExposedByDefault bool            `description:"Expose containers by default." json:"exposedByDefault,omitempty" toml:"exposedByDefault,omitempty" yaml:"exposedByDefault,omitempty" export:"true"`
-
-	labelEnabled        string
-	getConsulClientFunc getConsulClientFunc
-	clientCatalog       consulCatalog
+// EndpointHttpAuthConfig holds configurations of the authentication.
+type EndpointHttpAuthConfig struct {
+	Username string `description:"Basic Auth username" json:"username,omitempty" toml:"username,omitempty" yaml:"username,omitempty" export:"true"`
+	Password string `description:"Basic Auth password" json:"password,omitempty" toml:"password,omitempty" yaml:"password,omitempty" export:"true"`
 }
+
+// SetDefaults sets the default values.
 
 func (p *Provider) SetDefaults() {
 	p.Endpoint = &EndpointConfig{
 		Address: "http://127.0.0.1:8500",
 	}
-	p.RefreshInterval = types.Duration(time.Millisecond * 100)
+	p.RefreshInterval = types.Duration(15 * time.Second)
 	p.Prefix = "traefik"
-	p.Entrypoints = []string{"web"}
-	p.RouterRule = "Path(`/`)"
-	p.PassHostHeader = true
-	p.Protocol = "http"
 	p.ExposedByDefault = true
-	p.getConsulClientFunc = getConsulClient
-
+	p.DefaultRule = DefaultTemplateRule
 }
 
+// Init the provider.
 func (p *Provider) Init() error {
-	if err := p.validateConfig(); err != nil {
-		return err
+	defaultRuleTpl, err := provider.MakeDefaultRuleTemplate(p.DefaultRule, nil)
+	if err != nil {
+		return fmt.Errorf("error while parsing default rule: %v", err)
 	}
 
-	p.labelEnabled = p.Prefix + ".enable=true"
-
+	p.defaultRuleTpl = defaultRuleTpl
 	return nil
+}
+
+type itemData struct {
+	ID      string
+	Name    string
+	Address string
+	Port    string
+	Enable  bool
+	Status  string
+	Labels  map[string]string
+}
+
+func createClient(cfg *EndpointConfig) (*api.Client, error) {
+	config := api.Config{
+		Address:    cfg.Address,
+		Scheme:     cfg.Scheme,
+		Datacenter: cfg.Datacenter,
+		HttpAuth: &api.HttpBasicAuth{
+			Username: cfg.HttpAuth.Username,
+			Password: cfg.HttpAuth.Password,
+		},
+		WaitTime: time.Duration(cfg.EndpointWaitTime),
+		Token:    cfg.Token,
+		TLSConfig: api.TLSConfig{
+			Address:            cfg.Address,
+			CAFile:             cfg.TLS.CA,
+			CertFile:           cfg.TLS.Cert,
+			KeyFile:            cfg.TLS.Key,
+			InsecureSkipVerify: cfg.TLS.InsecureSkipVerify,
+		},
+	}
+
+	return api.NewClient(&config)
 }
 
 func (p *Provider) Provide(configurationChan chan<- dynamic.Message, pool *safe.Pool) error {
@@ -93,7 +115,7 @@ func (p *Provider) Provide(configurationChan chan<- dynamic.Message, pool *safe.
 		operation := func() error {
 			var err error
 
-			p.clientCatalog, err = p.getConsulClientFunc(p.Endpoint)
+			p.client, err = createClient(p.Endpoint)
 			if err != nil {
 				return fmt.Errorf("error create consul client, %v", err)
 			}
@@ -109,12 +131,7 @@ func (p *Provider) Provide(configurationChan chan<- dynamic.Message, pool *safe.
 						return err
 					}
 
-					configuration, err := p.buildConfiguration(routineCtx, data)
-					if err != nil {
-						logger.Errorf("error building configuration, %v", err)
-						return err
-					}
-
+					configuration := p.buildConfiguration(routineCtx, data)
 					configurationChan <- dynamic.Message{
 						ProviderName:  "consulcatalog",
 						Configuration: configuration,
@@ -131,7 +148,6 @@ func (p *Provider) Provide(configurationChan chan<- dynamic.Message, pool *safe.
 		}
 
 		err := backoff.RetryNotify(safe.OperationWithRecover(operation), backoff.WithContext(job.NewBackOff(backoff.NewExponentialBackOff()), ctxLog), notify)
-
 		if err != nil {
 			logger.Errorf("Cannot connect to consulcatalog server %+v", err)
 		}
@@ -140,32 +156,47 @@ func (p *Provider) Provide(configurationChan chan<- dynamic.Message, pool *safe.
 	return nil
 }
 
-func getConsulClient(cfg *EndpointConfig) (consulCatalog, error) {
+func (p *Provider) getConsulServicesData(ctx context.Context) ([]itemData, error) {
+	var data []itemData
 
-	config := api.Config{
-		Address:    cfg.Address,
-		Scheme:     cfg.Scheme,
-		Datacenter: cfg.Datacenter,
-		HttpAuth: &api.HttpBasicAuth{
-			Username: cfg.HttpAuth.Username,
-			Password: cfg.HttpAuth.Password,
-		},
-		WaitTime: time.Duration(cfg.EndpointWaitTime),
-		Token:    cfg.Token,
-		TLSConfig: api.TLSConfig{
-			Address:            cfg.TLS.Address,
-			CAFile:             cfg.TLS.CAFile,
-			CAPath:             cfg.TLS.CAPath,
-			CertFile:           cfg.TLS.CertFile,
-			KeyFile:            cfg.TLS.KeyFile,
-			InsecureSkipVerify: cfg.TLS.InsecureSkipVerify,
-		},
-	}
-
-	client, err := api.NewClient(&config)
+	consulServiceNames, err := p.fetchServices(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	return client.Catalog(), nil
+	for name := range consulServiceNames {
+		consulServices, err := p.fetchService(ctx, name)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, consulService := range consulServices {
+			labels := tagsToNeutralLabels(consulService.ServiceTags, p.Prefix)
+			item := itemData{
+				ID:      consulService.ServiceID,
+				Name:    consulService.ServiceName,
+				Address: consulService.ServiceAddress,
+				Port:    string(consulService.ServicePort),
+				Labels:  labels,
+			}
+
+			data = append(data, item)
+		}
+	}
+	return data, nil
+}
+
+func (p *Provider) fetchService(ctx context.Context, name string) ([]*api.CatalogService, error) {
+	var tagFilter string
+	if !p.ExposedByDefault {
+		tagFilter = p.Prefix + ".enable=true"
+	}
+
+	consulServices, _, err := p.client.Catalog().Service(name, tagFilter, &api.QueryOptions{})
+	return consulServices, err
+}
+
+func (p *Provider) fetchServices(ctx context.Context) (map[string][]string, error) {
+	serviceNames, _, err := p.client.Catalog().Services(&api.QueryOptions{})
+	return serviceNames, err
 }
