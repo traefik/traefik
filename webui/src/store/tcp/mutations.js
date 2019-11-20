e397a595791492dc7fead98f93974bb8450cf5c0
Matthieu Hostache <matthieu.hostache@gmail.com>
2019-11-20 14:28:04 +0100

    Apply infinite scrolling to tcp routers

--- webui/src/store/tcp/mutations.js
+++ webui/src/store/tcp/mutations.js
@@ -5,12 +5,33 @@ export function getAllRoutersRequest (state) {
   state.allRouters.loading = true
 }
 
-export function getAllRoutersSuccess (state, body) {
-  state.allRouters = { items: body.data, total: body.total, loading: false }
+export function getAllRoutersSuccess (state, data) {
+  const { body, query = '', status = '', page } = data
+  const currentState = state.allRouters
+
+  const isSameContext = currentState.currentQuery === query && currentState.currentStatus === status
+
+  state.allRouters = {
+    ...state.allRouters,
+    items: [
+      ...(isSameContext && currentState.items && page !== 1 ? currentState.items : []),
+      ...(body.data || [])
+    ],
+    currentPage: page,
+    total: body.total,
+    loading: false,
+    currentQuery: query,
+    currentStatus: status
+  }
 }
 
 export function getAllRoutersFailure (state, error) {
-  state.allRouters = { error }
+  state.allRouters = {
+    ...state.allRouters,
+    loading: false,
+    error,
+    endReached: error.message.includes('invalid request: page:')
+  }
 }
 
 export function getAllRoutersClear (state) {
