36e4bfce0d9f8e223b3b13b292deeeb129e383c8
Maxim Fominykh <vominyh@yandex.ru>
2019-11-23 13:44:09 +0500

    Duration order consistency when multiplying number by time unit

--- pkg/provider/rancher/rancher.go
+++ pkg/provider/rancher/rancher.go
@@ -151,7 +151,7 @@ func (p *Provider) Provide(configurationChan chan<- dynamic.Message, pool *safe.
 }
 
 func (p *Provider) intervalPoll(ctx context.Context, client rancher.Client, updateConfiguration func(string)) {
-	ticker := time.NewTicker(time.Second * time.Duration(p.RefreshSeconds))
+	ticker := time.NewTicker(time.Duration(p.RefreshSeconds) * time.Second)
 	defer ticker.Stop()
 
 	var version string
