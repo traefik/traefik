b3fcf2f17e3e77eebbea70e38e31bbba6b5d906c
Fernandez Ludovic <ludovic@containo.us>
2019-12-07 22:21:09 +0100

    fix: remove double call to close.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -48,7 +48,6 @@ func NewServer(routinesPool *safe.Pool, entryPoints TCPEntryPoints, watcher *Con
 // Start starts the server and Stop/Close it when context is Done
 func (s *Server) Start(ctx context.Context) {
 	go func() {
-		defer s.Close()
 		<-ctx.Done()
 		logger := log.FromContext(ctx)
 		logger.Info("I have to go...")
