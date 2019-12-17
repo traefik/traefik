98c991364322abf0128fa9925185e04ff14bb12a
Sylvain Rabot <s.rabot@lectra.com>
2019-12-17 10:11:45 +0100

    Rename captureRequestReader.count to captureRequestReader.size
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- pkg/middlewares/accesslog/capture_request_reader.go
+++ pkg/middlewares/accesslog/capture_request_reader.go
@@ -3,13 +3,16 @@ package accesslog
 import "io"
 
 type captureRequestReader struct {
+	// ReadCloser from where the request body is read
 	source io.ReadCloser
-	count  int64
+	// Size of the request body incremented each time
+	// captureRequestReader.Read is called
+	size int64
 }
 
 func (r *captureRequestReader) Read(p []byte) (int, error) {
 	n, err := r.source.Read(p)
-	r.count += int64(n)
+	r.size += int64(n)
 	return n, err
 }
 
--- pkg/middlewares/accesslog/logger.go
+++ pkg/middlewares/accesslog/logger.go
@@ -171,7 +171,7 @@ func (h *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request, next http
 
 	var crr *captureRequestReader
 	if req.Body != nil {
-		crr = &captureRequestReader{source: req.Body, count: 0}
+		crr = &captureRequestReader{source: req.Body, size: 0}
 		reqWithDataTable.Body = crr
 	}
 
@@ -214,7 +214,7 @@ func (h *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request, next http
 		size:    crw.Size(),
 	}
 	if crr != nil {
-		logDataTable.Request.count = crr.count
+		logDataTable.Request.size = crr.size
 	}
 
 	if h.config.BufferingSize > 0 {
@@ -280,7 +280,7 @@ func (h *Handler) logTheRoundTrip(logDataTable *LogData) {
 		retryAttempts = 0
 	}
 	core[RetryAttempts] = retryAttempts
-	core[RequestContentSize] = logDataTable.Request.count
+	core[RequestContentSize] = logDataTable.Request.size
 
 	status := logDataTable.DownstreamResponse.status
 	core[DownstreamStatus] = status
