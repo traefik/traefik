2ead215bd84762666852e6a0563973634834338d
maziang <maziang@bytedance.com>
2020-02-18 19:44:14 +0800

    add comments for ignore non-positive weight handler

--- pkg/server/service/loadbalancer/wrr/wrr.go
+++ pkg/server/service/loadbalancer/wrr/wrr.go
@@ -135,6 +135,7 @@ func (b *Balancer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 // AddService adds a handler.
 // It is not thread safe with ServeHTTP.
+// A handler with a non-positive weight is ignored.
 func (b *Balancer) AddService(name string, handler http.Handler, weight *int) {
 	w := 1
 	if weight != nil {
