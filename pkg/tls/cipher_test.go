07d6aa4544ff28cae9119ea17f7d2c4630fa100f
Sylvain Rabot <sylvain@abstraction.fr>
2020-03-02 10:46:58 +0100

    Fix redundant ciphers caused by legacy aliases
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- pkg/tls/cipher_test.go
+++ pkg/tls/cipher_test.go
@@ -6,6 +6,15 @@ import (
 
 func TestCiphersMapsSync(t *testing.T) {
 	for k, v := range CipherSuites {
+		// Following names are legacy aliases. We do not test for their presence
+		// in CipherSuitesReversed
+		switch k {
+		case "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305":
+			fallthrough
+		case "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305":
+			continue
+		}
+
 		if rv, ok := CipherSuitesReversed[v]; !ok {
 			t.Errorf("Maps not in sync: `%v` key is missing in tls.CipherSuitesReversed", v)
 		} else if k != rv {
