91553ad97b3b60890e95e27d748dae4f75346279
Sylvain Rabot <sylvain@abstraction.fr>
2020-03-05 11:05:42 +0100

    Fix format verbs
    Co-Authored-By: Ludovic Fernandez <ldez@users.noreply.github.com>

--- pkg/tls/cipher_test.go
+++ pkg/tls/cipher_test.go
@@ -14,17 +14,17 @@ func TestCiphersMapsSync(t *testing.T) {
 		}
 
 		if rv, ok := CipherSuitesReversed[v]; !ok {
-			t.Errorf("Maps not in sync: `%v` key is missing in tls.CipherSuitesReversed", v)
+			t.Errorf("Maps not in sync: `%d` key is missing in tls.CipherSuitesReversed", v)
 		} else if k != rv {
-			t.Errorf("Maps not in sync: tls.CipherSuites[%v] = `%v` AND tls.CipherSuitesReversed[`%v`] = `%v`", k, v, v, rv)
+			t.Errorf("Maps not in sync: tls.CipherSuites[%s] = `%d` AND tls.CipherSuitesReversed[`%d`] = `%v`", k, v, v, rv)
 		}
 	}
 
 	for k, v := range CipherSuitesReversed {
 		if rv, ok := CipherSuites[v]; !ok {
-			t.Errorf("Maps not in sync: `%v` key is missing in tls.CipherSuites", v)
+			t.Errorf("Maps not in sync: `%s` key is missing in tls.CipherSuites", v)
 		} else if k != rv {
-			t.Errorf("Maps not in sync: tls.CipherSuitesReversed[`%v`] = `%v` AND tls.CipherSuites[`%v`] = `%v`", k, v, v, rv)
+			t.Errorf("Maps not in sync: tls.CipherSuitesReversed[`%d`] = `%s` AND tls.CipherSuites[`%s`] = `%d`", k, v, v, rv)
 		}
 	}
 }
