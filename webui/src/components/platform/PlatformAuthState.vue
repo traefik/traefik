b980c87effca3f12e9cc441123e99b0a8d4e6623
Matthieu Hostache <matthieu.hostache@gmail.com>
2020-09-15 10:26:03 +0200

    Avoid Traefik Pilot iframe code in Traefik webui regarding notifications

--- webui/src/components/platform/PlatformAuthState.vue
+++ webui/src/components/platform/PlatformAuthState.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="iframe-wrapper" v-if="isOnline">
     <iframe
-      v-if="renderIrame"
       id="platform-auth-state"
-      :src="AuthStateIFrameUrl"
-      @load="onAuthStateIFrameLoad"
+      :src="iFrameUrl"
+      v-if="renderIrame"
+      v-resize="resizeOpts"
       height="64px"
       frameBorder="0"
     />
@@ -14,47 +14,50 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
-import iFrameResize from 'iframe-resizer/js/iframeResizer'
+import qs from 'query-string'
 import '../../_directives/resize'
 
 export default {
   name: 'PlatformPanel',
   data () {
     return {
-      renderIrame: true
-    }
-  },
-  computed: {
-    ...mapGetters('platform', { isPlatformOpen: 'isOpen' }),
-    isOnline () {
-      return window.navigator.onLine
-    }
-  },
-  methods: {
-    ...mapActions('platform', { openPlatform: 'open' }, { closePlatform: 'close' }),
-    onAuthStateIFrameLoad () {
-      iFrameResize({
+      renderIrame: true,
+      resizeOpts: {
         log: false,
-        resize: true,
         onMessage: ({ iframe, message }) => {
           if (typeof message === 'string') {
+            // 1st condition for backward compatibility
             if (message === 'open:profile') {
-              this.openPlatform()
-            }
-
-            if (message === 'logout') {
+              this.openPlatform('/')
+            } else if (message.includes('open:')) {
+              this.openPlatform(message.split('open:')[1])
+            } else if (message === 'logout') {
               this.closePlatform()
             }
           }
         }
-      }, '#platform-auth-state')
+      }
     }
   },
   created () {
-    const authRedirectUrl = `${window.location.href.split('?')[0]}?platform=on`
-    const queryParams = `?authRedirectUrl=${encodeURIComponent(authRedirectUrl)}`
+    this.getInstanceInfos()
+  },
+  computed: {
+    ...mapGetters('platform', { isPlatformOpen: 'isOpen', platformPath: 'path' }),
+    ...mapGetters('core', { instanceInfos: 'version' }),
+    isOnline () {
+      return window.navigator.onLine
+    },
+    iFrameUrl () {
+      const instanceInfos = JSON.stringify(this.instanceInfos)
+      const authRedirectUrl = `${window.location.href.split('?')[0]}?platform=${this.platformPath}`
 
-    this.AuthStateIFrameUrl = `${this.platformUrl}/partials/auth-state${queryParams}`
+      return qs.stringifyUrl({ url: `${this.platformUrl}/partials/auth-state`, query: { authRedirectUrl, instanceInfos } })
+    }
+  },
+  methods: {
+    ...mapActions('platform', { openPlatform: 'open' }, { closePlatform: 'close' }),
+    ...mapActions('core', { getInstanceInfos: 'getVersion' })
   },
   watch: {
     isPlatformOpen (isOpen, wasOpen) {
@@ -68,3 +71,12 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+  @import "../../css/sass/variables";
+
+  #platform-auth-state {
+    width: 1px;
+    min-width: 296px;
+  }
+</style>
