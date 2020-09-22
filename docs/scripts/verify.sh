07629ec25d301f9333e060fa13b527b366b8c706
Fernandez Ludovic <ludovic@containo.us>
2020-09-22 23:05:16 +0200

    chore: disable doc deployement.

--- docs/scripts/verify.sh
+++ docs/scripts/verify.sh
@@ -19,8 +19,9 @@ find "${PATH_TO_SITE}" -type f -not -path "/app/site/theme/*" \
   htmlproofer \
   --check-html \
   --check_external_hash \
-  --alt_ignore="/traefik.logo.png/" \
+  --alt_ignore="/traefikproxy-vertical-logo-color.svg/" \
   --http_status_ignore="0,500,501,503" \
+  --file_ignore="/404.html/" \
   --url_ignore="/https://groups.google.com/a/traefik.io/forum/#!forum/security/,/localhost:/,/127.0.0.1:/,/fonts.gstatic.com/,/.minikube/,/github.com\/traefik\/traefik\/*edit*/,/github.com\/traefik\/traefik/,/doc.traefik.io/,/github\.com\/golang\/oauth2\/blob\/36a7019397c4c86cf59eeab3bc0d188bac444277\/.+/,/www.akamai.com/,/pilot.traefik.io\/profile/,/traefik.io/,/doc.traefik.io\/traefik-mesh/,/www.mkdocs.org/,/squidfunk.github.io/,/ietf.org/" \
   '{}' 1>/dev/null
 ## HTML-proofer options at https://github.com/gjtorikian/html-proofer#configuration
