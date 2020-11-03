9da38ecc9d1d49df78ea5d7610035eb6fd5de27e
Matthias Schneider <ms@wck.biz>
2020-11-03 14:54:15 +0100

    making tests stronger

--- pkg/tcp/proxy_test.go
+++ pkg/tcp/proxy_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"strconv"
 	"testing"
 	"time"
 
@@ -82,47 +83,70 @@ func TestCloseWrite(t *testing.T) {
 }
 
 func TestProxyProtocol(t *testing.T) {
-	backendListener, err := net.Listen("tcp", ":0")
-	require.NoError(t, err)
-
-	proxybackendListener := proxyproto.Listener{
-		Listener: backendListener,
-	}
-
-	go fakeRedis(t, &proxybackendListener)
-	_, port, err := net.SplitHostPort(proxybackendListener.Addr().String())
-	require.NoError(t, err)
-
-	proxy, err := NewProxy(":"+port, 10*time.Millisecond, "2")
-	require.NoError(t, err)
-
-	proxyListener, err := net.Listen("tcp", ":0")
-	require.NoError(t, err)
-
-	go func() {
-		for {
-			conn, err := proxyListener.Accept()
+	testCases := []string{"", "1", "2"}
+	for _, proxyversion := range testCases {
+		func(proxyprotocolversion string) {
+			backendListener, err := net.Listen("tcp", ":0")
 			require.NoError(t, err)
-			proxy.ServeTCP(conn.(*net.TCPConn))
-		}
-	}()
+			proxybackendListener := proxyproto.Listener{
+				Listener: backendListener,
+				ValidateHeader: func(h *proxyproto.Header) error {
+					versionFromHeader := int(h.Version)
+					expectVersion, _ := strconv.Atoi(proxyprotocolversion)
+					if versionFromHeader != expectVersion {
+						t.Fatalf("Expected version %s got: %d", proxyprotocolversion, versionFromHeader)
+					}
+					return nil
+				},
+				Policy: func(upstream net.Addr) (proxyproto.Policy, error) {
+					switch proxyprotocolversion {
+					case "1", "2":
+						return proxyproto.USE, nil
+					case "":
+						return proxyproto.IGNORE, nil
+					default:
+						return proxyproto.REQUIRE, fmt.Errorf("fail")
+					}
+				},
+			}
+			defer proxybackendListener.Close()
 
-	_, port, err = net.SplitHostPort(proxyListener.Addr().String())
-	require.NoError(t, err)
+			go fakeRedis(t, &proxybackendListener)
+			_, port, err := net.SplitHostPort(proxybackendListener.Addr().String())
+			require.NoError(t, err)
 
-	conn, err := net.Dial("tcp", ":"+port)
-	require.NoError(t, err)
+			proxy, err := NewProxy(":"+port, 10*time.Millisecond, proxyprotocolversion)
+			require.NoError(t, err)
 
-	_, err = conn.Write([]byte("ping\n"))
-	require.NoError(t, err)
+			proxyListener, err := net.Listen("tcp", ":0")
+			require.NoError(t, err)
 
-	err = conn.(*net.TCPConn).CloseWrite()
-	require.NoError(t, err)
+			go func() {
+				for {
+					conn, err := proxyListener.Accept()
+					require.NoError(t, err)
+					proxy.ServeTCP(conn.(*net.TCPConn))
+				}
+			}()
 
-	var buf []byte
-	buffer := bytes.NewBuffer(buf)
-	n, err := io.Copy(buffer, conn)
-	require.NoError(t, err)
-	require.Equal(t, int64(4), n)
-	require.Equal(t, "PONG", buffer.String())
+			_, port, err = net.SplitHostPort(proxyListener.Addr().String())
+			require.NoError(t, err)
+
+			conn, err := net.Dial("tcp", ":"+port)
+			require.NoError(t, err)
+
+			_, err = conn.Write([]byte("ping\n"))
+			require.NoError(t, err)
+
+			err = conn.(*net.TCPConn).CloseWrite()
+			require.NoError(t, err)
+
+			var buf []byte
+			buffer := bytes.NewBuffer(buf)
+			n, err := io.Copy(buffer, conn)
+			require.NoError(t, err)
+			require.Equal(t, int64(4), n)
+			require.Equal(t, "PONG", buffer.String())
+		}(proxyversion)
+	}
 }
