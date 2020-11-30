a03483b195c72958986cabb9e09d90f944af3fb1
Harold Ozouf <harold.ozouf@gmail.com>
2020-11-30 16:14:45 +0100

    Add export struct tags on dynamic config

--- pkg/config/dynamic/udp_config.go
+++ pkg/config/dynamic/udp_config.go
@@ -8,31 +8,31 @@ import (
 
 // UDPConfiguration contains all the UDP configuration parameters.
 type UDPConfiguration struct {
-	Routers  map[string]*UDPRouter  `json:"routers,omitempty" toml:"routers,omitempty" yaml:"routers,omitempty"`
-	Services map[string]*UDPService `json:"services,omitempty" toml:"services,omitempty" yaml:"services,omitempty"`
+	Routers  map[string]*UDPRouter  `json:"routers,omitempty" toml:"routers,omitempty" yaml:"routers,omitempty" export:"true"`
+	Services map[string]*UDPService `json:"services,omitempty" toml:"services,omitempty" yaml:"services,omitempty" export:"true"`
 }
 
 // +k8s:deepcopy-gen=true
 
 // UDPService defines the configuration for a UDP service. All fields are mutually exclusive.
 type UDPService struct {
-	LoadBalancer *UDPServersLoadBalancer `json:"loadBalancer,omitempty" toml:"loadBalancer,omitempty" yaml:"loadBalancer,omitempty"`
-	Weighted     *UDPWeightedRoundRobin  `json:"weighted,omitempty" toml:"weighted,omitempty" yaml:"weighted,omitempty" label:"-"`
+	LoadBalancer *UDPServersLoadBalancer `json:"loadBalancer,omitempty" toml:"loadBalancer,omitempty" yaml:"loadBalancer,omitempty" export:"true"`
+	Weighted     *UDPWeightedRoundRobin  `json:"weighted,omitempty" toml:"weighted,omitempty" yaml:"weighted,omitempty" label:"-" export:"true"`
 }
 
 // +k8s:deepcopy-gen=true
 
 // UDPWeightedRoundRobin is a weighted round robin UDP load-balancer of services.
 type UDPWeightedRoundRobin struct {
-	Services []UDPWRRService `json:"services,omitempty" toml:"services,omitempty" yaml:"services,omitempty"`
+	Services []UDPWRRService `json:"services,omitempty" toml:"services,omitempty" yaml:"services,omitempty" export:"true"`
 }
 
 // +k8s:deepcopy-gen=true
 
 // UDPWRRService is a reference to a UDP service load-balanced with weighted round robin.
 type UDPWRRService struct {
-	Name   string `json:"name,omitempty" toml:"name,omitempty" yaml:"name,omitempty"`
-	Weight *int   `json:"weight,omitempty" toml:"weight,omitempty" yaml:"weight,omitempty"`
+	Name   string `json:"name,omitempty" toml:"name,omitempty" yaml:"name,omitempty" export:"true"`
+	Weight *int   `json:"weight,omitempty" toml:"weight,omitempty" yaml:"weight,omitempty" export:"true"`
 }
 
 // SetDefaults sets the default values for a UDPWRRService.
@@ -45,15 +45,15 @@ func (w *UDPWRRService) SetDefaults() {
 
 // UDPRouter defines the configuration for an UDP router.
 type UDPRouter struct {
-	EntryPoints []string `json:"entryPoints,omitempty" toml:"entryPoints,omitempty" yaml:"entryPoints,omitempty"`
-	Service     string   `json:"service,omitempty" toml:"service,omitempty" yaml:"service,omitempty"`
+	EntryPoints []string `json:"entryPoints,omitempty" toml:"entryPoints,omitempty" yaml:"entryPoints,omitempty" export:"true"`
+	Service     string   `json:"service,omitempty" toml:"service,omitempty" yaml:"service,omitempty" export:"true"`
 }
 
 // +k8s:deepcopy-gen=true
 
 // UDPServersLoadBalancer defines the configuration for a load-balancer of UDP servers.
 type UDPServersLoadBalancer struct {
-	Servers []UDPServer `json:"servers,omitempty" toml:"servers,omitempty" yaml:"servers,omitempty" label-slice-as-struct:"server"`
+	Servers []UDPServer `json:"servers,omitempty" toml:"servers,omitempty" yaml:"servers,omitempty" label-slice-as-struct:"server" export:"true"`
 }
 
 // Mergeable reports whether the given load-balancer can be merged with the receiver.
