53531987e0291595ac8cfa0181137b16629611d9
Guido Maria Serra <zeph@fsfe.org>
2021-02-08 19:18:26 +0100

    proposed unit test

--- pkg/middlewares/forwardedheaders/forwarded_header_test.go
+++ pkg/middlewares/forwardedheaders/forwarded_header_test.go
@@ -97,6 +97,20 @@ func TestServeHTTP(t *testing.T) {
 			},
 		},
 		{
+			desc:       "insecure false with incoming X-Forwarded headers and valid Trusted Ips -> xRealIP",
+			insecure:   false,
+			trustedIps: []string{"10.0.0.0/8"},
+			remoteAddr: "10.0.1.100:80",
+			incomingHeaders: map[string]string{
+				"X-Forwarded-for":      "172.31.1.25, 10.0.1.0, 10.0.1.12",
+			},
+			expectedHeaders: map[string]string{
+				"X-Forwarded-for":      "172.31.1.25, 10.0.1.0, 10.0.1.12",
+				xRealIP:                "172.31.1.25",
+
+			},
+		},
+		{
 			desc:       "insecure false with incoming X-Forwarded headers and invalid Trusted Ips",
 			insecure:   false,
 			trustedIps: []string{"10.0.1.100"},
