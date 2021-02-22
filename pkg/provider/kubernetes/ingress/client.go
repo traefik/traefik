122531bd4890096f2af2f80a7619c2b7279489b9
Tom Moulard <tom.moulard@traefik.io>
2021-02-22 09:45:18 +0100

    ingress: filter ingress resources by name

--- pkg/provider/kubernetes/ingress/client.go
+++ pkg/provider/kubernetes/ingress/client.go
@@ -476,3 +476,15 @@ func supportsIngressClass(serverVersion *version.Version) bool {
 
 	return ingressClassVersion.LessThanOrEqual(serverVersion)
 }
+
+// filterIngressClassByName remove ingressclass from slice
+func filterIngressClassByName(ingressClassName string, ics []*networkingv1beta1.IngressClass) []*networkingv1beta1.IngressClass {
+	var ingressClasses []*networkingv1beta1.IngressClass
+
+	for _, ic := range ics {
+		if ic.Name == ingressClassName {
+			ingressClasses = append(ingressClasses, ic)
+		}
+	}
+	return ingressClasses
+}
