360596c33a388706bbe3fcd6e46dda7121e58cc8
Tom Moulard <tom.moulard@traefik.io>
2021-02-23 11:18:01 +0100

    k8s client: fmt

--- pkg/provider/kubernetes/ingress/client.go
+++ pkg/provider/kubernetes/ingress/client.go
@@ -477,7 +477,7 @@ func supportsIngressClass(serverVersion *version.Version) bool {
 	return ingressClassVersion.LessThanOrEqual(serverVersion)
 }
 
-// filterIngressClassByName remove ingressclass from slice
+// filterIngressClassByName remove ingressclass from slice.
 func filterIngressClassByName(ingressClassName string, ics []*networkingv1beta1.IngressClass) []*networkingv1beta1.IngressClass {
 	var ingressClasses []*networkingv1beta1.IngressClass
 
