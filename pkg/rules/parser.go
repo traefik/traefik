c92cccf764e17c8b33899dc4f7b8f005ea1ba62d
Daniel Tomcej <daniel.tomcej@gmail.com>
2021-06-11 10:11:54 -0700

    add clientIP in router builder

--- pkg/rules/parser.go
+++ pkg/rules/parser.go
@@ -16,7 +16,7 @@ type treeBuilder func() *tree
 
 // ParseDomains extract domains from rule.
 func ParseDomains(rule string) ([]string, error) {
-	parser, err := newParser()
+	parser, err := newParser(false)
 	if err != nil {
 		return nil, err
 	}
@@ -37,7 +37,7 @@ func ParseDomains(rule string) ([]string, error) {
 // ParseHostSNI extracts the HostSNIs declared in a rule.
 // This is a first naive implementation used in TCP routing.
 func ParseHostSNI(rule string) ([]string, error) {
-	parser, err := newTCPParser()
+	parser, err := newParser(true)
 	if err != nil {
 		return nil, err
 	}
@@ -55,6 +55,27 @@ func ParseHostSNI(rule string) ([]string, error) {
 	return lower(parseDomain(buildTree())), nil
 }
 
+// ParseClientIP extracts the ClientIPs declared in a rule.
+// This is a first naive implementation used in TCP routing.
+func ParseClientIP(rule string) ([]string, error) {
+	parser, err := newParser(true)
+	if err != nil {
+		return nil, err
+	}
+
+	parse, err := parser.Parse(rule)
+	if err != nil {
+		return nil, err
+	}
+
+	buildTree, ok := parse.(treeBuilder)
+	if !ok {
+		return nil, errors.New("cannot parse")
+	}
+
+	return lower(parseClientIP(buildTree())), nil
+}
+
 func lower(slice []string) []string {
 	var lowerStrings []string
 	for _, value := range slice {
@@ -68,6 +89,17 @@ func parseDomain(tree *tree) []string {
 	case and, or:
 		return append(parseDomain(tree.ruleLeft), parseDomain(tree.ruleRight)...)
 	case "Host", "HostSNI":
+		return tree.value
+	default:
+		return nil
+	}
+}
+
+func parseClientIP(tree *tree) []string {
+	switch tree.matcher {
+	case and, or:
+		return append(parseDomain(tree.ruleLeft), parseDomain(tree.ruleRight)...)
+	case "ClientIP":
 		return tree.value
 	default:
 		return nil
@@ -117,56 +149,47 @@ func notFunc(elem treeBuilder) treeBuilder {
 	}
 }
 
-func newParser() (predicate.Parser, error) {
+func newParser(tcp bool) (predicate.Parser, error) {
 	parserFuncs := make(map[string]interface{})
 
-	for matcherName := range funcs {
-		matcherName := matcherName
-		fn := func(value ...string) treeBuilder {
-			return func() *tree {
-				return &tree{
-					matcher: matcherName,
-					value:   value,
+	if tcp {
+		for matcherName := range tcpFuncs {
+			matcherName := matcherName
+			fn := func(value ...string) treeBuilder {
+				return func() *tree {
+					return &tree{
+						matcher: matcherName,
+						value:   value,
+					}
 				}
 			}
+			parserFuncs[matcherName] = fn
+			parserFuncs[strings.ToLower(matcherName)] = fn
+			parserFuncs[strings.ToUpper(matcherName)] = fn
+			parserFuncs[strings.Title(strings.ToLower(matcherName))] = fn
 		}
-		parserFuncs[matcherName] = fn
-		parserFuncs[strings.ToLower(matcherName)] = fn
-		parserFuncs[strings.ToUpper(matcherName)] = fn
-		parserFuncs[strings.Title(strings.ToLower(matcherName))] = fn
+	} else {
+		for matcherName := range funcs {
+			matcherName := matcherName
+			fn := func(value ...string) treeBuilder {
+				return func() *tree {
+					return &tree{
+						matcher: matcherName,
+						value:   value,
+					}
+				}
+			}
+			parserFuncs[matcherName] = fn
+			parserFuncs[strings.ToLower(matcherName)] = fn
+			parserFuncs[strings.ToUpper(matcherName)] = fn
+			parserFuncs[strings.Title(strings.ToLower(matcherName))] = fn
+		}
 	}
-
 	return predicate.NewParser(predicate.Def{
 		Operators: predicate.Operators{
 			AND: andFunc,
 			OR:  orFunc,
 			NOT: notFunc,
-		},
-		Functions: parserFuncs,
-	})
-}
-
-func newTCPParser() (predicate.Parser, error) {
-	parserFuncs := make(map[string]interface{})
-
-	// FIXME quircky way of waiting for new rules
-	matcherName := "HostSNI"
-	fn := func(value ...string) treeBuilder {
-		return func() *tree {
-			return &tree{
-				matcher: matcherName,
-				value:   value,
-			}
-		}
-	}
-	parserFuncs[matcherName] = fn
-	parserFuncs[strings.ToLower(matcherName)] = fn
-	parserFuncs[strings.ToUpper(matcherName)] = fn
-	parserFuncs[strings.Title(strings.ToLower(matcherName))] = fn
-
-	return predicate.NewParser(predicate.Def{
-		Operators: predicate.Operators{
-			OR: orFunc,
 		},
 		Functions: parserFuncs,
 	})
