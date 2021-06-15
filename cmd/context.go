1f9b643f9748c87883e9cfce03b13e67f3c19ecc
Daniel Tomcej <daniel.tomcej@gmail.com>
2021-06-15 12:13:10 -0700

    create buffered channel

--- cmd/context.go
+++ cmd/context.go
@@ -10,7 +10,7 @@ import (
 // ContextWithSignal creates a context canceled when SIGINT or SIGTERM are notified.
 func ContextWithSignal(ctx context.Context) context.Context {
 	newCtx, cancel := context.WithCancel(ctx)
-	signals := make(chan os.Signal)
+	signals := make(chan os.Signal, 100)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-signals
