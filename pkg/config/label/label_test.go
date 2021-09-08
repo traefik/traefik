aaa42a8a73a8e01117e986e122749d54642bf860
Tom Moulard <tom.moulard@traefik.io>
2021-09-08 14:54:29 +0200

    feat: add in flight request middleware

--- pkg/config/label/label_test.go
+++ pkg/config/label/label_test.go
@@ -174,6 +174,7 @@ func TestDecodeConfiguration(t *testing.T) {
 		"traefik.http.services.Service1.loadbalancer.sticky.cookie.name":               "fui",
 
 		"traefik.tcp.middlewares.Middleware0.ipwhitelist.sourcerange":      "foobar, fiibar",
+		"traefik.tcp.middlewares.Middleware2.inflightreq.amount":           "42",
 		"traefik.tcp.routers.Router0.rule":                                 "foobar",
 		"traefik.tcp.routers.Router0.entrypoints":                          "foobar, fiibar",
 		"traefik.tcp.routers.Router0.service":                              "foobar",
@@ -234,6 +235,11 @@ func TestDecodeConfiguration(t *testing.T) {
 				"Middleware0": {
 					IPWhiteList: &dynamic.TCPIPWhiteList{
 						SourceRange: []string{"foobar", "fiibar"},
+					},
+				},
+				"Middleware2": {
+					InFlightReq: &dynamic.TCPInFlightReq{
+						Amount: 42,
 					},
 				},
 			},
@@ -717,6 +723,11 @@ func TestEncodeConfiguration(t *testing.T) {
 				"Middleware0": {
 					IPWhiteList: &dynamic.TCPIPWhiteList{
 						SourceRange: []string{"foobar", "fiibar"},
+					},
+				},
+				"Middleware2": {
+					InFlightReq: &dynamic.TCPInFlightReq{
+						Amount: 42,
 					},
 				},
 			},
@@ -1320,6 +1331,7 @@ func TestEncodeConfiguration(t *testing.T) {
 		"traefik.HTTP.Services.Service0.LoadBalancer.HealthCheck.Headers.name0":        "foobar",
 
 		"traefik.TCP.Middlewares.Middleware0.IPWhiteList.SourceRange": "foobar, fiibar",
+		"traefik.TCP.Middlewares.Middleware2.InFlightReq.Amount":      "42",
 		"traefik.TCP.Routers.Router0.Rule":                            "foobar",
 		"traefik.TCP.Routers.Router0.EntryPoints":                     "foobar, fiibar",
 		"traefik.TCP.Routers.Router0.Service":                         "foobar",
