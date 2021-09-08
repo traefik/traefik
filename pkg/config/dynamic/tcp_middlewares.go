2dbef9885a7c598d9e303606dfae03404048b181
Tom Moulard <tom.moulard@traefik.io>
2021-09-08 14:54:29 +0200

    feat: add in flight request middleware

--- pkg/config/dynamic/tcp_middlewares.go
+++ pkg/config/dynamic/tcp_middlewares.go
@@ -4,7 +4,15 @@ package dynamic
 
 // TCPMiddleware holds the TCPMiddleware configuration.
 type TCPMiddleware struct {
+	InFlightReq *TCPInFlightReq `json:"inFlightReq,omitempty" toml:"inFlightReq,omitempty" yaml:"inFlightReq,omitempty" export:"true"`
 	IPWhiteList *TCPIPWhiteList `json:"ipWhiteList,omitempty" toml:"ipWhiteList,omitempty" yaml:"ipWhiteList,omitempty" export:"true"`
+}
+
+// +k8s:deepcopy-gen=true
+
+// TCPInFlightReq limits the number of requests being processed and served concurrently.
+type TCPInFlightReq struct {
+	Amount int64 `json:"amount,omitempty" toml:"amount,omitempty" yaml:"amount,omitempty" export:"true"`
 }
 
 // +k8s:deepcopy-gen=true
--- pkg/config/label/label_test.go
+++ pkg/config/label/label_test.go
@@ -173,6 +173,7 @@ func TestDecodeConfiguration(t *testing.T) {
 		"traefik.http.services.Service1.loadbalancer.sticky.cookie.name":               "fui",
 
 		"traefik.tcp.middlewares.Middleware0.ipwhitelist.sourcerange":      "foobar, fiibar",
+		"traefik.tcp.middlewares.Middleware2.inflightreq.amount":           "42",
 		"traefik.tcp.routers.Router0.rule":                                 "foobar",
 		"traefik.tcp.routers.Router0.entrypoints":                          "foobar, fiibar",
 		"traefik.tcp.routers.Router0.service":                              "foobar",
@@ -233,6 +234,11 @@ func TestDecodeConfiguration(t *testing.T) {
 				"Middleware0": {
 					IPWhiteList: &dynamic.TCPIPWhiteList{
 						SourceRange: []string{"foobar", "fiibar"},
+					},
+				},
+				"Middleware2": {
+					InFlightReq: &dynamic.TCPInFlightReq{
+						Amount: 42,
 					},
 				},
 			},
@@ -714,6 +720,11 @@ func TestEncodeConfiguration(t *testing.T) {
 				"Middleware0": {
 					IPWhiteList: &dynamic.TCPIPWhiteList{
 						SourceRange: []string{"foobar", "fiibar"},
+					},
+				},
+				"Middleware2": {
+					InFlightReq: &dynamic.TCPInFlightReq{
+						Amount: 42,
 					},
 				},
 			},
@@ -1315,6 +1326,7 @@ func TestEncodeConfiguration(t *testing.T) {
 		"traefik.HTTP.Services.Service0.LoadBalancer.HealthCheck.Headers.name0":        "foobar",
 
 		"traefik.TCP.Middlewares.Middleware0.IPWhiteList.SourceRange": "foobar, fiibar",
+		"traefik.TCP.Middlewares.Middleware2.InFlightReq.Amount":      "42",
 		"traefik.TCP.Routers.Router0.Rule":                            "foobar",
 		"traefik.TCP.Routers.Router0.EntryPoints":                     "foobar, fiibar",
 		"traefik.TCP.Routers.Router0.Service":                         "foobar",
