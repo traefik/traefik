caec52c0d18ab31c67cfd9819f71d3a16319ef61
Tom Moulard <tom.moulard@traefik.io>
2021-10-25 14:41:09 +0200

    fix: removing checks in node_modules/ folder

--- script/validate-shell-script.sh
+++ script/validate-shell-script.sh
@@ -12,10 +12,10 @@ then
         # The shellcheck command are run in background, to have an overview of the linter (instead of a fail at first issue)
         shellcheck "${script_to_check}" &
     done < <( # Search all the repository for sh and bash shebangs, excluding .js and .md files
-        # the folders ".git" and "vendor" are also ignored
+        # the folders ".git", "vendor" and "node_modules" are also ignored
         grep -rI '#!/' "${script_dir}"/.. \
         | grep 'sh' | grep -v '.js' | grep -v '.md' \
-        | grep -v '.git/' | grep -v 'vendor/' \
+        | grep -v '.git/' | grep -v 'vendor/'  | grep -v 'node_modules/' \
         | cut -d':' -f1
     )
     wait # Wait for all background command to be completed
