2aa9def373ce95d5ed311961b5689236690d723e
Tom Moulard <tom.moulard@traefik.io>
2021-12-02 11:09:21 +0100

    review: fixing testing CheckRule

--- pkg/rules/parser_test.go
+++ pkg/rules/parser_test.go
@@ -7,14 +7,27 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// testTree = Tree + CheckErr
+type testTree struct {
+	Matcher   string
+	Not       bool
+	Value     []string
+	RuleLeft  *testTree
+	RuleRight *testTree
+
+	// CheckErr allow knowing if a Tree has a rule error.
+	CheckErr bool
+}
+
 func TestRuleMatch(t *testing.T) {
 	matchers := []string{"m"}
 	testCases := []struct {
 		desc           string
 		rule           string
-		tree           Tree
+		tree           testTree
+		matchers       []string
+		values         []string
 		expectParseErr bool
-		expectCheckErr bool
 	}{
 		{
 			desc:           "No rule",
@@ -29,11 +42,22 @@ func TestRuleMatch(t *testing.T) {
 		{
 			desc: "No value in rule",
 			rule: "m()",
-			tree: Tree{
-				Matcher: "m",
-				Value:   []string{},
+			tree: testTree{
+				Matcher:  "m",
+				Value:    []string{},
+				CheckErr: true,
 			},
-			expectCheckErr: true,
+		},
+		{
+			desc: "Empty value in rule",
+			rule: "m(``)",
+			tree: testTree{
+				Matcher:  "m",
+				Value:    []string{""},
+				CheckErr: true,
+			},
+			matchers: []string{"m"},
+			values:   []string{""},
 		},
 		{
 			desc:           "One value in rule with and",
@@ -63,26 +87,32 @@ func TestRuleMatch(t *testing.T) {
 		{
 			desc: "One value in rule",
 			rule: "m(`1`)",
-			tree: Tree{
+			tree: testTree{
 				Matcher: "m",
 				Value:   []string{"1"},
 			},
+			matchers: []string{"m"},
+			values:   []string{"1"},
 		},
 		{
 			desc: "One value in rule with superfluous parenthesis",
 			rule: "(m(`1`))",
-			tree: Tree{
+			tree: testTree{
 				Matcher: "m",
 				Value:   []string{"1"},
 			},
+			matchers: []string{"m"},
+			values:   []string{"1"},
 		},
 		{
 			desc: "Rule with CAPS matcher",
 			rule: "M(`1`)",
-			tree: Tree{
+			tree: testTree{
 				Matcher: "m",
 				Value:   []string{"1"},
 			},
+			matchers: []string{"m"},
+			values:   []string{"1"},
 		},
 		{
 			desc:           "Invalid matcher in rule",
@@ -90,89 +120,129 @@ func TestRuleMatch(t *testing.T) {
 			expectParseErr: true,
 		},
 		{
+			desc: "Invalid matchers",
+			rule: "m(`1`)",
+			tree: testTree{
+				Matcher: "m",
+				Value:   []string{"1"},
+			},
+			matchers: []string{"not-m"},
+		},
+		{
 			desc: "Two value in rule",
 			rule: "m(`1`, `2`)",
-			tree: Tree{
+			tree: testTree{
 				Matcher: "m",
 				Value:   []string{"1", "2"},
 			},
+			matchers: []string{"m"},
+			values:   []string{"1", "2"},
 		},
 		{
 			desc: "Not one value in rule",
 			rule: "!m(`1`)",
-			tree: Tree{
+			tree: testTree{
 				Matcher: "m",
 				Not:     true,
 				Value:   []string{"1"},
 			},
+			matchers: []string{"m"},
+			values:   []string{"1"},
 		},
 		{
 			desc: "Two value in rule with and",
 			rule: "m(`1`) && m(`2`)",
-			tree: Tree{
-				Matcher: "and",
-				RuleLeft: &Tree{
+			tree: testTree{
+				Matcher:  "and",
+				CheckErr: true,
+				RuleLeft: &testTree{
 					Matcher: "m",
 					Value:   []string{"1"},
 				},
-				RuleRight: &Tree{
+				RuleRight: &testTree{
 					Matcher: "m",
 					Value:   []string{"2"},
 				},
 			},
-			expectCheckErr: true,
+			matchers: []string{"m"},
+			values:   []string{"1", "2"},
 		},
 		{
 			desc: "Two value in rule with or",
 			rule: "m(`1`) || m(`2`)",
-			tree: Tree{
-				Matcher: "or",
-				RuleLeft: &Tree{
+			tree: testTree{
+				Matcher:  "or",
+				CheckErr: true,
+				RuleLeft: &testTree{
 					Matcher: "m",
 					Value:   []string{"1"},
 				},
-				RuleRight: &Tree{
+				RuleRight: &testTree{
 					Matcher: "m",
 					Value:   []string{"2"},
 				},
 			},
-			expectCheckErr: true,
+			matchers: []string{"m"},
+			values:   []string{"1", "2"},
 		},
 		{
 			desc: "Two value in rule with and negated",
 			rule: "!(m(`1`) && m(`2`))",
-			tree: Tree{
-				Matcher: "or",
-				RuleLeft: &Tree{
+			tree: testTree{
+				Matcher:  "or",
+				CheckErr: true,
+				RuleLeft: &testTree{
 					Matcher: "m",
 					Not:     true,
 					Value:   []string{"1"},
 				},
-				RuleRight: &Tree{
+				RuleRight: &testTree{
 					Matcher: "m",
 					Not:     true,
 					Value:   []string{"2"},
 				},
 			},
-			expectCheckErr: true,
+			matchers: []string{"m"},
+			values:   []string{"1", "2"},
 		},
 		{
 			desc: "Two value in rule with or negated",
 			rule: "!(m(`1`) || m(`2`))",
-			tree: Tree{
-				Matcher: "and",
-				RuleLeft: &Tree{
+			tree: testTree{
+				Matcher:  "and",
+				CheckErr: true,
+				RuleLeft: &testTree{
 					Matcher: "m",
 					Not:     true,
 					Value:   []string{"1"},
 				},
-				RuleRight: &Tree{
+				RuleRight: &testTree{
 					Matcher: "m",
 					Not:     true,
 					Value:   []string{"2"},
 				},
 			},
-			expectCheckErr: true,
+			matchers: []string{"m"},
+			values:   []string{"1", "2"},
+		},
+		{
+			desc: "No value in rule",
+			rule: "m(`1`) && m()",
+			tree: testTree{
+				Matcher:  "and",
+				CheckErr: true,
+				RuleLeft: &testTree{
+					Matcher: "m",
+					Value:   []string{"1"},
+				},
+				RuleRight: &testTree{
+					Matcher:  "m",
+					Value:    []string{},
+					CheckErr: true,
+				},
+			},
+			matchers: []string{"m"},
+			values:   []string{"1"},
 		},
 	}
 
@@ -196,15 +266,36 @@ func TestRuleMatch(t *testing.T) {
 			require.True(t, ok)
 
 			tree := treeBuilder()
+			checkEquivalence(t, &test.tree, tree)
 
-			assert.Equal(t, &test.tree, tree)
-
-			err = CheckRule(tree)
-			if test.expectCheckErr {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-			}
+			assert.Equal(t, test.values, tree.ParseMatchers(test.matchers))
 		})
+	}
+}
+
+func checkEquivalence(t *testing.T, expected *testTree, actual *Tree) {
+	t.Helper()
+
+	if actual == nil {
+		return
+	}
+
+	if actual.RuleLeft != nil {
+		checkEquivalence(t, expected.RuleLeft, actual.RuleLeft)
+	}
+
+	if actual.RuleRight != nil {
+		checkEquivalence(t, expected.RuleRight, actual.RuleRight)
+	}
+
+	assert.Equal(t, expected.Matcher, actual.Matcher)
+	assert.Equal(t, expected.Not, actual.Not)
+	assert.Equal(t, expected.Value, actual.Value)
+
+	t.Logf("%+v -> %v", actual, CheckRule(actual))
+	if expected.CheckErr {
+		assert.Error(t, CheckRule(actual))
+	} else {
+		assert.NoError(t, CheckRule(actual))
 	}
 }
