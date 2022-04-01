8fb7e89954d71bd0c5406b436ee7b6ddc42aff1d
Adrian Lai <adrian-lai@cookpad.jp>
2022-04-01 12:42:02 +0100

    Allow config of additonal CircuitBreaker params
    Allow the configuration of the other circuit breaker params mentioned in
    the documentation:
     * CheckPeriod
     * FallbackDuration
     * Recover(ing|y)Duration
    
    Rename RecoveringDuration to RecoveryDuration to match the underlying
    library.

--- pkg/config/label/label_test.go
+++ pkg/config/label/label_test.go
@@ -983,7 +983,10 @@ func TestEncodeConfiguration(t *testing.T) {
 				},
 				"Middleware4": {
 					CircuitBreaker: &dynamic.CircuitBreaker{
-						Expression: "foobar",
+						Expression:       "foobar",
+						CheckPeriod:      ptypes.Duration(100 * time.Millisecond),
+						FallbackDuration: ptypes.Duration(10 * time.Second),
+						RecoveryDuration: ptypes.Duration(10 * time.Second),
 					},
 				},
 				"Middleware5": {
@@ -1191,6 +1194,9 @@ func TestEncodeConfiguration(t *testing.T) {
 		"traefik.HTTP.Middlewares.Middleware2.Buffering.RetryExpression":                           "foobar",
 		"traefik.HTTP.Middlewares.Middleware3.Chain.Middlewares":                                   "foobar, fiibar",
 		"traefik.HTTP.Middlewares.Middleware4.CircuitBreaker.Expression":                           "foobar",
+		"traefik.HTTP.Middlewares.Middleware4.CircuitBreaker.CheckPeriod":                          "100000000",   // 100ms in ns
+		"traefik.HTTP.Middlewares.Middleware4.CircuitBreaker.FallbackDuration":                     "10000000000", // 10s in ns
+		"traefik.HTTP.Middlewares.Middleware4.CircuitBreaker.RecoveryDuration":                     "10000000000", // 10s in ns
 		"traefik.HTTP.Middlewares.Middleware5.DigestAuth.HeaderField":                              "foobar",
 		"traefik.HTTP.Middlewares.Middleware5.DigestAuth.Realm":                                    "foobar",
 		"traefik.HTTP.Middlewares.Middleware5.DigestAuth.RemoveHeader":                             "true",
