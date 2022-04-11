fd7fd924838943c9efb090e6e1e7104179044c53
Tom Moulard <tom.moulard@traefik.io>
2022-04-11 17:24:15 +0200

    Chore: linting lint.sh

--- docs/scripts/lint.sh
+++ docs/scripts/lint.sh
@@ -12,15 +12,15 @@ echo "== Linting Markdown"
 # Uses the file ".markdownlint.json" for setup
 cd "${BASE_DIR}" || exit 1
 
-LINTER_EXCLUSIONS="$(find "${BASE_DIR}/content" -type f -name '.markdownlint.json')" \
+LINTER_EXCLUSIONS="$(find "${BASE_DIR}/content" -type f -name '.markdownlint.json')"
 GLOBAL_LINT_OPTIONS="--config ${BASE_DIR}/.markdownlint.json"
 
 # Lint the specific folders (containing linter specific rulesets)
 for LINTER_EXCLUSION in ${LINTER_EXCLUSIONS}
 do
-    markdownlint --config "${LINTER_EXCLUSION}" "$(dirname "${LINTER_EXCLUSION}")" || EXIT_CODE=1
-    # Add folder to the ignore list for global lint
-    GLOBAL_LINT_OPTIONS="${GLOBAL_LINT_OPTIONS} --ignore=$(dirname "${LINTER_EXCLUSION}")"
+	markdownlint --config "${LINTER_EXCLUSION}" "$(dirname "${LINTER_EXCLUSION}")" || EXIT_CODE=1
+	# Add folder to the ignore list for global lint
+	GLOBAL_LINT_OPTIONS="${GLOBAL_LINT_OPTIONS} --ignore=$(dirname "${LINTER_EXCLUSION}")"
 done
 
 # Lint all the content, excluding the previously done`
