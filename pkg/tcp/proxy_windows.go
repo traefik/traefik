5343d75233ce54b72c710834a10e9ad5c064eab2
Romain <rtribotte@users.noreply.github.com>
2022-05-13 15:39:11 +0200

    Apply suggestions from code review
    Co-authored-by: Tom Moulard <tom@moulard.org>

--- pkg/tcp/proxy_windows.go
+++ pkg/tcp/proxy_windows.go
@@ -12,9 +12,5 @@ import (
 // isReadConnResetError reports whether err is a connection reset error during a read operation.
 func isReadConnResetError(err error) bool {
 	var oerr *net.OpError
-	if errors.As(err, &oerr) && oerr.Op == "read" {
-		return errors.Is(err, syscall.WSAECONNRESET)
-	}
-
-	return false
+	return errors.As(err, &oerr) && oerr.Op == "read" && errors.Is(err, syscall.WSAECONNRESET)
 }
