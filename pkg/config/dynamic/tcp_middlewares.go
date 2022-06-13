19d9726c74b1cf777402be307d418a45c95b2b84
Michel Loiseleur <michel.loiseleur@traefik.io>
2022-06-13 12:09:44 +0200

    Add description on all properties of Middleware & MiddlewareTCP

--- pkg/config/dynamic/tcp_middlewares.go
+++ pkg/config/dynamic/tcp_middlewares.go
@@ -2,7 +2,7 @@ package dynamic
 
 // +k8s:deepcopy-gen=true
 
-// TCPMiddleware holds the TCPMiddleware configuration.
+// MiddlewareTCP is the CRD implementation of a Traefik TCP middleware. More info: https://doc.traefik.io/traefik/middlewares/overview/
 type TCPMiddleware struct {
 	InFlightConn *TCPInFlightConn `json:"inFlightConn,omitempty" toml:"inFlightConn,omitempty" yaml:"inFlightConn,omitempty" export:"true"`
 	IPWhiteList  *TCPIPWhiteList  `json:"ipWhiteList,omitempty" toml:"ipWhiteList,omitempty" yaml:"ipWhiteList,omitempty" export:"true"`
@@ -10,14 +10,16 @@ type TCPMiddleware struct {
 
 // +k8s:deepcopy-gen=true
 
-// TCPInFlightConn holds the TCP in flight connection configuration.
+// To proactively prevent services from being overwhelmed with high load, the number of allowed simultaneous connections by IP can be limited. More info: https://doc.traefik.io/traefik/middlewares/tcp/inflightconn/
 type TCPInFlightConn struct {
+	// The amount option defines the maximum amount of allowed simultaneous connections. The middleware closes the connection if there are already amount connections opened.
 	Amount int64 `json:"amount,omitempty" toml:"amount,omitempty" yaml:"amount,omitempty" export:"true"`
 }
 
 // +k8s:deepcopy-gen=true
 
-// TCPIPWhiteList holds the TCP ip white list configuration.
+// IPWhitelist accepts / refuses connections based on the client IP.
 type TCPIPWhiteList struct {
+	// The sourceRange option sets the allowed IPs (or ranges of allowed IPs by using CIDR notation).
 	SourceRange []string `json:"sourceRange,omitempty" toml:"sourceRange,omitempty" yaml:"sourceRange,omitempty"`
 }
