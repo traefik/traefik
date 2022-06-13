cf7d834cac2c3e795bc0720897ca7854f5491385
Michel Loiseleur <michel.loiseleur@traefik.io>
2022-06-13 17:31:41 +0200

    Fix linter & gofmt -s issues

--- pkg/provider/kubernetes/crd/traefik/v1alpha1/tlsoption.go
+++ pkg/provider/kubernetes/crd/traefik/v1alpha1/tlsoption.go
@@ -21,21 +21,21 @@ type TLSOption struct {
 // TLSOptionSpec defines the desired state of TLSOption.
 type TLSOptionSpec struct {
 	// Defines the minimum TLS version that Traefik will accept. Possible values: VersionTLS10, VersionTLS11, VersionTLS12, VersionTLS13. Default: VersionTLS10
-	MinVersion               string     `json:"minVersion,omitempty"`
+	MinVersion string `json:"minVersion,omitempty"`
 	// Defines the maximum TLS version that Traefik will accept. Possible values: VersionTLS10, VersionTLS11, VersionTLS12, VersionTLS13. Default: None.
-	MaxVersion               string     `json:"maxVersion,omitempty"`
+	MaxVersion string `json:"maxVersion,omitempty"`
 	// List of supported cipher suites for TLS versions up to TLS 1.2. More info: https://doc.traefik.io/traefik/https/tls/#cipher-suites
-	CipherSuites             []string   `json:"cipherSuites,omitempty"`
+	CipherSuites []string `json:"cipherSuites,omitempty"`
 	// This option allows to set the preferred elliptic curves in a specific order. More info: https://doc.traefik.io/traefik/https/tls/#curve-preferences
-	CurvePreferences         []string   `json:"curvePreferences,omitempty"`
-	ClientAuth               ClientAuth `json:"clientAuth,omitempty"`
+	CurvePreferences []string   `json:"curvePreferences,omitempty"`
+	ClientAuth       ClientAuth `json:"clientAuth,omitempty"`
 	// If true, Traefik won't allow connections from clients connections that do not specify a server_name extension. Default: false.
-	SniStrict                bool       `json:"sniStrict,omitempty"`
+	SniStrict bool `json:"sniStrict,omitempty"`
 	// This option allows the server to choose its most preferred cipher suite instead of the client's.
 	// It is enabled automatically when minVersion or maxVersion are set.
-	PreferServerCipherSuites bool       `json:"preferServerCipherSuites,omitempty"`
+	PreferServerCipherSuites bool `json:"preferServerCipherSuites,omitempty"`
 	// List of supported application level protocols for the TLS handshake, in order of preference. More info: https://doc.traefik.io/traefik/https/tls/#alpn-protocols
-	ALPNProtocols            []string   `json:"alpnProtocols,omitempty"`
+	ALPNProtocols []string `json:"alpnProtocols,omitempty"`
 }
 
 // +k8s:deepcopy-gen=true
