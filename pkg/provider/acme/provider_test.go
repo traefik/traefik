cc7b4e517aea692f41f7cd16e406514a95f41853
kevinpollet <pollet.kevin@gmail.com>
2022-08-03 10:21:36 +0200

    review: lint

--- pkg/provider/acme/provider_test.go
+++ pkg/provider/acme/provider_test.go
@@ -15,13 +15,13 @@ import (
 func TestGetUncheckedCertificates(t *testing.T) {
 	t.Skip("Needs TLS Manager")
 	wildcardMap := make(map[string][]*tls.Certificate)
-	wildcardMap["*.traefik.wtf"] = []*tls.Certificate{&tls.Certificate{}}
+	wildcardMap["*.traefik.wtf"] = []*tls.Certificate{{}}
 
 	wildcardSafe := &safe.Safe{}
 	wildcardSafe.Set(wildcardMap)
 
 	domainMap := make(map[string][]*tls.Certificate)
-	domainMap["traefik.wtf"] = []*tls.Certificate{&tls.Certificate{}}
+	domainMap["traefik.wtf"] = []*tls.Certificate{{}}
 
 	domainSafe := &safe.Safe{}
 	domainSafe.Set(domainMap)
