763b347dde84878389ececb3b51d8f83da95701f
mpl <mathieu.lonjaret@gmail.com>
2022-09-06 11:50:01 +0200

    Apply suggestions from code review

--- integration/retry_test.go
+++ integration/retry_test.go
@@ -39,7 +39,7 @@ func (s *RetrySuite) TestRetry(c *check.C) {
 	response, err := http.Get("http://127.0.0.1:8000/")
 	c.Assert(err, checker.IsNil)
 
-	// The test only verifies that the retry middleware allows finally to reach the working service.
+	// The test only verifies that the retry middleware makes sure that the working service is eventually reached.
 	c.Assert(response.StatusCode, checker.Equals, http.StatusOK)
 }
 
@@ -76,12 +76,12 @@ func (s *RetrySuite) TestRetryWebsocket(c *check.C) {
 	err = try.GetRequest("http://127.0.0.1:8080/api/rawdata", 60*time.Second, try.BodyContains("PathPrefix(`/`)"))
 	c.Assert(err, checker.IsNil)
 
-	// The test only verifies that the retry middleware allows finally to reach the working service.
+	// The test only verifies that the retry middleware makes sure that the working service is eventually reached.
 	_, response, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:8000/echo", nil)
 	c.Assert(err, checker.IsNil)
 	c.Assert(response.StatusCode, checker.Equals, http.StatusSwitchingProtocols)
 
-	// The test only verifies that the retry middleware allows finally to reach the working service (a second time).
+	// The test verifies a second time that the working service is eventually reached.
 	_, response, err = websocket.DefaultDialer.Dial("ws://127.0.0.1:8000/echo", nil)
 	c.Assert(err, checker.IsNil)
 	c.Assert(response.StatusCode, checker.Equals, http.StatusSwitchingProtocols)
