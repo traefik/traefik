871dc36b29d46cd5d4ee83d10b19710276708f96
Fernandez Ludovic <ldez@users.noreply.github.com>
2022-09-13 18:07:56 +0200

    chore: remove Pilot support

--- pkg/config/static/pilot.go
+++ pkg/config/static/pilot.go
@@ -6,8 +6,3 @@ type Pilot struct {
 	Token     string `description:"Traefik Pilot token." json:"token,omitempty" toml:"token,omitempty" yaml:"token,omitempty" loggable:"false"`
 	Dashboard bool   `description:"Enable Traefik Pilot in the dashboard." json:"dashboard,omitempty" toml:"dashboard,omitempty" yaml:"dashboard,omitempty"`
 }
-
-// SetDefaults sets the default values.
-func (p *Pilot) SetDefaults() {
-	p.Dashboard = true
-}
