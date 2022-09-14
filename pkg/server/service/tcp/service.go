788f8fa951ee215d0d9bd3d43299e400f015e2c1
Qi <qmlfighting@gmail.com>
2022-09-14 20:42:08 +0800

    Make the loadbalancers servers order random
    Co-authored-by: Mathieu Lonjaret <mathieu.lonjaret@gmail.com>
    Co-authored-by: Romain <rtribotte@users.noreply.github.com>

--- pkg/server/service/tcp/service.go
+++ pkg/server/service/tcp/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math/rand"
 	"net"
 	"time"
 
@@ -16,12 +17,14 @@ import (
 // Manager is the TCPHandlers factory.
 type Manager struct {
 	configs map[string]*runtime.TCPServiceInfo
+	rand    *rand.Rand // For the initial shuffling of load-balancers.
 }
 
 // NewManager creates a new manager.
 func NewManager(conf *runtime.Configuration) *Manager {
 	return &Manager{
 		configs: conf.TCPServices,
+		rand:    rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
 }
 
@@ -53,7 +56,7 @@ func (m *Manager) BuildTCP(rootCtx context.Context, serviceName string) (tcp.Han
 		}
 		duration := time.Duration(*conf.LoadBalancer.TerminationDelay) * time.Millisecond
 
-		for name, server := range conf.LoadBalancer.Servers {
+		for name, server := range shuffle(conf.LoadBalancer.Servers, m.rand) {
 			if _, _, err := net.SplitHostPort(server.Address); err != nil {
 				logger.Errorf("In service %q: %v", serviceQualifiedName, err)
 				continue
@@ -71,7 +74,8 @@ func (m *Manager) BuildTCP(rootCtx context.Context, serviceName string) (tcp.Han
 		return loadBalancer, nil
 	case conf.Weighted != nil:
 		loadBalancer := tcp.NewWRRLoadBalancer()
-		for _, service := range conf.Weighted.Services {
+
+		for _, service := range shuffle(conf.Weighted.Services, m.rand) {
 			handler, err := m.BuildTCP(rootCtx, service.Name)
 			if err != nil {
 				logger.Errorf("In service %q: %v", serviceQualifiedName, err)
@@ -85,4 +89,12 @@ func (m *Manager) BuildTCP(rootCtx context.Context, serviceName string) (tcp.Han
 		conf.AddError(err, true)
 		return nil, err
 	}
+}
+
+func shuffle[T any](values []T, r *rand.Rand) []T {
+	shuffled := make([]T, len(values))
+	copy(shuffled, values)
+	r.Shuffle(len(shuffled), func(i, j int) { shuffled[i], shuffled[j] = shuffled[j], shuffled[i] })
+
+	return shuffled
 }
