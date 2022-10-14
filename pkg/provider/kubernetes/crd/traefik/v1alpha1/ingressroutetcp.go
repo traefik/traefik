cdf86b517b60a18800ed94fad22d674e0b089fbc
Simon Delicata <simon.delicata@traefik.io>
2022-10-14 16:01:45 +0200

    Add Kubernetes CRDs

--- pkg/provider/kubernetes/crd/traefik/v1alpha1/ingressroutetcp.go
+++ pkg/provider/kubernetes/crd/traefik/v1alpha1/ingressroutetcp.go
@@ -78,6 +78,10 @@ type ServiceTCP struct {
 	// ProxyProtocol defines the PROXY protocol configuration.
 	// More info: https://doc.traefik.io/traefik/v2.9/routing/services/#proxy-protocol
 	ProxyProtocol *dynamic.ProxyProtocol `json:"proxyProtocol,omitempty"`
+	// ServersTransport defines the name of ServersTransport resource to use.
+	// It allows to configure the transport between Traefik and your servers.
+	// Can only be used on a Kubernetes Service.
+	ServersTransport string `json:"serversTransport,omitempty"`
 }
 
 // +genclient
