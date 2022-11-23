08279047ae93c908d6ffa1b6b35d24d525eaf6b5
Ludovic Fernandez <ldez@users.noreply.github.com>
2022-11-23 12:14:04 +0100

    Improve test logger assertions

--- pkg/logs/oxy_test.go
+++ pkg/logs/oxy_test.go
@@ -1,17 +1,24 @@
 package logs
 
 import (
+	"bytes"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewOxyWrapper(t *testing.T) {
-	out := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}
+	buf := bytes.NewBuffer(nil)
+	cwb := zerolog.ConsoleWriter{Out: buf, TimeFormat: time.RFC3339, NoColor: true}
+
+	out := zerolog.MultiLevelWriter(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}, cwb)
 
 	logger := NewOxyWrapper(zerolog.New(out).With().Caller().Logger())
 
 	logger.Info("foo")
+
+	assert.Equal(t, "<nil> INF oxy_test.go:21 > foo\n", buf.String())
 }
