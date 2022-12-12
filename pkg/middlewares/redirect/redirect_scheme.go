d85811546c4debf27f66ba10a8daa3f0def04473
Fernandez Ludovic <ldez@users.noreply.github.com>
2022-12-12 11:25:55 +0100

    add log

--- pkg/middlewares/redirect/redirect_scheme.go
+++ pkg/middlewares/redirect/redirect_scheme.go
@@ -14,9 +14,15 @@ import (
 
 const (
 	typeSchemeName  = "RedirectScheme"
-	uriPattern      = `^(https?:\/\/)?(\[[\w:.]+\]|[\w\._-]+)?(:\d+)?(.*)$`
+	uriPattern      = `^(https?:\/\/)(\[[\w:.]+\]|[\w\._-]+)?(:\d+)?(.*)$`
 	xForwardedProto = "X-Forwarded-Proto"
 )
+
+type redirectScheme struct {
+	http.Handler
+
+	name string
+}
 
 // NewRedirectScheme creates a new RedirectScheme middleware.
 func NewRedirectScheme(ctx context.Context, next http.Handler, conf dynamic.RedirectScheme, name string) (http.Handler, error) {
@@ -33,10 +39,19 @@ func NewRedirectScheme(ctx context.Context, next http.Handler, conf dynamic.Redi
 		port = ":" + conf.Port
 	}
 
-	return newRedirect(next, uriPattern, conf.Scheme+"://${2}"+port+"${4}", conf.Permanent, clientRequestURL, name)
+	rs := &redirectScheme{name: name}
+
+	handler, err := newRedirect(next, uriPattern, conf.Scheme+"://${2}"+port+"${4}", conf.Permanent, rs.clientRequestURL, name)
+	if err != nil {
+		return nil, err
+	}
+
+	rs.Handler = handler
+
+	return rs, nil
 }
 
-func clientRequestURL(req *http.Request) string {
+func (r *redirectScheme) clientRequestURL(req *http.Request) string {
 	scheme := schemeHTTP
 	host, port, err := net.SplitHostPort(req.Host)
 	if err != nil {
@@ -75,6 +90,8 @@ func clientRequestURL(req *http.Request) string {
 			scheme = schemeHTTP
 		case strings.EqualFold(xProto, "wss"), strings.EqualFold(xProto, schemeHTTPS):
 			scheme = schemeHTTPS
+		default:
+			log.FromContext(middlewares.GetLoggerCtx(req.Context(), r.name, typeSchemeName)).Debugf("invalid X-Forwarded-Proto: %s", xProto)
 		}
 	}
 
