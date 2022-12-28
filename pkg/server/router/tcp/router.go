8b585063933c92606f0ac28e7dec5842ca5731f5
romain <rtribotte@users.noreply.github.com>
2022-12-28 12:10:13 +0100

    review: compute effective priority only once

--- pkg/server/router/tcp/router.go
+++ pkg/server/router/tcp/router.go
@@ -202,7 +202,7 @@ func (r *Router) ServeTCP(conn tcp.WriteCloser) {
 }
 
 // AddRoute defines a handler for the given rule.
-func (r *Router) AddRoute(rule string, priority int, target tcp.Handler) error {
+func (r *Router) AddRoute(rule string, priority int, target tcp.Handler) (int, error) {
 	return r.muxerTCP.AddRoute(rule, priority, target)
 }
 
@@ -268,7 +268,7 @@ func (r *Router) SetHTTPSForwarder(handler tcp.Handler) {
 
 		// muxerHTTPS only contains single HostSNI rules (and no other kind of rules),
 		// so there's no need for specifying a priority for them.
-		err := r.muxerHTTPS.AddRoute("HostSNI(`"+sniHost+"`)", 0, tcpHandler)
+		_, err := r.muxerHTTPS.AddRoute("HostSNI(`"+sniHost+"`)", 0, tcpHandler)
 		if err != nil {
 			log.Error().Err(err).Msg("Error while adding route for host")
 		}
