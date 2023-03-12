a49ba628a48f660e26909fd1ae14f33b6406aad1
Thomas Quinot <thomas@quinot.org>
2023-03-12 01:50:23 +0100

    Expose ContainerName in Docker provider

--- pkg/provider/docker/config.go
+++ pkg/provider/docker/config.go
@@ -74,11 +74,13 @@ func (p *Provider) buildConfiguration(ctx context.Context, containersInspected [
 		serviceName := getServiceName(container)
 
 		model := struct {
-			Name   string
-			Labels map[string]string
+			Name          string
+			ContainerName string
+			Labels        map[string]string
 		}{
-			Name:   serviceName,
-			Labels: container.Labels,
+			Name:          serviceName,
+			ContainerName: provider.Normalize(container.ServiceName),
+			Labels:        container.Labels,
 		}
 
 		provider.BuildRouterConfiguration(ctx, confFromLabel.HTTP, serviceName, p.defaultRuleTpl, model)
