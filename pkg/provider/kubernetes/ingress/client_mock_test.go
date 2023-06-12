cda7eabd529214841aede5e399594240c2c48513
romain <rtribotte@users.noreply.github.com>
2023-06-12 16:08:53 +0200

    remove networking.k8s.io/v1beta1 support.

--- pkg/provider/kubernetes/ingress/client_mock_test.go
+++ pkg/provider/kubernetes/ingress/client_mock_test.go
@@ -4,11 +4,9 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/hashicorp/go-version"
 	"github.com/traefik/traefik/v3/pkg/provider/kubernetes/k8s"
 	corev1 "k8s.io/api/core/v1"
 	netv1 "k8s.io/api/networking/v1"
-	netv1beta1 "k8s.io/api/networking/v1beta1"
 )
 
 var _ Client = (*clientMock)(nil)
@@ -20,8 +18,6 @@ type clientMock struct {
 	endpoints      []*corev1.Endpoints
 	ingressClasses []*netv1.IngressClass
 
-	serverVersion *version.Version
-
 	apiServiceError       error
 	apiSecretError        error
 	apiEndpointsError     error
@@ -30,10 +26,8 @@ type clientMock struct {
 	watchChan chan interface{}
 }
 
-func newClientMock(serverVersion string, paths ...string) clientMock {
+func newClientMock(paths ...string) clientMock {
 	c := clientMock{}
-
-	c.serverVersion = version.Must(version.NewVersion(serverVersion))
 
 	for _, path := range paths {
 		yamlContent, err := os.ReadFile(path)
@@ -50,21 +44,8 @@ func newClientMock(serverVersion string, paths ...string) clientMock {
 				c.secrets = append(c.secrets, o)
 			case *corev1.Endpoints:
 				c.endpoints = append(c.endpoints, o)
-			case *netv1beta1.Ingress:
-				ing, err := convert[netv1.Ingress](o)
-				if err != nil {
-					panic(err)
-				}
-				addServiceFromV1Beta1(ing, *o)
-				c.ingresses = append(c.ingresses, ing)
 			case *netv1.Ingress:
 				c.ingresses = append(c.ingresses, o)
-			case *netv1beta1.IngressClass:
-				ic, err := convert[netv1.IngressClass](o)
-				if err != nil {
-					panic(err)
-				}
-				c.ingressClasses = append(c.ingressClasses, ic)
 			case *netv1.IngressClass:
 				c.ingressClasses = append(c.ingressClasses, o)
 			default:
@@ -78,10 +59,6 @@ func newClientMock(serverVersion string, paths ...string) clientMock {
 
 func (c clientMock) GetIngresses() []*netv1.Ingress {
 	return c.ingresses
-}
-
-func (c clientMock) GetServerVersion() *version.Version {
-	return c.serverVersion
 }
 
 func (c clientMock) GetService(namespace, name string) (*corev1.Service, bool, error) {
