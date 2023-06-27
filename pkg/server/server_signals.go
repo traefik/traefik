8594ac341540498e3ca787080dbb74e020ff7106
So Koide <So.Koide@morganstanley.com>
2023-06-27 11:22:45 +0900

    feat: reload file config on SIGHUP
    Co-Authored-By: sandy2008 <Yuxuan.Chen@morganstanley.com>

--- pkg/server/server_signals.go
+++ pkg/server/server_signals.go
@@ -12,7 +12,7 @@ import (
 )
 
 func (s *Server) configureSignals() {
-	signal.Notify(s.signals, syscall.SIGUSR1)
+	signal.Notify(s.signals, syscall.SIGUSR1, syscall.SIGHUP)
 }
 
 func (s *Server) listenSignals(ctx context.Context) {
@@ -21,13 +21,21 @@ func (s *Server) listenSignals(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		case sig := <-s.signals:
-			if sig == syscall.SIGUSR1 {
+			switch sig {
+			case syscall.SIGUSR1:
 				log.Info().Msgf("Closing and re-opening log files for rotation: %+v", sig)
 
 				if s.accessLoggerMiddleware != nil {
 					if err := s.accessLoggerMiddleware.Rotate(); err != nil {
 						log.Error().Err(err).Msg("Error rotating access log")
 					}
+				}
+			case syscall.SIGHUP:
+				log.Info().Msgf("%+v: Reloading provider configurations", sig)
+				// Assuming that s.watcher is an instance of ConfigurationWatcher
+				// and that it has a method to reload all reloadable providers.
+				if err := s.watcher.ReloadAllProviders(); err != nil {
+					log.Error().Err(err).Msg("Error reloading provider configurations")
 				}
 			}
 		}
