49961af09d2f091401cf3ce8da626da79008dfe1
Jesse Haka <haka.jesse@gmail.com>
2023-10-26 10:48:59 +0300

    make Build more readable

--- pkg/plugins/middlewares.go
+++ pkg/plugins/middlewares.go
@@ -23,21 +23,21 @@ func (b Builder) Build(pName string, config map[string]interface{}, middlewareNa
 		return nil, fmt.Errorf("no plugin definitions in the static configuration: %s", pName)
 	}
 
-	// plugin (pName) can be located in yaegi or wasm middleware builders
-	yaegiDescriptor, ok := b.yaegiMiddlewareBuilders[pName]
-	if !ok {
-		wasmDescriptor, ok := b.wasmMiddlewareBuilders[pName]
-		if !ok {
-			return nil, fmt.Errorf("unknown plugin type: %s", pName)
+	// plugin (pName) can be located in yaegi middleware builders.
+	if yaegiDescriptor, ok := b.yaegiMiddlewareBuilders[pName]; ok {
+		m, err := newYaegiMiddleware(yaegiDescriptor, config, middlewareName)
+		if err != nil {
+			return nil, err
 		}
-		m := newWasmMiddleware(wasmDescriptor, config, middlewareName)
 		return m.NewHandler, nil
 	}
-	m, err := newYaegiMiddleware(yaegiDescriptor, config, middlewareName)
-	if err != nil {
-		return nil, err
+
+	// Or in wasm middleware builders.
+	if wasmDescriptor, ok := b.wasmMiddlewareBuilders[pName]; ok {
+		return newWasmMiddleware(wasmDescriptor, config, middlewareName).NewHandler, nil
 	}
-	return m.NewHandler, nil
+
+	return nil, fmt.Errorf("unknown plugin type: %s", pName)
 }
 
 type yaegiMiddlewareBuilder struct {
