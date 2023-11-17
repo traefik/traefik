2cb8dcc5661f59d520bb711977d93d28347dd0c6
Landry Benguigui <lbenguigui@gmail.com>
2023-11-17 11:03:03 +0100

    Deprecate IPWhiteList middleware in favor of IPAllowList

--- pkg/provider/kubernetes/crd/traefikio/v1alpha1/middleware.go
+++ pkg/provider/kubernetes/crd/traefikio/v1alpha1/middleware.go
@@ -33,6 +33,7 @@ type MiddlewareSpec struct {
 	ReplacePathRegex  *dynamic.ReplacePathRegex  `json:"replacePathRegex,omitempty"`
 	Chain             *Chain                     `json:"chain,omitempty"`
 	IPWhiteList       *dynamic.IPWhiteList       `json:"ipWhiteList,omitempty"`
+	IPAllowList       *dynamic.IPAllowList       `json:"ipAllowList,omitempty"`
 	Headers           *dynamic.Headers           `json:"headers,omitempty"`
 	Errors            *ErrorPage                 `json:"errors,omitempty"`
 	RateLimit         *RateLimit                 `json:"rateLimit,omitempty"`
