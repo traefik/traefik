367559f254bb47946bc9827fc0fdf4edf2fd5fc6
romain <rtribotte@users.noreply.github.com>
2024-01-18 11:51:01 +0100

    review

--- pkg/cli/deprecation_test.go
+++ pkg/cli/deprecation_test.go
@@ -3,330 +3,296 @@ package cli
 import (
 	"testing"
 
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/traefik/paerser/cli"
 	"github.com/traefik/traefik/v3/cmd"
 )
 
-// func TestDeprecationNotice(t *testing.T) {
-// 	tests := []struct {
-// 		desc         string
-// 		config       rawConfiguration
-// 		logLevel     zerolog.Level
-// 		incompatible assert.BoolAssertionFunc
-// 	}{
-// 		{
-// 			desc: "Docker provider swarmMode option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"docker": map[string]bool{
-// 						"swarmMode": true,
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Docker provider tls.CAOptional option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"docker": map[string]interface{}{
-// 						"tls": map[string]bool{
-// 							"caOptional": true,
-// 						},
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Swarm provider tls.CAOptional option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"swarm": map[string]interface{}{
-// 						"tls": map[string]bool{
-// 							"caOptional": true,
-// 						},
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Consul provider namespace option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"consul": map[string]string{
-// 						"namespace": "myNamespace",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Consul provider tls.CAOptional option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"consul": map[string]interface{}{
-// 						"tls": map[string]bool{
-// 							"caOptional": true,
-// 						},
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "ConsulCatalog provider namespace option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"consulCatalog": map[string]string{
-// 						"namespace": "myNamespace",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "ConsulCatalog provider tls.CAOptional option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"consulCatalog": map[string]interface{}{
-// 						"endpoint": map[string]interface{}{
-// 							"tls": map[string]bool{
-// 								"caOptional": true,
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Nomad provider namespace option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"nomad": map[string]string{
-// 						"namespace": "myNamespace",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Nomad provider tls.CAOptional option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"nomad": map[string]interface{}{
-// 						"endpoint": map[string]interface{}{
-// 							"tls": map[string]bool{
-// 								"caOptional": true,
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Marathon configuration is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"marathon": map[string]string{
-// 						"foo": "bar",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Rancher configuration is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"rancher": map[string]string{
-// 						"foo": "bar",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "ETCD provider tls.CAOptional option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"etcd": map[string]interface{}{
-// 						"tls": map[string]bool{
-// 							"caOptional": true,
-// 						},
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Redis provider tls.CAOptional option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"redis": map[string]interface{}{
-// 						"tls": map[string]bool{
-// 							"caOptional": true,
-// 						},
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "HTTP provider tls.CAOptional option is incompatible",
-// 			config: map[string]interface{}{
-// 				"providers": map[string]interface{}{
-// 					"http": map[string]interface{}{
-// 						"tls": map[string]bool{
-// 							"caOptional": true,
-// 						},
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Pilot configuration is incompatible",
-// 			config: map[string]interface{}{
-// 				"pilot": map[string]string{
-// 					"foo": "bar",
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Experimental HTTP3 enablement configuration is incompatible",
-// 			config: map[string]interface{}{
-// 				"experimental": map[string]interface{}{
-// 					"http3": true,
-// 				},
-// 			},
-// 			logLevel:     zerolog.WarnLevel,
-// 			incompatible: assert.False,
-// 		},
-// 		{
-// 			desc: "Tracing SpanNameLimit option is incompatible",
-// 			config: map[string]interface{}{
-// 				"tracing": map[string]interface{}{
-// 					"spanNameLimit": 42,
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Tracing Jaeger configuration is incompatible",
-// 			config: map[string]interface{}{
-// 				"tracing": map[string]interface{}{
-// 					"jaeger": map[string]interface{}{
-// 						"foo": "bar",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Tracing Zipkin configuration is incompatible",
-// 			config: map[string]interface{}{
-// 				"tracing": map[string]interface{}{
-// 					"zipkin": map[string]interface{}{
-// 						"foo": "bar",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Tracing Datadog configuration is incompatible",
-// 			config: map[string]interface{}{
-// 				"tracing": map[string]interface{}{
-// 					"datadog": map[string]interface{}{
-// 						"foo": "bar",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Tracing Instana configuration is incompatible",
-// 			config: map[string]interface{}{
-// 				"tracing": map[string]interface{}{
-// 					"instana": map[string]interface{}{
-// 						"foo": "bar",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Tracing Haystack configuration is incompatible",
-// 			config: map[string]interface{}{
-// 				"tracing": map[string]interface{}{
-// 					"haystack": map[string]interface{}{
-// 						"foo": "bar",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 		{
-// 			desc: "Tracing Elastic configuration is incompatible",
-// 			config: map[string]interface{}{
-// 				"tracing": map[string]interface{}{
-// 					"elastic": map[string]interface{}{
-// 						"foo": "bar",
-// 					},
-// 				},
-// 			},
-// 			logLevel:     zerolog.ErrorLevel,
-// 			incompatible: assert.True,
-// 		},
-// 	}
-//
-// 	for _, test := range tests {
-// 		test := test
-// 		t.Run(test.desc, func(t *testing.T) {
-// 			t.Parallel()
-//
-// 			var gotLog bool
-// 			var gotLevel zerolog.Level
-// 			testHook := zerolog.HookFunc(func(e *zerolog.Event, level zerolog.Level, message string) {
-// 				gotLog = true
-// 				gotLevel = level
-// 			})
-//
-// 			logger := log.With().Logger().Hook(testHook)
-// 			test.incompatible(t, test.config.deprecationNotice(logger))
-// 			assert.True(t, gotLog)
-// 			assert.Equal(t, test.logLevel, gotLevel)
-// 		})
-// 	}
-// }
+func boolP(v bool) *bool {
+	return &v
+}
+
+func intP(v int) *int {
+	return &v
+}
+
+func stringP(v string) *string {
+	return &v
+}
+
+func TestDeprecationNotice(t *testing.T) {
+	tests := []struct {
+		desc   string
+		config configuration
+	}{
+		{
+			desc: "Docker provider swarmMode option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					Docker: &docker{
+						SwarmMode: boolP(true),
+					},
+				},
+			},
+		},
+		{
+			desc: "Docker provider tls.CAOptional option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					Docker: &docker{
+						TLS: &tls{
+							CAOptional: boolP(true),
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "Swarm provider tls.CAOptional option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					Swarm: &swarm{
+						TLS: &tls{
+							CAOptional: boolP(true),
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "Consul provider namespace option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					Consul: &consul{
+						Namespace: stringP("foobar"),
+					},
+				},
+			},
+		},
+		{
+			desc: "Consul provider tls.CAOptional option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					Consul: &consul{
+						TLS: &tls{
+							CAOptional: boolP(true),
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "ConsulCatalog provider namespace option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					ConsulCatalog: &consulCatalog{
+						Namespace: stringP("foobar"),
+					},
+				},
+			},
+		},
+		{
+			desc: "ConsulCatalog provider tls.CAOptional option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					ConsulCatalog: &consulCatalog{
+						Endpoint: &endpointConfig{
+							TLS: &tls{
+								CAOptional: boolP(true),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "Nomad provider namespace option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					Nomad: &nomad{
+						Namespace: stringP("foobar"),
+					},
+				},
+			},
+		},
+		{
+			desc: "Nomad provider tls.CAOptional option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					Nomad: &nomad{
+						Endpoint: &endpointConfig{
+							TLS: &tls{
+								CAOptional: boolP(true),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "Marathon configuration is incompatible",
+			config: configuration{
+				Providers: &providers{
+					Marathon: map[string]any{
+						"foo": "bar",
+					},
+				},
+			},
+		},
+		{
+			desc: "Rancher configuration is incompatible",
+			config: configuration{
+				Providers: &providers{
+					Rancher: map[string]any{
+						"foo": "bar",
+					},
+				},
+			},
+		},
+		{
+			desc: "ETCD provider tls.CAOptional option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					ETCD: &etcd{
+						TLS: &tls{
+							CAOptional: boolP(true),
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "Redis provider tls.CAOptional option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					Redis: &redis{
+						TLS: &tls{
+							CAOptional: boolP(true),
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "HTTP provider tls.CAOptional option is incompatible",
+			config: configuration{
+				Providers: &providers{
+					HTTP: &http{
+						TLS: &tls{
+							CAOptional: boolP(true),
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "Pilot configuration is incompatible",
+			config: configuration{
+				Pilot: map[string]any{
+					"foo": "bar",
+				},
+			},
+		},
+		{
+			desc: "Experimental HTTP3 enablement configuration is incompatible",
+			config: configuration{
+				Experimental: &experimental{
+					HTTP3: boolP(true),
+				},
+			},
+		},
+		{
+			desc: "Tracing SpanNameLimit option is incompatible",
+			config: configuration{
+				Tracing: &tracing{
+					SpanNameLimit: intP(42),
+				},
+			},
+		},
+		{
+			desc: "Tracing Jaeger configuration is incompatible",
+			config: configuration{
+				Tracing: &tracing{
+					Jaeger: map[string]any{
+						"foo": "bar",
+					},
+				},
+			},
+		},
+		{
+			desc: "Tracing Zipkin configuration is incompatible",
+			config: configuration{
+				Tracing: &tracing{
+					Zipkin: map[string]any{
+						"foo": "bar",
+					},
+				},
+			},
+		},
+		{
+			desc: "Tracing Datadog configuration is incompatible",
+			config: configuration{
+				Tracing: &tracing{
+					Datadog: map[string]any{
+						"foo": "bar",
+					},
+				},
+			},
+		},
+		{
+			desc: "Tracing Instana configuration is incompatible",
+			config: configuration{
+				Tracing: &tracing{
+					Instana: map[string]any{
+						"foo": "bar",
+					},
+				},
+			},
+		},
+		{
+			desc: "Tracing Haystack configuration is incompatible",
+			config: configuration{
+				Tracing: &tracing{
+					Haystack: map[string]any{
+						"foo": "bar",
+					},
+				},
+			},
+		},
+		{
+			desc: "Tracing Elastic configuration is incompatible",
+			config: configuration{
+				Tracing: &tracing{
+					Elastic: map[string]any{
+						"foo": "bar",
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			var gotLog bool
+			var gotLevel zerolog.Level
+			testHook := zerolog.HookFunc(func(e *zerolog.Event, level zerolog.Level, message string) {
+				gotLog = true
+				gotLevel = level
+			})
+
+			logger := log.With().Logger().Hook(testHook)
+			assert.True(t, test.config.deprecationNotice(logger))
+			assert.True(t, gotLog)
+			assert.Equal(t, zerolog.ErrorLevel, gotLevel)
+		})
+	}
+}
 
 func TestLoad(t *testing.T) {
 	testCases := []struct {
@@ -415,7 +381,6 @@ func TestLoad(t *testing.T) {
 			args: []string{
 				"--configfile=traefik_no_deprecated.toml",
 			},
-
 			expected: false,
 		},
 	}
@@ -425,7 +390,7 @@ func TestLoad(t *testing.T) {
 		t.Run(test.desc, func(t *testing.T) {
 			tconfig := cmd.NewTraefikConfiguration()
 			c := &cli.Command{Configuration: tconfig}
-			l := DeprecatedLoader{}
+			l := DeprecationLoader{}
 
 			for name, val := range test.env {
 				t.Setenv(name, val)
