fd811f0f4a0d6f899e9c701646e4cb12f1797fc2
Julien Salleyron <julien.salleyron@gmail.com>
2024-01-30 09:08:55 +0100

    Adds systemd socket-activation feature

--- pkg/server/server_entrypoint_tcp.go
+++ pkg/server/server_entrypoint_tcp.go
@@ -16,6 +16,7 @@ import (
 	"time"
 
 	"github.com/containous/alice"
+	"github.com/coreos/go-systemd/activation"
 	gokitmetrics "github.com/go-kit/kit/metrics"
 	"github.com/pires/go-proxyproto"
 	"github.com/rs/zerolog"
@@ -48,6 +49,7 @@ const (
 var (
 	clientConnectionStates   = map[string]*connState{}
 	clientConnectionStatesMu = sync.RWMutex{}
+	listeners                map[string]net.Listener
 )
 
 type connState struct {
@@ -96,6 +98,20 @@ func NewTCPEntryPoints(entryPointsConfig static.EntryPoints, hostResolverConfig 
 			return clientConnectionStates
 		}))
 	}
+
+	// populate pre-defined listeners by socket activation
+	listenersWithName, _ := activation.ListenersWithNames()
+	if len(listenersWithName) > 0 {
+		listeners = make(map[string]net.Listener)
+		for name, lns := range listenersWithName {
+			if len(lns) == 1 {
+				listeners[name] = lns[0]
+				continue
+			}
+			log.Error().Str("listenersName", name).Msg("socket activation listeners must have one and only one listener per name")
+		}
+	}
+
 	serverEntryPointsTCP := make(TCPEntryPoints)
 	for entryPointName, config := range entryPointsConfig {
 		protocol, err := config.GetProtocol()
@@ -113,7 +129,7 @@ func NewTCPEntryPoints(entryPointsConfig static.EntryPoints, hostResolverConfig 
 			OpenConnectionsGauge().
 			With("entrypoint", entryPointName, "protocol", "TCP")
 
-		serverEntryPointsTCP[entryPointName], err = NewTCPEntryPoint(ctx, config, hostResolverConfig, openConnectionsGauge)
+		serverEntryPointsTCP[entryPointName], err = NewTCPEntryPoint(ctx, entryPointName, config, hostResolverConfig, openConnectionsGauge)
 		if err != nil {
 			return nil, fmt.Errorf("error while building entryPoint %s: %w", entryPointName, err)
 		}
@@ -169,10 +185,10 @@ type TCPEntryPoint struct {
 }
 
 // NewTCPEntryPoint creates a new TCPEntryPoint.
-func NewTCPEntryPoint(ctx context.Context, configuration *static.EntryPoint, hostResolverConfig *types.HostResolverConfig, openConnectionsGauge gokitmetrics.Gauge) (*TCPEntryPoint, error) {
+func NewTCPEntryPoint(ctx context.Context, entryPointName string, configuration *static.EntryPoint, hostResolverConfig *types.HostResolverConfig, openConnectionsGauge gokitmetrics.Gauge) (*TCPEntryPoint, error) {
 	tracker := newConnectionTracker(openConnectionsGauge)
 
-	listener, err := buildListener(ctx, configuration)
+	listener, err := buildListener(ctx, entryPointName, configuration)
 	if err != nil {
 		return nil, fmt.Errorf("error preparing server: %w", err)
 	}
@@ -460,11 +476,23 @@ func buildProxyProtocolListener(ctx context.Context, entryPoint *static.EntryPoi
 	return proxyListener, nil
 }
 
-func buildListener(ctx context.Context, entryPoint *static.EntryPoint) (net.Listener, error) {
-	listenConfig := newListenConfig(entryPoint)
-	listener, err := listenConfig.Listen(ctx, "tcp", entryPoint.GetAddress())
-	if err != nil {
-		return nil, fmt.Errorf("error opening listener: %w", err)
+func buildListener(ctx context.Context, entryPointName string, entryPoint *static.EntryPoint) (net.Listener, error) {
+	var listener net.Listener
+	var err error
+
+	// if we have predefined listener from socket activation
+	if ln, ok := listeners[entryPointName]; ok {
+		listener = ln
+	} else {
+		if len(listeners) > 0 {
+			log.Warn().Interface("listeners", listeners).Str("entryPointName", entryPointName).Msgf("Unable to find socket activation for entryPoint")
+		}
+
+		listenConfig := newListenConfig(entryPoint)
+		listener, err = listenConfig.Listen(ctx, "tcp", entryPoint.GetAddress())
+		if err != nil {
+			return nil, fmt.Errorf("error opening listener: %w", err)
+		}
 	}
 
 	listener = tcpKeepAliveListener{listener.(*net.TCPListener)}
