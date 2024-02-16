ec28fcb52b0f99dd4898a3a29078411a98d29b81
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-02-16 03:34:52 +0100

    fix: regression on flag using spaces between key and value

--- pkg/cli/deprecation.go
+++ pkg/cli/deprecation.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"os"
 	"reflect"
+	"slices"
 	"strings"
 
 	"github.com/rs/zerolog"
@@ -24,7 +25,10 @@ func (d DeprecationLoader) Load(args []string, cmd *cli.Command) (bool, error) {
 }
 
 // logDeprecation prints deprecation hints and returns whether incompatible deprecated options need to be removed.
-func logDeprecation(traefikConfiguration interface{}, args []string) bool {
+func logDeprecation(traefikConfiguration interface{}, arguments []string) bool {
+	// the args should be copied to avoid modification of the slice.
+	args := slices.Clone(arguments)
+
 	// This part doesn't handle properly a flag defined like this:
 	// --accesslog true
 	// where `true` could be considered as a new argument.
