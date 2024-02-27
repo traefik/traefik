08a60e8587a318258e1e72f7dc19fde2d69329ea
Alessandro Chitolina <alekitto@gmail.com>
2024-02-27 20:56:58 +0100

    ocsp stapling working

--- pkg/config/dynamic/zz_generated.deepcopy.go
+++ pkg/config/dynamic/zz_generated.deepcopy.go
@@ -1319,7 +1319,9 @@ func (in *ServersTransport) DeepCopyInto(out *ServersTransport) {
 	if in.Certificates != nil {
 		in, out := &in.Certificates, &out.Certificates
 		*out = make(tls.Certificates, len(*in))
-		copy(*out, *in)
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
 	}
 	if in.ForwardingTimeouts != nil {
 		in, out := &in.ForwardingTimeouts, &out.ForwardingTimeouts
@@ -1924,7 +1926,9 @@ func (in *TLSClientConfig) DeepCopyInto(out *TLSClientConfig) {
 	if in.Certificates != nil {
 		in, out := &in.Certificates, &out.Certificates
 		*out = make(tls.Certificates, len(*in))
-		copy(*out, *in)
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
 	}
 	if in.Spiffe != nil {
 		in, out := &in.Spiffe, &out.Spiffe
