e2fa97af95ab5aff98a69eaf1641a4d2587cb8e5
Julien Salleyron <julien.salleyron@gmail.com>
2024-03-14 09:09:02 +0100

    runtime.Object in routerTransform

--- pkg/provider/kubernetes/k8s/router_transform.go
+++ pkg/provider/kubernetes/k8s/router_transform.go
@@ -4,8 +4,9 @@ import (
 	"context"
 
 	"github.com/traefik/traefik/v3/pkg/config/dynamic"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 type RouterTransform interface {
-	Apply(ctx context.Context, rt *dynamic.Router, annotations map[string]string) error
+	Apply(ctx context.Context, rt *dynamic.Router, object runtime.Object) error
 }
