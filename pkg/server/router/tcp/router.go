65785c6919d8764c53f5fe4993ee2fd8f6f65293
romain <rtribotte@users.noreply.github.com>
2024-05-02 08:56:15 +0200

    fix: remove deadlines for postgres connections.

--- pkg/server/router/tcp/router.go
+++ pkg/server/router/tcp/router.go
@@ -123,6 +123,11 @@ func (r *Router) ServeTCP(conn tcp.WriteCloser) {
 	}
 
 	if postgres {
+		// Remove read/write deadline and delegate this to underlying TCP server.
+		if err := conn.SetDeadline(time.Time{}); err != nil {
+			log.Error().Err(err).Msg("Error while setting deadline")
+		}
+
 		r.servePostgres(r.GetConn(conn, getPeeked(br)))
 		return
 	}
