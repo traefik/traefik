3edeb1051f0a3183ea1443eee92984f5919c1feb
Andrea Cappuccio <hood@null.net>
2024-06-17 19:37:57 +0200

    fix: ensure proper logs for aborted streaming responses

--- pkg/middlewares/accesslog/logger.go
+++ pkg/middlewares/accesslog/logger.go
@@ -184,16 +184,6 @@ func (h *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request, next http
 		},
 	}
 
-	defer func() {
-		if h.config.BufferingSize > 0 {
-			h.logHandlerChan <- handlerParams{
-				logDataTable: logDataTable,
-			}
-			return
-		}
-		h.logTheRoundTrip(logDataTable)
-	}()
-
 	reqWithDataTable := req.WithContext(context.WithValue(req.Context(), DataTableKey, logDataTable))
 
 	core[RequestCount] = nextRequestCount()
@@ -237,6 +227,25 @@ func (h *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request, next http
 		log.Ctx(ctx).Error().Err(err).Str(logs.MiddlewareType, "AccessLogs").Msg("Could not get Capture")
 		return
 	}
+
+	defer func(capt capture.Capture, rw http.ResponseWriter) {
+		if h.config.BufferingSize > 0 {
+			h.logHandlerChan <- handlerParams{
+				logDataTable: logDataTable,
+			}
+			return
+		}
+
+		logDataTable.DownstreamResponse = downstreamResponse{
+			headers: rw.Header().Clone(),
+		}
+
+		logDataTable.DownstreamResponse.status = capt.StatusCode()
+		logDataTable.DownstreamResponse.size = capt.ResponseSize()
+		logDataTable.Request.size = capt.RequestSize()
+
+		h.logTheRoundTrip(logDataTable)
+	}(capt, rw)
 
 	next.ServeHTTP(rw, reqWithDataTable)
 
