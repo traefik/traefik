1e6d94cead2d7f11ffd0251a52cf1d0a2d662ad5
kevinpollet <pollet.kevin@gmail.com>
2024-06-25 15:22:35 +0200

    review

--- pkg/server/socket_activation_unix.go
+++ pkg/server/socket_activation_unix.go
@@ -11,14 +11,14 @@ import (
 
 func populateSocketActivationListeners() {
 	listenersWithName, _ := activation.ListenersWithNames()
-	if len(listenersWithName) > 0 {
-		listeners = make(map[string]net.Listener)
-		for name, lns := range listenersWithName {
-			if len(lns) == 1 {
-				listeners[name] = lns[0]
-				continue
-			}
-			log.Error().Str("listenersName", name).Msg("socket activation listeners must have one and only one listener per name")
+
+	socketActivationListeners = make(map[string]net.Listener)
+	for name, lns := range listenersWithName {
+		if len(lns) != 1 {
+			log.Error().Str("listenersName", name).Msg("Socket activation listeners must have one and only one listener per name")
+			continue
 		}
+
+		socketActivationListeners[name] = lns[0]
 	}
 }
