0767dac1feb91eb428f6453a86950f603a350e14
mmatur <michael.matur@gmail.com>
2024-06-26 18:32:45 +0200

    fix unit test

--- pkg/middlewares/auth/forward_test.go
+++ pkg/middlewares/auth/forward_test.go
@@ -513,7 +513,7 @@ func TestForwardAuthTracing(t *testing.T) {
 						attribute.String("url.scheme", "http"),
 						attribute.String("user_agent.original", ""),
 						attribute.String("network.peer.address", "127.0.0.1"),
-						attribute.String("network.peer.port", serverPort),
+						attribute.Int64("network.peer.port", int64(serverPortInt)),
 						attribute.String("server.address", "127.0.0.1"),
 						attribute.Int64("server.port", int64(serverPortInt)),
 						attribute.StringSlice("http.request.header.x-foo", []string{"foo", "bar"}),
