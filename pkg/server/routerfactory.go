587a44f4eab3b5602ab9cca6fc715859746b62ee
romain <rtribotte@users.noreply.github.com>
2024-08-26 10:48:57 +0200

    review: allow bypassing ACME challenges per entryPoint

--- pkg/server/routerfactory.go
+++ pkg/server/routerfactory.go
@@ -21,26 +21,37 @@ import (
 
 // RouterFactory the factory of TCP/UDP routers.
 type RouterFactory struct {
-	entryPointsTCP []string
-	entryPointsUDP []string
+	entryPointsTCP  []string
+	entryPointsUDP  []string
+	allowACMEByPass map[string]bool
 
-	managerFactory  *service.ManagerFactory
+	managerFactory *service.ManagerFactory
+
 	metricsRegistry metrics.Registry
 
 	pluginBuilder middleware.PluginsBuilder
-
-	chainBuilder        *middleware.ChainBuilder
-	tlsManager          *tls.Manager
-	handlesTLSChallenge bool
+	chainBuilder  *middleware.ChainBuilder
+	tlsManager    *tls.Manager
 }
 
 // NewRouterFactory creates a new RouterFactory.
 func NewRouterFactory(staticConfiguration static.Configuration, managerFactory *service.ManagerFactory, tlsManager *tls.Manager,
 	chainBuilder *middleware.ChainBuilder, pluginBuilder middleware.PluginsBuilder, metricsRegistry metrics.Registry,
 ) *RouterFactory {
+	handlesTLSChallenge := false
+	for _, resolver := range staticConfiguration.CertificatesResolvers {
+		if resolver.ACME.TLSChallenge != nil {
+			handlesTLSChallenge = true
+			break
+		}
+	}
+
+	allowACMEByPass := map[string]bool{}
 	var entryPointsTCP, entryPointsUDP []string
-	for name, cfg := range staticConfiguration.EntryPoints {
-		protocol, err := cfg.GetProtocol()
+	for name, ep := range staticConfiguration.EntryPoints {
+		allowACMEByPass[name] = ep.ACMEByPass || !handlesTLSChallenge
+
+		protocol, err := ep.GetProtocol()
 		if err != nil {
 			// Should never happen because Traefik should not start if protocol is invalid.
 			log.WithoutContext().Errorf("Invalid protocol: %v", err)
@@ -53,23 +64,15 @@ func NewRouterFactory(staticConfiguration static.Configuration, managerFactory *
 		}
 	}
 
-	handlesTLSChallenge := false
-	for _, resolver := range staticConfiguration.CertificatesResolvers {
-		if resolver.ACME.TLSChallenge != nil {
-			handlesTLSChallenge = true
-			break
-		}
-	}
-
 	return &RouterFactory{
-		entryPointsTCP:      entryPointsTCP,
-		entryPointsUDP:      entryPointsUDP,
-		managerFactory:      managerFactory,
-		metricsRegistry:     metricsRegistry,
-		tlsManager:          tlsManager,
-		chainBuilder:        chainBuilder,
-		pluginBuilder:       pluginBuilder,
-		handlesTLSChallenge: handlesTLSChallenge,
+		entryPointsTCP:  entryPointsTCP,
+		entryPointsUDP:  entryPointsUDP,
+		managerFactory:  managerFactory,
+		metricsRegistry: metricsRegistry,
+		tlsManager:      tlsManager,
+		chainBuilder:    chainBuilder,
+		pluginBuilder:   pluginBuilder,
+		allowACMEByPass: allowACMEByPass,
 	}
 }
 
@@ -97,8 +100,8 @@ func (f *RouterFactory) CreateRouters(rtConf *runtime.Configuration) (map[string
 	rtTCPManager := tcprouter.NewManager(rtConf, svcTCPManager, middlewaresTCPBuilder, handlersNonTLS, handlersTLS, f.tlsManager)
 	routersTCP := rtTCPManager.BuildHandlers(ctx, f.entryPointsTCP)
 
-	if !f.handlesTLSChallenge {
-		for _, r := range routersTCP {
+	for ep, r := range routersTCP {
+		if allowACMEByPass, ok := f.allowACMEByPass[ep]; ok && allowACMEByPass {
 			r.EnableACMETLSPassthrough()
 		}
 	}
