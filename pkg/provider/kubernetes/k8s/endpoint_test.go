b4992393e929e9518fc638a37fd50557b8ac3a02
Valery Fouques <valery.fouques@hardis-group.com>
2024-09-24 23:36:58 +0200

    more readable test

--- pkg/provider/kubernetes/k8s/endpoint_test.go
+++ pkg/provider/kubernetes/k8s/endpoint_test.go
@@ -7,29 +7,26 @@ import (
 )
 
 func TestEndpointServing(t *testing.T) {
-	type args struct {
-		endpoint v1.Endpoint
-	}
 	valTrue := true
 	valFalse := false
 	tests := []struct {
-		name string
-		args args
-		want bool
+		name     string
+		endpoint v1.Endpoint
+		want     bool
 	}{
-		{name: "test1.1", args: args{endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valTrue, Serving: nil}}}, want: true},
-		{name: "test1.2", args: args{endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valTrue, Serving: &valTrue}}}, want: true},
-		{name: "test1.3", args: args{endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valTrue, Serving: &valFalse}}}, want: true},
-		{name: "test2.1", args: args{endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valFalse, Serving: nil}}}, want: false},
-		{name: "test2.2", args: args{endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valFalse, Serving: &valTrue}}}, want: true},
-		{name: "test2.3", args: args{endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valFalse, Serving: &valFalse}}}, want: false},
-		{name: "test3.1", args: args{endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: nil, Serving: nil}}}, want: false},
-		{name: "test3.2", args: args{endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: nil, Serving: &valTrue}}}, want: false},
-		{name: "test3.3", args: args{endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: nil, Serving: &valFalse}}}, want: false},
+		{name: "test1.1", endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valTrue, Serving: nil}}, want: true},
+		{name: "test1.2", endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valTrue, Serving: &valTrue}}, want: true},
+		{name: "test1.3", endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valTrue, Serving: &valFalse}}, want: true},
+		{name: "test2.1", endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valFalse, Serving: nil}}, want: false},
+		{name: "test2.2", endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valFalse, Serving: &valTrue}}, want: true},
+		{name: "test2.3", endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: &valFalse, Serving: &valFalse}}, want: false},
+		{name: "test3.1", endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: nil, Serving: nil}}, want: false},
+		{name: "test3.2", endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: nil, Serving: &valTrue}}, want: false},
+		{name: "test3.3", endpoint: v1.Endpoint{Conditions: v1.EndpointConditions{Ready: nil, Serving: &valFalse}}, want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := EndpointServing(tt.args.endpoint); got != tt.want {
+			if got := EndpointServing(tt.endpoint); got != tt.want {
 				t.Errorf("EndpointServing() = %v, want %v", got, tt.want)
 			}
 		})
