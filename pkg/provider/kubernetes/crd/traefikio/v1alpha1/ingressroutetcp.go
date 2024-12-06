8a734bcb6003aa0c6b0ff68092d69c8a549d295e
Michel Loiseleur <michel.loiseleur@traefik.io>
2024-12-06 15:36:06 +0100

    self-review after rebase

--- pkg/provider/kubernetes/crd/traefikio/v1alpha1/ingressroutetcp.go
+++ pkg/provider/kubernetes/crd/traefikio/v1alpha1/ingressroutetcp.go
@@ -104,6 +104,8 @@ type ServiceTCP struct {
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:storageversion
 // +kubebuilder:printcolumn:name="EntryPoints",type=string,JSONPath=`.spec.entryPoints`
+// +kubebuilder:printcolumn:name="Rule",type=string,JSONPath=`.spec.routes[*].match`
+// +kubebuilder:printcolumn:name="Middlewares",type=string,JSONPath=`.spec.routes[*].middlewares[*].name`
 
 // IngressRouteTCP is the CRD implementation of a Traefik TCP Router.
 type IngressRouteTCP struct {
