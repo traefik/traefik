a29628fa2e45b355ef9439fc27af6be2c940e537
Kevin Pollet <pollet.kevin@gmail.com>
2024-12-20 11:26:04 +0100

    Fix fenced server status computation
    Co-authored-by: Romain <rtribotte@users.noreply.github.com>

--- pkg/provider/kubernetes/crd/kubernetes_test.go
+++ pkg/provider/kubernetes/crd/kubernetes_test.go
@@ -4751,12 +4751,6 @@ func TestLoadIngressRoutes(t *testing.T) {
 										URL:    "http://10.10.0.4:80",
 										Fenced: true,
 									},
-									{
-										URL: "http://10.10.0.5:80",
-									},
-									{
-										URL: "http://10.10.0.6:80",
-									},
 								},
 								PassHostHeader: pointer(true),
 								ResponseForwarding: &dynamic.ResponseForwarding{
