262f213370d82af5cb8afd0df1f3dce885242100
kevinpollet <pollet.kevin@gmail.com>
2025-01-09 15:49:59 +0100

    review

--- pkg/config/dynamic/http_config.go
+++ pkg/config/dynamic/http_config.go
@@ -270,9 +270,9 @@ func (r *ResponseForwarding) SetDefaults() {
 
 // Server holds the server configuration.
 type Server struct {
-	URL          string `json:"url,omitempty" toml:"url,omitempty" yaml:"url,omitempty" label:"url,omitempty"`
-	Weight       *int   `json:"weight,omitempty" toml:"weight,omitempty" yaml:"weight,omitempty" label:"weight" export:"true"`
-	PreservePath bool   `json:"preservePath,omitempty" toml:"preservePath,omitempty" yaml:"preservePath,omitempty" label:"-" export:"true"`
+	URL          string `json:"url,omitempty" toml:"url,omitempty" yaml:"url,omitempty"`
+	Weight       *int   `json:"weight,omitempty" toml:"weight,omitempty" yaml:"weight,omitempty" export:"true"`
+	PreservePath bool   `json:"preservePath,omitempty" toml:"preservePath,omitempty" yaml:"preservePath,omitempty" export:"true"`
 	Fenced       bool   `json:"fenced,omitempty" toml:"-" yaml:"-" label:"-" file:"-" kv:"-"`
 	// Scheme can only be defined with label Providers.
 	Scheme string `json:"-" toml:"-" yaml:"-" file:"-" kv:"-"`
