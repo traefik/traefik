8d4b0781faeab30ff657108291fa24d67780c271
Nelson Isioma <nelsonisioma1@gmail.com>
2025-01-21 01:02:00 +0100

    allowing root ca to be added through config maps

--- pkg/provider/kubernetes/crd/traefikio/v1alpha1/serverstransport.go
+++ pkg/provider/kubernetes/crd/traefikio/v1alpha1/serverstransport.go
@@ -31,6 +31,8 @@ type ServersTransportSpec struct {
 	ServerName string `json:"serverName,omitempty"`
 	// InsecureSkipVerify disables SSL certificate verification.
 	InsecureSkipVerify bool `json:"insecureSkipVerify,omitempty"`
+	// RootCAs gives you the option to use secrets or config maps to store CAs
+	RootCAs []RootCAs `json:"rootCAs,omitempty"`
 	// RootCAsSecrets defines a list of CA secret used to validate self-signed certificate.
 	RootCAsSecrets []string `json:"rootCAsSecrets,omitempty"`
 	// CertificatesSecrets defines a list of secret storing client certificates for mTLS.
@@ -61,6 +63,14 @@ type ForwardingTimeouts struct {
 	ReadIdleTimeout *intstr.IntOrString `json:"readIdleTimeout,omitempty"`
 	// PingTimeout is the timeout after which the HTTP/2 connection will be closed if a response to ping is not received.
 	PingTimeout *intstr.IntOrString `json:"pingTimeout,omitempty"`
+}
+
+// +k8s:deepcopy-gen=true
+
+// RootCAs holds the reference to the secret or config map that stores the CA
+type RootCAs struct {
+	SecretRef    *string `json:"secretRef,omitempty"`
+	ConfigMapRef *string `json:"configMapRef,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
