3443d59dcf1996a3d8538bf4066c93386300f734
Kalyan Inampudi <Kalyan.Inampudi@nike.com>
2025-02-03 22:11:59 -0800

    chnages for conformance tests

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -36,10 +36,13 @@ import (
 )
 
 var (
-	showLog                      = flag.Bool("tlog", false, "always show Traefik logs")
-	k8sConformance               = flag.Bool("k8sConformance", false, "run K8s Gateway API conformance test")
-	k8sConformanceRunTest        = flag.String("k8sConformanceRunTest", "", "run a specific K8s Gateway API conformance test")
-	k8sConformanceTraefikVersion = flag.String("k8sConformanceTraefikVersion", "dev", "specify the Traefik version for the K8s Gateway API conformance report")
+	showLog                          = flag.Bool("tlog", false, "always show Traefik logs")
+	k8sConformance                   = flag.Bool("k8sConformance", false, "run K8s Gateway API conformance test")
+	k8sConformanceRunTest            = flag.String("k8sConformanceRunTest", "", "run a specific K8s Gateway API conformance test")
+	k8sConformanceTraefikVersion     = flag.String("k8sConformanceTraefikVersion", "dev", "specify the Traefik version for the K8s Gateway API conformance report")
+	kNativeConformance               = flag.Bool("k8sConformance", false, "run K8s Gateway API conformance test")
+	kNativeConformanceRunTest        = flag.String("k8sConformanceRunTest", "", "run a specific K8s Gateway API conformance test")
+	kNativeConformanceTraefikVersion = flag.String("k8sConformanceTraefikVersion", "dev", "specify the Traefik version for the K8s Gateway API conformance report")
 )
 
 const tailscaleSecretFilePath = "tailscale.secret"
