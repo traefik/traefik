07a413312de3441a7ccccc8f56bb71b1d86ecb9c
kevinpollet <pollet.kevin@gmail.com>
2025-02-27 11:55:53 +0100

    review: integration tests

--- pkg/provider/rest/rest.go
+++ pkg/provider/rest/rest.go
@@ -1,12 +1,13 @@
 package rest
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
+	"github.com/traefik/paerser/file"
 	"github.com/traefik/traefik/v3/pkg/config/dynamic"
 	"github.com/traefik/traefik/v3/pkg/provider"
 	"github.com/traefik/traefik/v3/pkg/safe"
@@ -47,7 +48,14 @@ func (p *Provider) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 	configuration := new(dynamic.Configuration)
 
-	if err := json.NewDecoder(req.Body).Decode(configuration); err != nil {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Error().Err(err).Msg("Error reading configuration")
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := file.DecodeContent(string(body), ".yaml", configuration); err != nil {
 		log.Error().Err(err).Msg("Error parsing configuration")
 		http.Error(rw, fmt.Sprintf("%+v", err), http.StatusBadRequest)
 		return
