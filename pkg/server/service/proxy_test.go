8ba99adc503b637b0e424605e2397ee132ddda34
Romain <rtribotte@users.noreply.github.com>
2025-03-21 11:00:06 +0100

    Error level log for configuration-related TLS errors with backends
    Co-authored-by: Kevin Pollet <pollet.kevin@gmail.com>

--- pkg/server/service/proxy_test.go
+++ pkg/server/service/proxy_test.go
@@ -1,12 +1,15 @@
 package service
 
 import (
+	"crypto/tls"
+	"errors"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"github.com/traefik/traefik/v2/pkg/testhelpers"
 )
 
@@ -33,5 +36,48 @@ func BenchmarkProxy(b *testing.B) {
 	b.ReportAllocs()
 	for range b.N {
 		handler.ServeHTTP(w, req)
+	}
+}
+
+func TestIsTLSConfigError(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		err      error
+		expected bool
+	}{
+		{
+			desc: "nil",
+		},
+		{
+			desc: "TLS ECHRejectionError",
+			err:  &tls.ECHRejectionError{},
+		},
+		{
+			desc: "TLS AlertError",
+			err:  tls.AlertError(0),
+		},
+		{
+			desc: "Random error",
+			err:  errors.New("random error"),
+		},
+		{
+			desc:     "TLS RecordHeaderError",
+			err:      tls.RecordHeaderError{},
+			expected: true,
+		},
+		{
+			desc:     "TLS CertificateVerificationError",
+			err:      &tls.CertificateVerificationError{},
+			expected: true,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			actual := isTLSConfigError(test.err)
+			require.Equal(t, test.expected, actual)
+		})
 	}
 }
