91204efa2b114a487055317cbb178a8c67741d51
gndz07 <ginadzani@gmail.com>
2025-04-03 17:38:45 +0200

    fix: image build

--- webui/embed.go
+++ webui/embed.go
@@ -1,12 +1,14 @@
-package dashboard
+package webui
 
 import (
 	"embed"
 	"io/fs"
 )
 
-//go:embed dist
+// Files starting with . and _ are excluded by default
+//
+//go:embed static
 var assets embed.FS
 
-// WebUI contains the Web UI assets.
-var WebUI, _ = fs.Sub(assets, "dist")
+// FS contains the web UI assets.
+var FS, _ = fs.Sub(assets, "static")
