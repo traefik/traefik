bfa4ce1f6b4329dca6f5d6a0e7d065dc36697033
gndz07 <ginadzani@gmail.com>
2025-04-07 10:58:16 +0200

    chore: dev index.html

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -3,6 +3,7 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default ({ mode }: { mode: string }) => {
@@ -10,7 +11,7 @@ export default ({ mode }: { mode: string }) => {
 
   return defineConfig({
     base: process.env.VITE_APP_BASE_URL || '',
-    plugins: [react(), viteTsconfigPaths()],
+    plugins: [react(), viteTsconfigPaths(), createHtmlPlugin({ template: 'index.dev.html' })],
     server: {
       open: true, // this ensures that the browser opens upon server start
       port: 3000,
