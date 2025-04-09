da82da900207a5649acab9454d512bf876a1634e
hrekouane <hichem.rekouane@ansys.com>
2025-04-09 19:12:06 +0200

    add templating line to forward auth

--- pkg/middlewares/auth/forward.go
+++ pkg/middlewares/auth/forward.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"net"
 	"net/http"
-	"net/url"
 	"regexp"
 	"strings"
 	"time"
@@ -124,7 +123,7 @@ func (fa *forwardAuth) GetTracingInformation() (string, string, trace.SpanKind) 
 func (fa *forwardAuth) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	logger := middlewares.GetLogger(req.Context(), fa.name, typeNameForward)
 
-	fa.address = strings.ReplaceAll(fa.address, "{escaped_request_uri}", url.QueryEscape(req.URL.String()))
+	fa.address = strings.ReplaceAll(fa.address, "{escaped_request_uri}", fmt.Sprintf("https://%s%s/", req.Host, req.URL.EscapedPath()))
 	forwardReq, err := http.NewRequestWithContext(req.Context(), http.MethodGet, fa.address, nil)
 	if err != nil {
 		logger.Debug().Msgf("Error calling %s. Cause %s", fa.address, err)
