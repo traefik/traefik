b88af9baa8f9f225aaea1b1c680464778b5898f8
gndz07 <ginadzani@gmail.com>
2025-04-11 15:46:35 +0200

    fix: remove trailing slash from api base path, if any

--- webui/src/libs/fetch.ts
+++ webui/src/libs/fetch.ts
@@ -1,6 +1,7 @@
+import { BASE_PATH } from './utils'
+
 export default async function (input: RequestInfo, init?: RequestInit): Promise<JSON> {
-  const { VITE_APP_BASE_API_URL } = import.meta.env
-  const res = await fetch(`${window.APIUrl || VITE_APP_BASE_API_URL || ''}${input}`, init)
+  const res = await fetch(`${BASE_PATH}${input}`, init)
   if (!res.ok) throw new Error(res.statusText)
   return await res.json()
 }
@@ -9,8 +10,7 @@ export const fetchPage = async function (
   input: RequestInfo,
   init?: RequestInit,
 ): Promise<Response & { data: unknown[]; nextPage: number }> {
-  const { VITE_APP_BASE_API_URL } = import.meta.env
-  const res = await fetch(`${window.APIUrl || VITE_APP_BASE_API_URL || ''}${input}`, init)
+  const res = await fetch(`${BASE_PATH}${input}`, init)
 
   if (!res.ok) throw new Error(res.statusText)
 
--- webui/src/libs/fetchMany.ts
+++ webui/src/libs/fetchMany.ts
@@ -1,6 +1,6 @@
 import { Key } from 'swr'
 
-const { VITE_APP_BASE_API_URL } = import.meta.env
+import { BASE_PATH } from './utils'
 
 export default async function <JSON>(key: Key): Promise<JSON[] | undefined> {
   const [baseUrl, params, init] = key as Array<string | string[] | RequestInit>
@@ -8,7 +8,7 @@ export default async function <JSON>(key: Key): Promise<JSON[] | undefined> {
   if (!params || !Array.isArray(params)) return
 
   const requests = params.map((param) => {
-    const apiUrl = `${window.APIUrl || VITE_APP_BASE_API_URL || ''}${baseUrl}${param}`
+    const apiUrl = `${BASE_PATH}${baseUrl}${param}`
     return fetch(apiUrl, init as RequestInit).then((res) => res.json())
   })
 
