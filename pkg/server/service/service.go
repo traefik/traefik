658efc09080fd112a10da635b77c0fbea7e4f044
Nelson Isioma <nelsonisioma1@gmail.com>
2025-05-17 14:34:23 +0100

    generating docs

--- pkg/server/service/service.go
+++ pkg/server/service/service.go
@@ -253,7 +253,7 @@ func (m *Manager) getWRRServiceHandler(ctx context.Context, serviceName string, 
 			return nil, err
 		}
 
-		balancer.Add(service.Name, serviceHandler, service.Weight, false)
+		balancer.Add(service.Name, serviceHandler, service.Weight, false, nil)
 
 		if config.HealthCheck == nil {
 			continue
