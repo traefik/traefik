9fab8480460777d9297dcf024cb782059cbb8117
Massimiliano D'Elia <massimiliano.delia@traefik.io>
2025-05-26 16:06:58 +0200

    fix: remove vite-plugin-html

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -3,7 +3,6 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default ({ mode }: { mode: string }) => {
@@ -11,9 +10,9 @@ export default ({ mode }: { mode: string }) => {
 
   return defineConfig({
     base: process.env.VITE_APP_BASE_URL || '',
-    plugins: [react(), viteTsconfigPaths(), createHtmlPlugin({ template: 'index.dev.html' })],
+    plugins: [react(), viteTsconfigPaths()],
     server: {
-      open: true, // this ensures that the browser opens upon server start
+      open: 'index.dev.html',
       port: 3000,
     },
     build: {
