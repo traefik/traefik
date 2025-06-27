2bbc03bf4a281cd5961a0668b237836af3284c03
Shi Yue <34417826+hasbai@users.noreply.github.com>
2025-06-28 03:17:23 +0800

    cmd tool to generate ech key

--- pkg/tls/ech.go
+++ pkg/tls/ech.go
@@ -2,6 +2,7 @@ package tls
 
 import (
 	"crypto/tls"
+	"encoding/base64"
 	"encoding/binary"
 	"encoding/pem"
 	"fmt"
@@ -156,16 +157,6 @@ func NewECHKey(publicName string) (*tls.EncryptedClientHelloKey, error) {
 	}, nil
 }
 
-func MarshalEncryptedClientHelloConfigList(configs []tls.EncryptedClientHelloKey) ([]byte, error) {
-	builder := cryptobyte.NewBuilder(nil)
-	builder.AddUint16LengthPrefixed(func(builder *cryptobyte.Builder) {
-		for _, c := range configs {
-			builder.AddBytes(c.Config)
-		}
-	})
-	return builder.Bytes()
-}
-
 // startECHServer starts a TLS server that supports Encrypted Client Hello (ECH).
 func startECHServer(bind string, cert tls.Certificate, echKey tls.EncryptedClientHelloKey) {
 	mux := http.NewServeMux()
@@ -188,41 +179,66 @@ func startECHServer(bind string, cert tls.Certificate, echKey tls.EncryptedClien
 	}
 }
 
-// RequestWithECH sends request to a server using the provided ECH configuration.
-func RequestWithECH(rawURL string, host string, echKey tls.EncryptedClientHelloKey) []byte {
-	configList, err := MarshalEncryptedClientHelloConfigList([]tls.EncryptedClientHelloKey{echKey})
-	if err != nil {
-		log.Fatalf("marshal ECH config list error: %v", err)
+type ECHRequestConf[T []byte | string] struct {
+	URL      string `description:"The URL to request." json:"u" export:"true"`
+	Host     string `description:"The host/sni to request with." json:"h" export:"true"`
+	ECH      T      `description:"A base64-encoded ECH public config list." json:"ech" export:"true"`
+	Insecure bool   `description:"If true, skip TLS verification (for testing purposes)." json:"k" export:"true"`
+}
+
+// RequestWithECH sends a GET request to a server using the provided ECH configuration.
+func RequestWithECH[T []byte | string](c ECHRequestConf[T]) (body []byte, err error) {
+	// Decode the ECH configuration from base64 if it's a string, otherwise use it directly.
+	var ech []byte
+	if s, ok := any(c.ECH).(string); ok {
+		ech, err = base64.StdEncoding.DecodeString(s)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		ech = []byte(c.ECH)
+	}
+
+	requestURL, _ := url.Parse(c.URL)
+	if c.Host == "" {
+		c.Host = requestURL.Hostname()
 	}
 
 	client := &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
-				ServerName:                     host,
-				EncryptedClientHelloConfigList: configList,
+				ServerName:                     c.Host,
+				EncryptedClientHelloConfigList: ech,
 				MinVersion:                     tls.VersionTLS13,
-				InsecureSkipVerify:             true, // For testing purposes, skip TLS verification
+				InsecureSkipVerify:             c.Insecure,
 			},
 		},
 	}
 
-	requestURL, _ := url.Parse(rawURL)
 	req := &http.Request{
 		Method: "GET",
 		URL:    requestURL,
-		Host:   host,
+		Host:   c.Host,
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("Request error: %v", err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	body := make([]byte, 1024)
+	body = make([]byte, 1024)
 	n, _ := resp.Body.Read(body)
 	fmt.Printf("server response: %s\n", body[:n])
 	fmt.Printf("Status code: %d\n", resp.StatusCode)
 	fmt.Printf("Response header: %v\n", resp.Header)
 
-	return body[:n]
+	return body[:n], nil
+}
+
+func ECHConfigToConfigList(echConfig []byte) ([]byte, error) {
+	var b cryptobyte.Builder
+	b.AddUint16LengthPrefixed(func(child *cryptobyte.Builder) {
+		child.AddBytes(echConfig)
+	})
+	return b.Bytes()
 }
