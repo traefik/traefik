b8e99360eb3a03b6514b345c7f93d749860def60
kevinpollet <pollet.kevin@gmail.com>
2025-07-22 12:01:36 +0200

    review

--- cmd/configuration.go
+++ cmd/configuration.go
@@ -10,6 +10,7 @@ import (
 // TraefikCmdConfiguration wraps the static configuration and extra parameters.
 type TraefikCmdConfiguration struct {
 	static.Configuration `export:"true"`
+
 	// ConfigFile is the path to the configuration file.
 	ConfigFile string `description:"Configuration file to use. If specified all other flags are ignored." export:"true"`
 }
@@ -42,26 +43,11 @@ type TraefikHealthCheckCmdConfiguration struct {
 	static.Configuration `export:"true"`
 
 	// URL is the url to use for the healthcheck command.
-	URL string `json:"url,omitempty" toml:"url,omitempty" yaml:"url,omitempty"`
+	URL string `description:"URL to use for the healthcheck command. If specified, the configuration file is ignored." export:"true"`
 }
 
 func NewTraefikHealthCheckConfiguration() *TraefikHealthCheckCmdConfiguration {
 	return &TraefikHealthCheckCmdConfiguration{
-		Configuration: static.Configuration{
-			Global: &static.Global{
-				CheckNewVersion: true,
-			},
-			EntryPoints: make(static.EntryPoints),
-			Providers: &static.Providers{
-				ProvidersThrottleDuration: ptypes.Duration(2 * time.Second),
-			},
-			ServersTransport: &static.ServersTransport{
-				MaxIdleConnsPerHost: 200,
-			},
-			TCPServersTransport: &static.TCPServersTransport{
-				DialTimeout:   ptypes.Duration(30 * time.Second),
-				DialKeepAlive: ptypes.Duration(15 * time.Second),
-			},
-		},
+		Configuration: NewTraefikConfiguration().Configuration,
 	}
 }
