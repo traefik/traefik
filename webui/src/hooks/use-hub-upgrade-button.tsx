dfd4896ac1b1257bf3649d0854bd587febf0fac8
gndz07 <ginadzani@gmail.com>
2025-10-31 10:48:19 +0100

    feat: use online hub upgrade button

---
+++ webui/src/hooks/use-hub-upgrade-button.tsx
@@ -0,0 +1,53 @@
+import { useContext, useEffect, useState } from 'react'
+
+import { VersionContext } from 'contexts/version'
+import verifySignature from 'pages/hub-demo/workers/scriptVerification'
+
+const HUB_BUTTON_URL = 'https://traefik.github.io/traefiklabs-hub-button-app/main-v1.js'
+const PUBLIC_KEY = 'MCowBQYDK2VwAyEAY0OZFFE5kSuqYK6/UprTL5RmvQ+8dpPTGMCw1MiO/Gs='
+
+const useHubUpgradeButton = () => {
+  const [signatureVerified, setSignatureVerified] = useState(false)
+  const [scriptBlobUrl, setScriptBlobUrl] = useState<string | null>(null)
+
+  const { showHubButton } = useContext(VersionContext)
+
+  useEffect(() => {
+    if (showHubButton) {
+      const verifyAndLoadScript = async () => {
+        try {
+          const { verified, scriptContent: content } = await verifySignature(
+            HUB_BUTTON_URL,
+            `${HUB_BUTTON_URL}.sig`,
+            PUBLIC_KEY,
+          )
+          if (!verified || !content) {
+            setSignatureVerified(false)
+          } else {
+            setSignatureVerified(true)
+
+            const blob = new Blob([content], { type: 'application/javascript' })
+            const blobUrl = URL.createObjectURL(blob)
+
+            setScriptBlobUrl(blobUrl)
+            setSignatureVerified(true)
+          }
+        } catch {
+          setSignatureVerified(false)
+        }
+      }
+
+      verifyAndLoadScript()
+
+      return () => {
+        if (scriptBlobUrl) {
+          URL.revokeObjectURL(scriptBlobUrl)
+        }
+      }
+    }
+  }, [scriptBlobUrl, showHubButton])
+
+  return { signatureVerified, scriptBlobUrl }
+}
+
+export default useHubUpgradeButton
