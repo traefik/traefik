8db990817384236e9d32ca6e1a0231b8ab4b808c
gndz07 <ginadzani@gmail.com>
2025-11-04 15:44:56 +0100

    fix: increase breakpoint to collapse the side navbar

--- webui/src/layout/navigation/index.ts
+++ webui/src/layout/navigation/index.ts
@@ -1,4 +1,5 @@
-export const LAPTOP_BP = 1025
+// common breakpoint for large screen, cf. https://www.w3schools.com/howto/howto_css_media_query_breakpoints.asp
+export const LAPTOP_BP = 1200
 
 export * from './SideNavBar'
 export * from './TopNavBar'
