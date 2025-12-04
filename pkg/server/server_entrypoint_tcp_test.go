39c3555345f7852cdab40aee471dcece5c666e62
romain <rtribotte@users.noreply.github.com>
2025-12-04 11:38:07 +0100

    review

--- pkg/server/server_entrypoint_tcp_test.go
+++ pkg/server/server_entrypoint_tcp_test.go
@@ -628,6 +628,10 @@ func TestPathOperations(t *testing.T) {
 	configuration := &static.EntryPoint{}
 	configuration.SetDefaults()
 
+	// We need to allow some of the suspicious encoded characters to test the path operations in case they are authorized.
+	configuration.HTTP.EncodedCharacters.AllowEncodedSlash = true
+	configuration.HTTP.EncodedCharacters.AllowEncodedPercent = true
+
 	// Create the HTTP server using createHTTPServer.
 	server, err := createHTTPServer(t.Context(), ln, configuration, false, requestdecorator.New(nil))
 	require.NoError(t, err)
