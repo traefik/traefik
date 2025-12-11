53725e5532dbb86dd267923eec6a259ef9dba9dd
divyanshu-vashu <vashusingh2004.jan@gmail.com>
2025-12-12 01:55:20 +0530

    feat(tcp): allow configuring server address with label providers
    Signed-off-by: divyanshu-vashu <vashusingh2004.jan@gmail.com>

--- pkg/config/dynamic/tcp_config.go
+++ pkg/config/dynamic/tcp_config.go
@@ -121,7 +121,7 @@ func (l *TCPServersLoadBalancer) Mergeable(loadBalancer *TCPServersLoadBalancer)
 
 // TCPServer holds a TCP Server configuration.
 type TCPServer struct {
-	Address string `json:"address,omitempty" toml:"address,omitempty" yaml:"address,omitempty" label:"-"`
+	Address string `json:"address,omitempty" toml:"address,omitempty" yaml:"address,omitempty" label:"address"`
 	Port    string `json:"-" toml:"-" yaml:"-"`
 	TLS     bool   `json:"tls,omitempty" toml:"tls,omitempty" yaml:"tls,omitempty"`
 }
