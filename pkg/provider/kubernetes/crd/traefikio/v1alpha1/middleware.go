97ce8164240f3f16ce94279440d67194052bd7cf
gndz07 <ginadzani@gmail.com>
2026-02-17 14:52:22 +0100

    fix: godoc wording

--- pkg/provider/kubernetes/crd/traefikio/v1alpha1/middleware.go
+++ pkg/provider/kubernetes/crd/traefikio/v1alpha1/middleware.go
@@ -155,7 +155,7 @@ type ForwardAuth struct {
 	AuthRequestHeaders []string `json:"authRequestHeaders,omitempty"`
 	// TLS defines the configuration used to secure the connection to the authentication server.
 	TLS *ClientTLS `json:"tls,omitempty"`
-	// MaxResponseBodySize defines the maximum response body size in bytes allowed to be forwarded to the authentication server.
+	// MaxResponseBodySize defines the maximum body size in bytes allowed in the response from the authentication server.
 	MaxResponseBodySize *int64 `json:"maxResponseBodySize,omitempty"`
 }
 
