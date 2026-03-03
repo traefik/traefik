03dda3343bbca187581e50865bd3f528a3f7dbf9
gndz07 <ginadzani@gmail.com>
2026-03-03 16:35:00 +0100

    fix test

--- pkg/provider/kubernetes/ingress-nginx/kubernetes_test.go
+++ pkg/provider/kubernetes/ingress-nginx/kubernetes_test.go
@@ -6717,16 +6717,18 @@ func TestLoadIngresses(t *testing.T) {
 					ServersTransports: map[string]*dynamic.ServersTransport{
 						"default-ingress-with-limit-rpm": {
 							ForwardingTimeouts: &dynamic.ForwardingTimeouts{
-								DialTimeout:  ptypes.Duration(60 * time.Second),
-								ReadTimeout:  ptypes.Duration(60 * time.Second),
-								WriteTimeout: ptypes.Duration(60 * time.Second),
+								DialTimeout:     ptypes.Duration(60 * time.Second),
+								ReadTimeout:     ptypes.Duration(60 * time.Second),
+								WriteTimeout:    ptypes.Duration(60 * time.Second),
+								IdleConnTimeout: ptypes.Duration(60 * time.Second),
 							},
 						},
 						"default-ingress-with-limit-rpm-zero": {
 							ForwardingTimeouts: &dynamic.ForwardingTimeouts{
-								DialTimeout:  ptypes.Duration(60 * time.Second),
-								ReadTimeout:  ptypes.Duration(60 * time.Second),
-								WriteTimeout: ptypes.Duration(60 * time.Second),
+								DialTimeout:     ptypes.Duration(60 * time.Second),
+								ReadTimeout:     ptypes.Duration(60 * time.Second),
+								WriteTimeout:    ptypes.Duration(60 * time.Second),
+								IdleConnTimeout: ptypes.Duration(60 * time.Second),
 							},
 						},
 					},
