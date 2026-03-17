31400876de16259730f4bfb73e61b6d9815ddac0
Desel72 <pedroluiscolmenares722@gmail.com>
2026-03-17 09:44:05 -0500

    Remove AGPL license in code

--- webui/src/components/buttons/IconButton.tsx
+++ webui/src/components/buttons/IconButton.tsx
@@ -1,17 +1,3 @@
-/*
-Copyright (C) 2022-2024 Traefik Labs
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-You should have received a copy of the GNU Affero General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
-*/
-
 import { Button, Flex, Text } from '@traefiklabs/faency'
 import { ComponentProps, ReactNode } from 'react'
 
