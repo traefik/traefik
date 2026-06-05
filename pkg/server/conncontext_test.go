5008468bbbe01496c0b0ea3c632c492331afa321
Julien Salleyron <julien.salleyron@gmail.com>
2026-06-05 14:26:04 +0200

    fix lint

--- pkg/server/conncontext_test.go
+++ pkg/server/conncontext_test.go
@@ -8,17 +8,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type keyTest string
+
 func TestConnContext(t *testing.T) {
 	var connContext multipleConnContext
 	connContext.AddConnContextFunc(func(ctx context.Context, _ net.Conn) context.Context {
-		return context.WithValue(ctx, "test", "test")
+		return context.WithValue(ctx, keyTest("test"), "test")
 	})
 	connContext.AddConnContextFunc(func(ctx context.Context, _ net.Conn) context.Context {
-		return context.WithValue(ctx, "test2", "test2")
+		return context.WithValue(ctx, keyTest("test2"), "test2")
 	})
 
 	ctx := connContext.Build()(context.Background(), nil)
 
-	require.Equal(t, "test", ctx.Value("test"))
-	require.Equal(t, "test2", ctx.Value("test2"))
+	require.Equal(t, "test", ctx.Value(keyTest("test")))
+	require.Equal(t, "test2", ctx.Value(keyTest("test2")))
 }
