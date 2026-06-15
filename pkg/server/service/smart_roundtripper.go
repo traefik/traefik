848e3a0866ac92923255bbf1b3195a72db008f1d
Julien Salleyron <julien.salleyron@gmail.com>
2026-06-15 10:40:31 +0200

    refacto

--- pkg/server/service/smart_roundtripper.go
+++ pkg/server/service/smart_roundtripper.go
@@ -1,46 +1,33 @@
 package service
 
 import (
-	"crypto/tls"
-	"net"
 	"net/http"
-	"time"
 
-	"github.com/traefik/traefik/v3/pkg/config/dynamic"
 	"golang.org/x/net/http/httpguts"
-	"golang.org/x/net/http2"
 )
 
-func newSmartRoundTripper(transport *http.Transport, forwardingTimeouts *dynamic.ForwardingTimeouts) (*smartRoundTripper, error) {
+func newSmartRoundTripper(transport *http.Transport) *smartRoundTripper {
+	// HTTP/1 only transport for requests with a Connection: Upgrade header.
 	transportHTTP1 := transport.Clone()
+	transportHTTP1.Protocols = new(http.Protocols)
+	transportHTTP1.Protocols.SetHTTP1(true)
 
-	transportHTTP2, err := http2.ConfigureTransports(transport)
-	if err != nil {
-		return nil, err
-	}
+	// Transport switching automatically to HTTP/2 with TLS ALPN.
+	transportHTTP2 := transport.Clone()
+	transportHTTP2.Protocols = new(http.Protocols)
+	transportHTTP2.Protocols.SetHTTP1(true)
+	transportHTTP2.Protocols.SetHTTP2(true)
 
-	if forwardingTimeouts != nil {
-		transportHTTP2.ReadIdleTimeout = time.Duration(forwardingTimeouts.ReadIdleTimeout)
-		transportHTTP2.PingTimeout = time.Duration(forwardingTimeouts.PingTimeout)
-	}
-
-	transportH2C := &http2.Transport{
-		DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
-			return net.Dial(network, addr)
-		},
-		AllowHTTP: true,
-	}
-
-	if forwardingTimeouts != nil {
-		transportH2C.ReadIdleTimeout = time.Duration(forwardingTimeouts.ReadIdleTimeout)
-		transportH2C.PingTimeout = time.Duration(forwardingTimeouts.PingTimeout)
-	}
+	// Transport speaking HTTP/2 with prior knowledge on unencrypted connections.
+	transportH2C := transport.Clone()
+	transportH2C.Protocols = new(http.Protocols)
+	transportH2C.Protocols.SetUnencryptedHTTP2(true)
 
 	return &smartRoundTripper{
-		http2: transport,
+		http2: transportHTTP2,
 		http:  transportHTTP1,
 		h2c:   transportH2C,
-	}, nil
+	}
 }
 
 // smartRoundTripper implements RoundTrip while making sure that HTTP/2 is not used
@@ -48,30 +35,29 @@ func newSmartRoundTripper(transport *http.Transport, forwardingTimeouts *dynamic
 type smartRoundTripper struct {
 	http2 *http.Transport
 	http  *http.Transport
-	h2c   *http2.Transport
+	h2c   *http.Transport
 }
 
 func (m *smartRoundTripper) Clone() http.RoundTripper {
-	h := m.http.Clone()
-	h2 := m.http2.Clone()
-	// TODO: this clone looses the "h2c" protocol registration. This was already the case before the fix for
-	// https://github.com/traefik/traefik/issues/7465, when "h2c" was registered with
-	// m.http2.RegisterProtocol("h2c", transportH2C).
-	// We should switch to the new http.Protocols.SetUnencryptedHTTP2 on http.Transport, which is part of the stdlib and
-	// supports Clone(). But this switch should probably be done with a new minor release.
-	return &smartRoundTripper{http: h, http2: h2}
+	return &smartRoundTripper{
+		http2: m.http2.Clone(),
+		http:  m.http.Clone(),
+		h2c:   m.h2c.Clone(),
+	}
 }
 
 func (m *smartRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	isH2c := (m.h2c != nil && req.URL.Scheme == "h2c")
-	if isH2c {
+	h2c := req.URL.Scheme == "h2c"
+	if h2c {
 		req.URL.Scheme = "http"
 	}
 
-	// If we have a connection upgrade, we don't use HTTP/2 or HTTP/2 cleartext
+	// Connection upgrades cannot be carried over HTTP/2, they always use HTTP/1.
 	if httpguts.HeaderValuesContainsToken(req.Header["Connection"], "Upgrade") {
 		return m.http.RoundTrip(req)
-	} else if isH2c {
+	}
+
+	if h2c {
 		return m.h2c.RoundTrip(req)
 	}
 
