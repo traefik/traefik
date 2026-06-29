9c6df022c4830e70599a013d2d66692202b95ac5
Kevin Pollet <pollet.kevin@gmail.com>
2026-06-29 16:30:24 +0200

    Sort endpointslices to keep backend IPs consistent across rebuilds

--- pkg/provider/kubernetes/k8s/endpointslice_indexer.go
+++ pkg/provider/kubernetes/k8s/endpointslice_indexer.go
@@ -2,6 +2,8 @@ package k8s
 
 import (
 	"fmt"
+	"slices"
+	"strings"
 
 	discoveryv1 "k8s.io/api/discovery/v1"
 	"k8s.io/client-go/tools/cache"
@@ -32,6 +34,7 @@ var EndpointSliceByServiceNameIndexers = cache.Indexers{
 }
 
 // EndpointSlicesByServiceName returns the EndpointSlices for the given service name in the given namespace from the indexer.
+// The returned endpoint slices are sorted by creation time and name to ensure a consistent order.
 func EndpointSlicesByServiceName(indexer cache.Indexer, namespace, serviceName string) ([]*discoveryv1.EndpointSlice, error) {
 	objectName := cache.NewObjectName(namespace, serviceName)
 	objects, err := indexer.ByIndex(endpointSliceByServiceNameIndexName, objectName.String())
@@ -48,6 +51,17 @@ func EndpointSlicesByServiceName(indexer cache.Indexer, namespace, serviceName s
 
 		endpointSlices = append(endpointSlices, endpointSlice)
 	}
+
+	// As Kubernetes indexer does not guarantee a consistent order of the returned objects,
+	// we sort them by creation time then name to keep backend IP assignment consistent across
+	// configuration rebuilds.
+	slices.SortStableFunc(endpointSlices, func(a, b *discoveryv1.EndpointSlice) int {
+		cmpTime := a.CreationTimestamp.Time.Compare(b.CreationTimestamp.Time)
+		if cmpTime == 0 {
+			return strings.Compare(a.Name, b.Name)
+		}
+		return cmpTime
+	})
 
 	return endpointSlices, nil
 }
